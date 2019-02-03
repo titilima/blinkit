3ef1488788cfccae1d9e2e28e09dc586c97db9fa
titilima <titilima@163.com>
2019-02-04 00:08:41 +0800

    Placeholder.

---
+++ src/chromium/third_party/WebKit/Source/wtf/testing/WTFUnitTestHelpersExport.h
@@ -0,0 +1,19 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: WTFUnitTestHelpersExport.h
+// Description: Placeholders for WTF Test Stuff
+//      Author: Ziming Li
+//     Created: 2019-02-03
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINK_WTF_UNIT_TEST_HELPERS_EXPORT_H
+#define BLINKIT_BLINK_WTF_UNIT_TEST_HELPERS_EXPORT_H
+
+#pragma once
+
+#define WTF_UNITTEST_HELPERS_EXPORT
+
+#endif // BLINKIT_BLINK_WTF_UNIT_TEST_HELPERS_EXPORT_H
