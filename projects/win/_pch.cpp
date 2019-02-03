1fc570e25e0667541dfecea17696864eda0f8292
Z <z@mysoft.tech>
2019-02-03 23:06:57 +0800

    Pre-compiled files.

---
+++ projects/win/_pch.cpp
@@ -0,0 +1,10 @@
+// -------------------------------------------------
+// BlinKit
+// -------------------------------------------------
+//   File Name: _pch.cpp
+// Description: Source for Pre-compiled Headers
+//      Author: Ziming Li
+//     Created: 2018-12-09
+// -------------------------------------------------
+// Copyright (C) 2018 MingYang Software Technology.
+// -------------------------------------------------
