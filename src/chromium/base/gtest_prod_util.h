2351d4e9e20ca678831b858be8ad97fb640596e8
titilima <titilima@163.com>
2019-02-04 16:49:23 +0800

    Placeholder for unit tests.

---
+++ src/chromium/base/gtest_prod_util.h
@@ -0,0 +1,17 @@
+// -------------------------------------------------
+// BlinKit - base Library
+// -------------------------------------------------
+//   File Name: gtest_prod_util.h
+// Description: Placeholders for Unit Tests
+//      Author: Ziming Li
+//     Created: 2018-07-08
+// -------------------------------------------------
+// Copyright (C) 2018 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BASE_GTEST_PROD_UTIL_H
+#define BLINKIT_BASE_GTEST_PROD_UTIL_H
+
+#define FRIEND_TEST_ALL_PREFIXES(...)
+
+#endif  // BLINKIT_BASE_GTEST_PROD_UTIL_H
