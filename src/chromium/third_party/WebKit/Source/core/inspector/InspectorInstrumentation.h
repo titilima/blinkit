7a0fb0aef6e2c912595838f27695cc7aea15b133
titilima <titilima@163.com>
2019-02-05 14:27:09 +0800

    Placeholders for inspector.

--- src/chromium/third_party/WebKit/Source/core/inspector/InspectorInstrumentation.h
+++ src/chromium/third_party/WebKit/Source/core/inspector/InspectorInstrumentation.h
@@ -1,184 +1,128 @@
-/*
-* Copyright (C) 2010 Google Inc. All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are
-* met:
-*
-*     * Redistributions of source code must retain the above copyright
-* notice, this list of conditions and the following disclaimer.
-*     * Redistributions in binary form must reproduce the above
-* copyright notice, this list of conditions and the following disclaimer
-* in the documentation and/or other materials provided with the
-* distribution.
-*     * Neither the name of Google Inc. nor the names of its
-* contributors may be used to endorse or promote products derived from
-* this software without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-* A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-* OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-* THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: InspectorInstrumentation.h
+// Description: Placeholders for Inspector
+//      Author: Ziming Li
+//     Created: 2018-08-31
+// -------------------------------------------------
+// Copyright (C) 2018 MingYang Software Technology.
+// -------------------------------------------------
 
-#ifndef InspectorInstrumentation_h
-#define InspectorInstrumentation_h
+#ifndef BLINKIT_BLINK_INSPECTOR_INSTRUMENTATION_H
+#define BLINKIT_BLINK_INSPECTOR_INSTRUMENTATION_H
 
-#include "bindings/core/v8/ScriptString.h"
-#include "core/CoreExport.h"
-#include "core/animation/Animation.h"
-#include "core/css/CSSSelector.h"
-#include "core/css/CSSStyleDeclaration.h"
-#include "core/css/CSSStyleSheet.h"
-#include "core/dom/CharacterData.h"
-#include "core/dom/Element.h"
-#include "core/dom/ExecutionContext.h"
-#include "core/events/NodeEventContext.h"
-#include "core/frame/LocalFrame.h"
-#include "core/inspector/ConsoleAPITypes.h"
-#include "core/layout/HitTestResult.h"
-#include "core/layout/LayoutImage.h"
+#pragma once
+
 #include "core/page/ChromeClient.h"
-#include "platform/network/EncodedFormData.h"
-#include "platform/network/ResourceRequest.h"
-#include "platform/network/WebSocketHandshakeRequest.h"
-#include "platform/network/WebSocketHandshakeResponse.h"
-#include "wtf/RefPtr.h"
+
+namespace WTF {
+class String;
+}
 
 namespace blink {
 
+struct FetchInitiatorInfo;
+class CharacterData;
 class Document;
+class DocumentLoader;
+class Element;
+class Event;
 class EventTarget;
 class ExecutionContext;
-class InstrumentingAgents;
+class ExecutionContextTask;
+class GraphicsContext;
+class GraphicsLayer;
+class LayoutObject;
+class LayoutRect;
+class LocalFrame;
+class Node;
+class PseudoElement;
+class ResourceLoader;
+class ResourceResponse;
+class QualifiedName;
+class Resource;
+class ResourceError;
+class ShadowRoot;
 class ThreadableLoaderClient;
-class WorkerGlobalScope;
-class WorkerInspectorProxy;
 
-#define FAST_RETURN_IF_NO_FRONTENDS(value) if (!hasFrontends()) return value;
-
-class CORE_EXPORT InspectorInstrumentationCookie {
-    STACK_ALLOCATED();
-public:
-    InspectorInstrumentationCookie();
-    explicit InspectorInstrumentationCookie(InstrumentingAgents*);
-    InspectorInstrumentationCookie(const InspectorInstrumentationCookie&);
-    InspectorInstrumentationCookie& operator=(const InspectorInstrumentationCookie&);
-    ~InspectorInstrumentationCookie();
-
-    InstrumentingAgents* instrumentingAgents() const { return m_instrumentingAgents.get(); }
-    bool isValid() const { return !!m_instrumentingAgents; }
-
-private:
-    RefPtrWillBeMember<InstrumentingAgents> m_instrumentingAgents;
-};
+typedef int InspectorInstrumentationCookie;
 
 namespace InspectorInstrumentation {
 
-class CORE_EXPORT FrontendCounter {
-    STATIC_ONLY(FrontendCounter);
-private:
-    friend void frontendCreated();
-    friend void frontendDeleted();
-    friend bool hasFrontends();
-    static int s_frontendCounter;
-};
-
-inline void frontendCreated() { atomicIncrement(&FrontendCounter::s_frontendCounter); }
-inline void frontendDeleted() { atomicDecrement(&FrontendCounter::s_frontendCounter); }
-inline bool hasFrontends() { return acquireLoad(&FrontendCounter::s_frontendCounter); }
-
-CORE_EXPORT void registerInstrumentingAgents(InstrumentingAgents*);
-CORE_EXPORT void unregisterInstrumentingAgents(InstrumentingAgents*);
-
-// Called from generated instrumentation code.
-CORE_EXPORT InstrumentingAgents* instrumentingAgentsFor(LocalFrame*);
-InstrumentingAgents* instrumentingAgentsFor(EventTarget*);
-InstrumentingAgents* instrumentingAgentsFor(ExecutionContext*);
-InstrumentingAgents* instrumentingAgentsFor(Document&);
-InstrumentingAgents* instrumentingAgentsFor(Document*);
-InstrumentingAgents* instrumentingAgentsFor(LayoutObject*);
-InstrumentingAgents* instrumentingAgentsFor(Node*);
-InstrumentingAgents* instrumentingAgentsFor(WorkerGlobalScope*);
-
-// Helper for the one above.
-CORE_EXPORT InstrumentingAgents* instrumentingAgentsForNonDocumentContext(ExecutionContext*);
-
-}  // namespace InspectorInstrumentation
-
-namespace InstrumentationEvents {
-extern const char PaintSetup[];
-extern const char Paint[];
-extern const char Layer[];
-extern const char RequestMainThreadFrame[];
-extern const char BeginFrame[];
-extern const char DrawFrame[];
-extern const char ActivateLayerTree[];
-extern const char EmbedderCallback[];
-};
-
-namespace InstrumentationEventArguments {
-extern const char FrameId[];
-extern const char LayerId[];
-extern const char LayerTreeId[];
-extern const char PageId[];
-extern const char CallbackName[];
-};
-
-namespace InspectorInstrumentation {
-
-inline InstrumentingAgents* instrumentingAgentsFor(ExecutionContext* context)
-{
-    if (!context)
-        return 0;
-    return context->isDocument() ? instrumentingAgentsFor(*toDocument(context)) : instrumentingAgentsForNonDocumentContext(context);
-}
-
-inline InstrumentingAgents* instrumentingAgentsFor(Document& document)
-{
-    LocalFrame* frame = document.frame();
-    if (!frame && document.templateDocumentHost())
-        frame = document.templateDocumentHost()->frame();
-    return instrumentingAgentsFor(frame);
-}
-
-inline InstrumentingAgents* instrumentingAgentsFor(Document* document)
-{
-    return document ? instrumentingAgentsFor(*document) : 0;
-}
-
-inline InstrumentingAgents* instrumentingAgentsFor(CSSStyleSheet* styleSheet)
-{
-    return styleSheet ? instrumentingAgentsFor(styleSheet->ownerDocument()) : 0;
-}
-
-inline InstrumentingAgents* instrumentingAgentsFor(Node* node)
-{
-    return node ? instrumentingAgentsFor(node->document()) : 0;
-}
-
-inline InstrumentingAgents* instrumentingAgentsFor(CSSStyleDeclaration* declaration)
-{
-    return declaration ? instrumentingAgentsFor(declaration->parentStyleSheet()) : 0;
-}
+inline void activeStyleSheetsUpdated(Document *) {}
+inline void animationPlayStateChanged(Document *, Animation *, Animation::AnimationPlayState, Animation::AnimationPlayState) {}
+inline void applyUserAgentOverride(LocalFrame *, String *) {}
+inline void characterDataModified(CharacterData *) {}
+inline bool collectingHTMLParseErrors(Document *) { return false; }
+inline void continueAfterXFrameOptionsDenied(LocalFrame *, DocumentLoader *, unsigned long, const ResourceResponse &) {}
+inline void continueWithPolicyIgnore(LocalFrame *, DocumentLoader *, unsigned long, const ResourceResponse &) {}
+inline void didBlockRequest(LocalFrame *, const ResourceRequest &, DocumentLoader *, const FetchInitiatorInfo &, ResourceRequestBlockedReason) {}
+inline void didCancelAnimationFrame(ExecutionContext *, int) {}
+inline void didClearDocumentOfWindowObject(LocalFrame *) {}
+inline void didCommitLoad(LocalFrame *, DocumentLoader *) {}
+inline void didCreateAnimation(Document *, unsigned) {}
+inline void didEnqueueEvent(EventTarget *, Event *) {}
+inline void didFailLoading(LocalFrame *frame, unsigned long, const ResourceError &) {}
+inline void didFinishEventSourceRequest(ExecutionContext *, ThreadableLoaderClient *) {}
+inline void didFinishLoading(LocalFrame *, unsigned long, double, int64_t) {}
+inline void didFireAnimationFrame(InspectorInstrumentationCookie) {}
+inline void didInsertDOMNode(Node *) {}
+inline void didInvalidateStyleAttr(Node *) {}
+inline void didModifyDOMAttr(Element *, const QualifiedName &, const AtomicString &) {}
+inline void didPaint(LayoutObject *, const GraphicsLayer *, GraphicsContext &, const LayoutRect &) {}
+inline void didPerformExecutionContextTask(ExecutionContext *) {}
+inline void didPerformElementShadowDistribution(Element *) {}
+inline void didPostExecutionContextTask(ExecutionContext *, ExecutionContextTask *) {}
+inline void didPushShadowRoot(Element *, ShadowRoot *) {}
+inline void didRecalculateStyle(InspectorInstrumentationCookie, int) {}
+inline void didReceiveCORSRedirectResponse(LocalFrame *, unsigned long, DocumentLoader *, const ResourceResponse &, ResourceLoader *) {}
+inline void didReceiveData(LocalFrame *, unsigned long, const char *, int, int) {}
+inline void didReceiveResourceResponse(LocalFrame *, unsigned long, DocumentLoader *, const ResourceResponse &, ResourceLoader *) {}
+inline void didRemoveDOMAttr(Element *, const QualifiedName &) {}
+inline void didRemoveEvent(EventTarget *, Event *) {}
+inline void didRequestAnimationFrame(ExecutionContext *, int) {}
+inline void didResizeMainFrame(LocalFrame *) {}
+inline void didRunJavaScriptDialog(InspectorInstrumentationCookie, bool) {}
+inline void didScheduleStyleRecalculation(Document *) {}
+inline void didStartProvisionalLoad(LocalFrame *) {}
+inline void didUpdateLayout(LocalFrame *) {}
+inline void documentDetached(Document *) {}
+inline void documentThreadableLoaderStartedLoadingForClient(ExecutionContext *, unsigned long, ThreadableLoaderClient *) {}
+inline void domContentLoadedEventFired(LocalFrame *) {}
+inline bool forcePseudoState(Element *, CSSSelector::PseudoType) { return false; }
+inline void frameAttachedToParent(LocalFrame *) {}
+inline void frameClearedScheduledNavigation(LocalFrame *) {}
+inline void frameDetachedFromParent(LocalFrame *) {}
+inline void frameScheduledNavigation(LocalFrame *, double) {}
+inline void frameStartedLoading(LocalFrame *) {}
+inline void frameStoppedLoading(LocalFrame *) {}
+inline bool hasFrontends(void) { return false; }
+inline bool isDebuggerPaused(LocalFrame *) { return false; }
+inline void layerTreeDidChange(LocalFrame *) {}
+inline void markResourceAsCached(LocalFrame *, unsigned long) {}
+inline void mediaQueryResultChanged(Document *) {}
+inline void networkStateChanged(LocalFrame *, bool) {}
+inline void pseudoElementCreated(PseudoElement *) {}
+inline void pseudoElementDestroyed(PseudoElement *) {}
+inline void scriptExecutionBlockedByCSP(ExecutionContext *, const String &) {}
+inline bool shouldForceCORSPreflight(Document *) { return false; }
+inline void updateApplicationCacheStatus(LocalFrame *) {}
+inline void willDestroyResource(Resource *) {}
+inline void willDispachEventSourceEvent(ExecutionContext *, ThreadableLoaderClient *, const AtomicString &, const AtomicString &, const Vector<UChar> &) {}
+inline InspectorInstrumentationCookie willFireAnimationFrame(ExecutionContext *, int) { return 0; }
+inline void willInsertDOMNode(Node *) {}
+inline void willModifyDOMAttr(Element *, const AtomicString &, const AtomicString &) {}
+inline void willPerformExecutionContextTask(ExecutionContext *, ExecutionContextTask *) {}
+inline void willPopShadowRoot(Element *, ShadowRoot *) {}
+inline InspectorInstrumentationCookie willRecalculateStyle(Document *) { return 0; }
+inline void willRemoveDOMNode(Node *) {}
+inline InspectorInstrumentationCookie willRunJavaScriptDialog(LocalFrame *, const String &, ChromeClient::DialogType) { return 0; }
+inline void willSendEventSourceRequest(ExecutionContext *, ThreadableLoaderClient *) {}
+inline void willSendRequest(LocalFrame *, unsigned long, DocumentLoader *, ResourceRequest &, const ResourceResponse &, const FetchInitiatorInfo &) {}
+inline void willSetInnerHTML(Element *) {}
 
 } // namespace InspectorInstrumentation
-
-InstrumentingAgents* instrumentationForWorkerGlobalScope(WorkerGlobalScope*);
-
 } // namespace blink
 
-#include "core/InspectorInstrumentationInl.h"
-
-#include "core/inspector/InspectorInstrumentationCustomInl.h"
-
-#include "core/InspectorOverridesInl.h"
-
-#endif // !defined(InspectorInstrumentation_h)
+#endif // BLINKIT_BLINK_INSPECTOR_INSTRUMENTATION_H
