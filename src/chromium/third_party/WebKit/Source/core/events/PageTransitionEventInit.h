fcd1e3c75d6f443ec4ee3bff5c4cff35cb3b3759
titilima <titilima@163.com>
2019-02-05 16:47:13 +0800

    Update placeholders.

---
+++ src/chromium/third_party/WebKit/Source/core/events/PageTransitionEventInit.h
@@ -0,0 +1,37 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: PageTransitionEventInit.h
+// Description: PageTransitionEventInit Class
+//      Author: Ziming Li
+//     Created: 2018-07-24
+// -------------------------------------------------
+// Copyright (C) 2018 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINK_PAGE_TRANSITION_EVENT_INIT_H
+#define BLINKIT_BLINK_PAGE_TRANSITION_EVENT_INIT_H
+
+#pragma once
+
+#include <optional>
+#include "core/events/EventInit.h"
+
+namespace blink {
+
+class PageTransitionEventInit : public EventInit
+{
+public:
+    bool hasPersisted(void) const { return m_persisted.has_value(); }
+    bool persisted(void) const {
+        assert(m_persisted.has_value());
+        return m_persisted.value();
+    }
+    void setPersisted(bool value) { m_persisted = value; }
+private:
+    std::optional<bool> m_persisted;
+};
+
+} // namespace blink
+
+#endif // BLINKIT_BLINK_PAGE_TRANSITION_EVENT_INIT_H
