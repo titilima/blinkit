8139f9ee6a418ed0d7095c8670712adf60489c6d
titilima <titilima@163.com>
2019-02-05 11:31:11 +0800

    Redirector for ExceptionState.

---
+++ src/chromium/third_party/WebKit/Source/bindings/core/v8/ExceptionStatePlaceholder.h
@@ -0,0 +1,12 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: ExceptionStatePlaceholder.h
+// Description: ExceptionState Redirector
+//      Author: Ziming Li
+//     Created: 2019-02-05
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#include "platform/bindings/exception_state.h"
