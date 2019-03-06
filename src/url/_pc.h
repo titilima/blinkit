aef20213aa71446b886180d9f824b83bfea70d06
titilima <titilima@163.com>
2019-03-06 16:51:33 +0800

    Build URL library.

---
+++ src/url/_pc.h
@@ -0,0 +1,25 @@
+// -------------------------------------------------
+// BlinKit - url Library
+// -------------------------------------------------
+//   File Name: _pc.h
+// Description: Pre-compiled Header File
+//      Author: Ziming Li
+//     Created: 2019-03-06
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_URL__PC_H
+#define BLINKIT_URL__PC_H
+
+#pragma once
+
+#include "build/build_config.h"
+
+#if OS_WIN
+#   pragma warning(disable: 4819)
+#endif
+
+#include <cassert>
+
+#endif // BLINKIT_URL__PC_H
---
+++ src/url/url_canon.cpp
@@ -0,0 +1,22 @@
+// -------------------------------------------------
+// BlinKit - url Library
+// -------------------------------------------------
+//   File Name: url_canon.cpp
+// Description: Canonicalize Helpers
+//      Author: Ziming Li
+//     Created: 2019-03-06
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#include "url/url_canon.h"
+
+namespace url {
+
+bool IDNToASCII(const base::char16 *src, int src_len, CanonOutputW *output)
+{
+    assert(false); // BKTODO:
+    return false;
+}
+
+} // namespace url
