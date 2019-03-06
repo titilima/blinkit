7c49905e4e00599fc805db7cea65d8f9934c2c12
titilima <titilima@163.com>
2019-03-06 16:50:47 +0800

    Placeholder for ANNOTATE_LEAKING_OBJECT_PTR.

---
+++ src/chromium/base/debug/leak_annotations.h
@@ -0,0 +1,19 @@
+// -------------------------------------------------
+// BlinKit - base Library
+// -------------------------------------------------
+//   File Name: leak_annotations.h
+// Description: Placeholders for Leak Annotations
+//      Author: Ziming Li
+//     Created: 2019-03-06
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BASE_LEAK_ANNOTATIONS_H
+#define BLINKIT_BASE_LEAK_ANNOTATIONS_H
+
+#pragma once
+
+#define ANNOTATE_LEAKING_OBJECT_PTR(...)
+
+#endif // BLINKIT_BASE_LEAK_ANNOTATIONS_H
