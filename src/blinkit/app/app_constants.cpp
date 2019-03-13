53e19025b193fe7dfb4023471a36e9ee7ef060ef
titilima <titilima@163.com>
2019-03-13 17:17:41 +0800

    Default user agent string for Windows.

---
+++ src/blinkit/app/app_constants.cpp
@@ -0,0 +1,24 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: app_constants.cpp
+// Description: Application Constants
+//      Author: Ziming Li
+//     Created: 2019-03-13
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#include "app_constants.h"
+
+namespace BlinKit {
+namespace AppConstants {
+
+const char DefaultUserAgent[] =
+#if OS_WIN
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36"
+#endif
+    ;
+
+} // namespace AppConstants
+} // namespace BlinKit
---
+++ src/blinkit/app/app_constants.h
@@ -0,0 +1,25 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: app_constants.h
+// Description: Application Constants
+//      Author: Ziming Li
+//     Created: 2019-03-13
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINKIT_APP_CONSTANTS_H
+#define BLINKIT_BLINKIT_APP_CONSTANTS_H
+
+#pragma once
+
+namespace BlinKit {
+namespace AppConstants {
+
+extern const char DefaultUserAgent[];
+
+} // namespace AppConstants
+} // namespace BlinKit
+
+#endif // BLINKIT_BLINKIT_APP_CONSTANTS_H
