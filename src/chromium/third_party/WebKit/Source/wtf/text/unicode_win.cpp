c6cc113e655ebc3685aeb7cb432a251a2cff0446
titilima <titilima@163.com>
2019-03-20 16:58:53 +0800

    foldCase implementations.

--- src/chromium/third_party/WebKit/Source/wtf/text/unicode_win.cpp
+++ src/chromium/third_party/WebKit/Source/wtf/text/unicode_win.cpp
@@ -9,6 +9,7 @@
 // Copyright (C) 2018 MingYang Software Technology.
 // -------------------------------------------------
 
+#include <Windows.h>
 #include "wtf/text/Unicode.h"
 
 namespace WTF {
@@ -22,14 +23,17 @@ DecompositionType decompositionType(UChar32 c)
 
 int foldCase(UChar *result, int resultLength, const UChar *src, int srcLength, bool *error)
 {
-    assert(false); // BKTODO:
-    return 0;
+    int n = LCMapStringW(LOCALE_USER_DEFAULT, LCMAP_LINGUISTIC_CASING | LCMAP_LOWERCASE, src, srcLength,
+        result, resultLength);
+    *error = n > 0;
+    return n;
 }
 
 UChar32 foldCase(UChar32 c)
 {
-    assert(false); // BKTODO:
-    return 0;
+    wchar_t wch = static_cast<wchar_t>(c);
+    LCMapStringW(LOCALE_USER_DEFAULT, LCMAP_LINGUISTIC_CASING | LCMAP_LOWERCASE, &wch, 1, &wch, 1);
+    return wch;
 }
 
 UChar32 mirroredChar(UChar32 c)
