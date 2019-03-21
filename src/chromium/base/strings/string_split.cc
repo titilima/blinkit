04fcf52b5c26b0c265d07101e95da8bd99179d82
titilima <titilima@163.com>
2019-03-21 15:57:28 +0800

    String splitting.

--- src/chromium/base/strings/string_split.cc
+++ src/chromium/base/strings/string_split.cc
@@ -1,3 +1,14 @@
+// -------------------------------------------------
+// BlinKit - base Library
+// -------------------------------------------------
+//   File Name: string_split.cc
+// Description: String Splitting
+//      Author: Ziming Li
+//     Created: 2019-03-21
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -8,7 +19,6 @@
 
 #include "base/logging.h"
 #include "base/strings/string_util.h"
-#include "base/third_party/icu/icu_utf.h"
 
 namespace base {
 
