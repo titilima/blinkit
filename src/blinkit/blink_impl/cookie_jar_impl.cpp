2bb82139ddc67c4eebff1dec8ae4398280356450
titilima <titilima@163.com>
2019-03-22 12:21:51 +0800

    Skeleton implementation for CookieJarImpl.

---
+++ src/blinkit/blink_impl/cookie_jar_impl.cpp
@@ -0,0 +1,46 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: cookie_jar_impl.cpp
+// Description: CookieJarImpl Class
+//      Author: Ziming Li
+//     Created: 2019-03-22
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#include "cookie_jar_impl.h"
+
+using namespace blink;
+
+namespace BlinKit {
+
+CookieJarImpl::CookieJarImpl(void)
+{
+    // Nothing
+}
+
+WebString CookieJarImpl::cookieRequestHeaderFieldValue(const WebURL &URL, const WebURL &firstPartyForCookies)
+{
+    assert(false); // BKTODO:
+    return WebString();
+}
+
+WebString CookieJarImpl::cookies(const WebURL &URL, const WebURL &firstPartyForCookies)
+{
+    assert(false); // BKTODO:
+    return WebString();
+}
+
+std::string CookieJarImpl::GetCookie(const std::string &URL) const
+{
+    assert(false); // BKTODO:
+    return std::string();
+}
+
+void CookieJarImpl::setCookie(const WebURL &URL, const WebURL &firstPartyForCookies, const WebString &cookie)
+{
+    assert(false); // BKTODO:
+}
+
+} // namespace BlinKit
---
+++ src/blinkit/blink_impl/cookie_jar_impl.h
@@ -0,0 +1,39 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: cookie_jar_impl.h
+// Description: CookieJarImpl Class
+//      Author: Ziming Li
+//     Created: 2019-03-22
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINKIT_COOKIE_JAR_IMPL_H
+#define BLINKIT_BLINKIT_COOKIE_JAR_IMPL_H
+
+#pragma once
+
+#include <mutex>
+#include "public/platform/WebCookieJar.h"
+
+namespace BlinKit {
+
+class CookieJarImpl final : public blink::WebCookieJar
+{
+public:
+    CookieJarImpl(void);
+
+    std::string GetCookie(const std::string &URL) const;
+private:
+    // blink::WebCookieJar
+    void setCookie(const blink::WebURL&, const blink::WebURL& firstPartyForCookies, const blink::WebString& cookie) override;
+    blink::WebString cookies(const blink::WebURL&, const blink::WebURL& firstPartyForCookies) override;
+    blink::WebString cookieRequestHeaderFieldValue(const blink::WebURL&, const blink::WebURL& firstPartyForCookies) override;
+
+    mutable std::recursive_mutex m_lock;
+};
+
+} // namespace BlinKit
+
+#endif // BLINKIT_BLINKIT_COOKIE_JAR_IMPL_H
