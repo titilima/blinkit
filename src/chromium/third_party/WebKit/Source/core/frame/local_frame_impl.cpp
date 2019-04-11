5f9d0e807a2e0736450b8de54d463dd2c77a841c
titilima <titilima@163.com>
2019-04-11 09:27:29 +0800

    Rename.

--- src/chromium/third_party/WebKit/Source/core/frame/local_frame_impl.cpp
+++ src/chromium/third_party/WebKit/Source/core/frame/local_frame_impl.cpp
@@ -1,15 +1,15 @@
 // -------------------------------------------------
 // BlinKit - blink Library
 // -------------------------------------------------
-//   File Name: lite_local_frame.cpp
-// Description: LiteLocalFrame Class
+//   File Name: local_frame_impl.cpp
+// Description: LocalFrameImpl Class
 //      Author: Ziming Li
 //     Created: 2019-04-10
 // -------------------------------------------------
 // Copyright (C) 2019 MingYang Software Technology.
 // -------------------------------------------------
 
-#include "lite_local_frame.h"
+#include "local_frame_impl.h"
 
 #include "core/frame/LocalDOMWindow.h"
 #include "core/loader/FrameLoaderClient.h"
@@ -19,7 +19,7 @@
 
 namespace blink {
 
-LiteLocalFrame::LiteLocalFrame(FrameLoaderClient *client, FrameHost *host, FrameOwner *owner)
+LocalFrameImpl::LocalFrameImpl(FrameLoaderClient *client, FrameHost *host, FrameOwner *owner)
     : Frame(client, host, owner)
     , m_loader(this)
     , m_navigationScheduler(NavigationScheduler::create(this))
@@ -27,64 +27,58 @@ LiteLocalFrame::LiteLocalFrame(FrameLoaderClient *client, FrameHost *host, Frame
 {
 }
 
-PassRefPtrWillBeRawPtr<LiteLocalFrame> LiteLocalFrame::create(FrameLoaderClient *client)
-{
-    RefPtrWillBeRawPtr<LiteLocalFrame> frame = adoptRefWillBeNoop(new LiteLocalFrame(client, nullptr, nullptr));
-    return frame.release();
-}
-
-Document* LiteLocalFrame::document(void) const
+Document* LocalFrameImpl::document(void) const
 {
     return m_domWindow ? m_domWindow->document() : nullptr;
 }
 
-DOMWindow* LiteLocalFrame::domWindow(void) const
+DOMWindow* LocalFrameImpl::domWindow(void) const
 {
     return m_domWindow.get();
 }
 
-void LiteLocalFrame::navigate(Document &originDocument, const KURL &url, bool replaceCurrentItem, UserGestureStatus userGestureStatus)
+void LocalFrameImpl::navigate(Document &originDocument, const KURL &url, bool replaceCurrentItem, UserGestureStatus userGestureStatus)
 {
     assert(false); // Not reached!
 }
 
-void LiteLocalFrame::navigate(const FrameLoadRequest & request)
+void LocalFrameImpl::navigate(const FrameLoadRequest & request)
 {
     assert(false); // Not reached!
 }
 
-bool LiteLocalFrame::prepareForCommit(void)
+bool LocalFrameImpl::prepareForCommit(void)
 {
     return loader().prepareForCommit();
 }
 
-void LiteLocalFrame::printNavigationErrorMessage(const Frame &targetFrame, const char *reason)
+void LocalFrameImpl::printNavigationErrorMessage(const Frame &targetFrame, const char *reason)
 {
     assert(false); // Not reached!
 }
 
-void LiteLocalFrame::reload(FrameLoadType loadType, ClientRedirectPolicy clientRedirectPolicy)
+void LocalFrameImpl::reload(FrameLoadType loadType, ClientRedirectPolicy clientRedirectPolicy)
 {
     assert(false); // Not reached!
 }
 
-SecurityContext* LiteLocalFrame::securityContext(void) const
+SecurityContext* LocalFrameImpl::securityContext(void) const
 {
     return document();
 }
 
-bool LiteLocalFrame::shouldClose(void)
+bool LocalFrameImpl::shouldClose(void)
 {
     return m_loader.shouldClose();
 }
 
-WindowProxy* LiteLocalFrame::windowProxy(DOMWrapperWorld &world)
+WindowProxy* LocalFrameImpl::windowProxy(DOMWrapperWorld &world)
 {
     assert(false); // Not reached!
     return nullptr;
 }
 
-WindowProxyManager* LiteLocalFrame::windowProxyManager(void) const
+WindowProxyManager* LocalFrameImpl::windowProxyManager(void) const
 {
     assert(false); // Not reached!
     return nullptr;
@@ -92,7 +86,7 @@ WindowProxyManager* LiteLocalFrame::windowProxyManager(void) const
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-FrameNavigationDisabler::FrameNavigationDisabler(LiteLocalFrame &frame) : m_frame(&frame)
+FrameNavigationDisabler::FrameNavigationDisabler(LocalFrameImpl &frame) : m_frame(&frame)
 {
     m_frame->DisableNavigation();
 }
--- src/chromium/third_party/WebKit/Source/core/frame/local_frame_impl.h
+++ src/chromium/third_party/WebKit/Source/core/frame/local_frame_impl.h
@@ -1,16 +1,16 @@
 // -------------------------------------------------
 // BlinKit - blink Library
 // -------------------------------------------------
-//   File Name: lite_local_frame.h
-// Description: LiteLocalFrame Class
+//   File Name: local_frame_impl.h
+// Description: LocalFrameImpl Class
 //      Author: Ziming Li
 //     Created: 2019-04-10
 // -------------------------------------------------
 // Copyright (C) 2019 MingYang Software Technology.
 // -------------------------------------------------
 
-#ifndef BLINKIT_BLINK_LITE_LOCAL_FRAME_H
-#define BLINKIT_BLINK_LITE_LOCAL_FRAME_H
+#ifndef BLINKIT_BLINK_LOCAL_FRAME_IMPL_H
+#define BLINKIT_BLINK_LOCAL_FRAME_IMPL_H
 
 #pragma once
 
@@ -23,11 +23,9 @@ class LocalDOMWindow;
 class NavigationScheduler;
 class ScriptController;
 
-class LiteLocalFrame : public Frame
+class LocalFrameImpl : public Frame
 {
 public:
-    static PassRefPtrWillBeRawPtr<LiteLocalFrame> create(FrameLoaderClient *client);
-
     void init(void) { m_loader.init(); }
 
     Document* document(void) const;
@@ -51,7 +49,7 @@ public:
 
     bool isNavigationAllowed(void) const { return 0 == m_navigationDisableCount; }
 protected:
-    LiteLocalFrame(FrameLoaderClient *, FrameHost *, FrameOwner *);
+    LocalFrameImpl(FrameLoaderClient *, FrameHost *, FrameOwner *);
 
     mutable FrameLoader m_loader;
     OwnPtrWillBeMember<NavigationScheduler> m_navigationScheduler;
@@ -63,7 +61,6 @@ private:
     void EnableNavigation(void) { --m_navigationDisableCount; }
     void DisableNavigation(void) { ++m_navigationDisableCount; }
 
-    Type GetType(void) const override { return Type::LiteLocal; }
     WindowProxyManager* windowProxyManager(void) const override final;
 
     int m_navigationDisableCount = 0;
@@ -73,12 +70,12 @@ class FrameNavigationDisabler {
     WTF_MAKE_NONCOPYABLE(FrameNavigationDisabler);
     STACK_ALLOCATED();
 public:
-    explicit FrameNavigationDisabler(LiteLocalFrame &);
+    explicit FrameNavigationDisabler(LocalFrameImpl &);
     ~FrameNavigationDisabler(void);
 private:
-    RawPtrWillBeMember<LiteLocalFrame> m_frame;
+    RawPtrWillBeMember<LocalFrameImpl> m_frame;
 };
 
 } // namespace blink
 
-#endif // BLINKIT_BLINK_LITE_LOCAL_FRAME_H
+#endif // BLINKIT_BLINK_LOCAL_FRAME_IMPL_H
