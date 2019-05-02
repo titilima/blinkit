606b8f9619d0962f81b241fa32c45e54bbefa07d
titilima <titilima@163.com>
2019-05-02 22:18:25 +0800

    Rollback.

--- src/chromium/third_party/WebKit/Source/core/html/HTMLTextFormControlElement.cpp
+++ src/chromium/third_party/WebKit/Source/core/html/HTMLTextFormControlElement.cpp
@@ -107,15 +107,11 @@ void HTMLTextFormControlElement::dispatchBlurEvent(Element* newFocusedElement, W
 
 void HTMLTextFormControlElement::defaultEventHandler(Event* event)
 {
-#ifdef BLINKIT_CRAWLER_ONLY
-    assert(false); // BKTODO: Not reached!
-#else
     if (event->type() == EventTypeNames::webkitEditableContentChanged && layoutObject() && layoutObject()->isTextControl()) {
         m_lastChangeWasUserEdit = !document().isRunningExecCommand();
         subtreeHasChanged();
         return;
     }
-#endif
 
     HTMLFormControlElementWithState::defaultEventHandler(event);
 }
@@ -520,10 +516,6 @@ static inline void setContainerAndOffsetForRange(Node* node, int offset, Node*& 
 
 PassRefPtrWillBeRawPtr<Range> HTMLTextFormControlElement::selection() const
 {
-#ifdef BLINKIT_CRAWLER_ONLY
-    assert(false); // BKTODO: Not reached!
-    return nullptr;
-#else
     if (!layoutObject() || !isTextFormControl())
         return nullptr;
 
@@ -561,7 +553,6 @@ PassRefPtrWillBeRawPtr<Range> HTMLTextFormControlElement::selection() const
         return nullptr;
 
     return Range::create(document(), startNode, start, endNode, end);
-#endif
 }
 
 const AtomicString& HTMLTextFormControlElement::autocapitalize() const
@@ -598,9 +589,6 @@ void HTMLTextFormControlElement::restoreCachedSelection()
 
 void HTMLTextFormControlElement::selectionChanged(bool userTriggered)
 {
-#ifdef BLINKIT_CRAWLER_ONLY
-    assert(false); // BKTODO: Not reached!
-#else
     if (!layoutObject() || !isTextFormControl())
         return;
 
@@ -611,7 +599,6 @@ void HTMLTextFormControlElement::selectionChanged(bool userTriggered)
         if (frame->selection().isRange() && userTriggered)
             dispatchEvent(Event::createBubble(EventTypeNames::select));
     }
-#endif
 }
 
 void HTMLTextFormControlElement::scheduleSelectEvent()
@@ -715,10 +702,6 @@ String HTMLTextFormControlElement::valueWithHardLineBreaks() const
     if (!innerText || !isTextFormControl())
         return value();
 
-#ifdef BLINKIT_CRAWLER_ONLY
-    assert(false); // BKTODO: Not reached!
-        return value();
-#else
     LayoutBlockFlow* layoutObject = toLayoutBlockFlow(innerText->layoutObject());
     if (!layoutObject)
         return value();
@@ -753,7 +736,6 @@ String HTMLTextFormControlElement::valueWithHardLineBreaks() const
             getNextSoftBreak(line, breakNode, breakOffset);
     }
     return finishText(result);
-#endif
 }
 
 HTMLTextFormControlElement* enclosingTextFormControl(const Position& position)
