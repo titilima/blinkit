a58d039415dc49c3253de1404ef9e792a43e402e
titilima <titilima@163.com>
2019-05-10 15:54:28 +0800

    Remove frame tree stuff.

--- src/chromium/third_party/WebKit/Source/core/paint/PaintPropertyTreeBuilder.cpp
+++ src/chromium/third_party/WebKit/Source/core/paint/PaintPropertyTreeBuilder.cpp
@@ -1,3 +1,14 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: PaintPropertyTreeBuilder.cpp
+// Description: PaintPropertyTreeBuilder Class
+//      Author: Ziming Li
+//     Created: 2019-05-10
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -97,13 +108,6 @@ void PaintPropertyTreeBuilder::walk(FrameView& frameView, const PaintPropertyTre
 
     if (LayoutView* layoutView = frameView.layoutView())
         walk(*layoutView, localContext);
-
-    for (Frame* child = frameView.frame().tree().firstChild(); child; child = child->tree().nextSibling()) {
-        if (!child->isLocalFrame())
-            continue;
-        if (FrameView* childView = toLocalFrame(child)->view())
-            walk(*childView, localContext);
-    }
 }
 
 static void deriveBorderBoxFromContainerContext(const LayoutObject& object, PaintPropertyTreeBuilderContext& context)
