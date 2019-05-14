dbe5e606b14f337792bbeeb6c621de2e83925863
titilima <titilima@163.com>
2019-05-14 09:11:26 +0800

    Remove DNS-prefetching stuff.

--- src/chromium/third_party/WebKit/Source/core/loader/NetworkHintsInterface.h
+++ src/chromium/third_party/WebKit/Source/core/loader/NetworkHintsInterface.h
@@ -1,3 +1,14 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: NetworkHintsInterface.h
+// Description: NetworkHintsInterface Classes
+//      Author: Ziming Li
+//     Created: 2019-05-14
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -11,16 +22,10 @@ namespace blink {
 
 class NetworkHintsInterface {
 public:
-    virtual void dnsPrefetchHost(const String&) const = 0;
     virtual void preconnectHost(const KURL&, const CrossOriginAttributeValue) const = 0;
 };
 
 class NetworkHintsInterfaceImpl : public NetworkHintsInterface {
-    void dnsPrefetchHost(const String& host) const override
-    {
-        prefetchDNS(host);
-    }
-
     void preconnectHost(const KURL& host, const CrossOriginAttributeValue crossOrigin) const override
     {
         preconnect(host, crossOrigin);
