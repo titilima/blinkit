7828f1faf792a9439fcdcdb428670af49c76d2fd
titilima <titilima@163.com>
2019-05-14 09:00:21 +0800

    Remove frame tree stuff.

--- src/chromium/third_party/WebKit/Source/core/paint/PaintInvalidationCapableScrollableArea.cpp
+++ src/chromium/third_party/WebKit/Source/core/paint/PaintInvalidationCapableScrollableArea.cpp
@@ -1,3 +1,14 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: PaintInvalidationCapableScrollableArea.cpp
+// Description: PaintInvalidationCapableScrollableArea Class
+//      Author: Ziming Li
+//     Created: 2019-05-14
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -5,7 +16,6 @@
 #include "core/paint/PaintInvalidationCapableScrollableArea.h"
 
 #include "core/frame/Settings.h"
-#include "core/html/HTMLFrameOwnerElement.h"
 #include "core/layout/LayoutBox.h"
 #include "core/layout/LayoutScrollbar.h"
 #include "core/layout/LayoutScrollbarPart.h"
