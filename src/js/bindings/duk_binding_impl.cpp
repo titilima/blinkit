e6ad942009f79aff06f33f4c88d8cd57d3ab59e1
titilima <titilima@163.com>
2019-05-31 23:12:08 +0800

    Common dummy implementation.

---
+++ src/js/bindings/duk_binding_impl.cpp
@@ -0,0 +1,23 @@
+// -------------------------------------------------
+// BlinKit - js Library
+// -------------------------------------------------
+//   File Name: duk_binding_impl.cpp
+// Description: Binding Implementations
+//      Author: Ziming Li
+//     Created: 2019-05-31
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#include "duk_binding_impl.h"
+
+namespace BlinKit {
+namespace Crawler {
+
+duk_ret_t NothingToDo(duk_context *ctx)
+{
+    return 0;
+}
+
+} // namespace Crawler
+} // namespace BlinKit
