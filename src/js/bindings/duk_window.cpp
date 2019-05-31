aa1ac0dea3a769395c82808a124d1c1157d8f384
titilima <titilima@163.com>
2019-05-31 22:17:04 +0800

    Property placeholders.

--- src/js/bindings/duk_window.cpp
+++ src/js/bindings/duk_window.cpp
@@ -14,13 +14,24 @@
 #include "bindings/duk_console.h"
 #include "context/duk_context.h"
 #include "context/prototype_manager.h"
+#include "wrappers/duk.h"
 
 namespace BlinKit {
 
 const char DukWindow::ProtoName[] = "Window";
 MetaData DukWindow::ClassMetaData = { MetaData::Window, nullptr };
 
+namespace Crawler {
+
+} // namespace Crawler
+
 namespace Impl {
+
+static duk_ret_t ClosedGetter(duk_context *ctx)
+{
+    assert(false); // BKTODO:
+    return 1;
+}
 
 static duk_ret_t ConsoleGetter(duk_context *ctx)
 {
@@ -30,8 +41,53 @@ static duk_ret_t ConsoleGetter(duk_context *ctx)
 
 static duk_ret_t ConsoleSetter(duk_context *ctx)
 {
-    assert(false); // Not reached!
+    return Duk::DefaultSetter(ctx, "console");
+}
+
+static duk_ret_t DocumentGetter(duk_context *ctx)
+{
+    assert(false); // BKTODO:
+    return 1;
+}
+
+static duk_ret_t FramesGetter(duk_context *ctx)
+{
+    assert(false); // BKTODO:
+    return 1;
+}
+
+static duk_ret_t FramesSetter(duk_context *ctx)
+{
+    return Duk::DefaultSetter(ctx, "frames");
+}
+
+static duk_ret_t LocationGetter(duk_context *ctx)
+{
+    assert(false); // BKTODO:
+    return 1;
+}
+
+static duk_ret_t LocationSetter(duk_context *ctx)
+{
+    assert(false); // BKTODO:
     return 0;
+}
+
+static duk_ret_t NavigatorGetter(duk_context *ctx)
+{
+    assert(false); // BKTODO:
+    return 1;
+}
+
+static duk_ret_t ParentSetter(duk_context *ctx)
+{
+    return Duk::DefaultSetter(ctx, "parent");
+}
+
+static duk_ret_t WindowGetter(duk_context *ctx)
+{
+    duk_push_global_object(ctx);
+    return 1;
 }
 
 } // namespace Impl
@@ -53,11 +109,21 @@ void DukWindow::RegisterPrototype(duk_context *ctx, PrototypeManager &protos)
 void DukWindow::RegisterPrototypeForCrawler(duk_context *ctx, PrototypeManager &protos)
 {
     static const PrototypeEntry::Property Properties[] = {
-        { "console", Impl::ConsoleGetter, Impl::ConsoleSetter }
+        { "closed",    Impl::ClosedGetter,      nullptr                 },
+        { "console",   Impl::ConsoleGetter,     Impl::ConsoleSetter     },
+        { "document",  Impl::DocumentGetter,    nullptr                 },
+        { "frames",    Impl::FramesGetter,      Impl::FramesSetter      },
+        { "location",  Impl::LocationGetter,    Impl::LocationSetter    },
+        { "navigator", Impl::NavigatorGetter,   nullptr                 },
+        { "parent",    Impl::WindowGetter,      Impl::ParentSetter      },
+        { "self",      Impl::WindowGetter,      nullptr                 },
+        { "top",       Impl::WindowGetter,      nullptr                 },
+        { "window",    Impl::WindowGetter,      nullptr                 },
     };
     const auto worker = [](PrototypeEntry &entry)
     {
         entry.Add(Properties, WTF_ARRAY_LENGTH(Properties));
+        entry.AddObject("external");
     };
     protos.Register(ctx, ProtoName, worker);
 }
