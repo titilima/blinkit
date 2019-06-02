8c37e738d853d26f84b0033e277bfe5bc619e525
titilima <titilima@163.com>
2019-06-02 19:18:39 +0800

    Bindings implementations.

--- src/js/context/duk_context.cpp
+++ src/js/context/duk_context.cpp
@@ -18,6 +18,7 @@
 
 #include "bindings/duk_console.h"
 #include "bindings/duk_document.h"
+#include "bindings/duk_element.h"
 #include "bindings/duk_window.h"
 #include "context/caller_context_impl.h"
 #include "context/function_manager.h"
@@ -321,6 +322,7 @@ void DukContext::RegisterPrototypesForCrawler(void)
     m_prototypeManager->BeginRegisterTransaction(m_context);
     DukConsole::RegisterPrototype(m_context, *m_prototypeManager);
     DukDocument::RegisterPrototypeForCrawler(m_context, *m_prototypeManager);
+    DukElement::RegisterPrototypeForCrawler(m_context, *m_prototypeManager);
     DukWindow::RegisterPrototypeForCrawler(m_context, *m_prototypeManager);
     m_prototypeManager->EndRegisterTransaction(m_context);
 }
