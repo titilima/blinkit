c7aac11564da160cc25b986339249a2794b3f5d7
titilima <titilima@163.com>
2019-06-02 17:03:12 +0800

    Fit for crawler.

--- src/chromium/third_party/WebKit/Source/core/dom/NonDocumentTypeChildNode.h
+++ src/chromium/third_party/WebKit/Source/core/dom/NonDocumentTypeChildNode.h
@@ -1,3 +1,14 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: NonDocumentTypeChildNode.h
+// Description: NonDocumentTypeChildNode Class
+//      Author: Ziming Li
+//     Created: 2019-06-02
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -22,10 +33,12 @@ public:
         return ElementTraversal::nextSibling(node);
     }
 
+#ifndef BLINKIT_CRAWLER_ONLY
     static HTMLSlotElement* assignedSlotForBinding(Node& node)
     {
         return node.assignedSlotForBinding();
     }
+#endif
 };
 
 } // namespace blink
