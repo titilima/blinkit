ddf70ef0158ffa0bd25e9b248378b63ac37375b8
titilima <titilima@163.com>
2019-06-10 16:47:37 +0800

    Remove `crawler.textContentBySelector`.

--- src/js/context/duk_context.cpp
+++ src/js/context/duk_context.cpp
@@ -69,27 +69,6 @@ static duk_ret_t Gather(duk_context *ctx)
     return 0;
 }
 
-static duk_ret_t TextContentBySelector(duk_context *ctx)
-{
-    duk_push_global_object(ctx);
-    DOMWindow *window = DukEventTarget::GetNativeThis<DOMWindow>(ctx);
-
-    DukExceptionState es(ctx, "querySelector", "Document");
-    PassRefPtr<Element> e = window->document()->querySelector(Duk::ToAtomicString(ctx, 0), es);
-    if (es.hadException())
-    {
-        es.throwIfNeeded();
-        return 0;
-    }
-
-    String ret;
-    if (e)
-        ret = e->textContent();
-    Duk::PushString(ctx, ret);
-    duk_trim(ctx, -1);
-    return 1;
-}
-
 } // namespace Crawler
 
 DukContext::DukContext(LocalFrame &frame)
@@ -218,8 +197,6 @@ std::tuple<int, std::string> DukContext::CreateCrawlerObject(const char *script,
 
     duk_push_c_function(m_context, Crawler::Gather, 1);
     duk_put_prop_string(m_context, -2, "gather");
-    duk_push_c_function(m_context, Crawler::TextContentBySelector, 1);
-    duk_put_prop_string(m_context, -2, "textContentBySelector");
 
     m_functionManager = std::make_unique<FunctionManager>(m_context);
     duk_put_prop_string(m_context, -2, StashFields::CrawlerObject);
