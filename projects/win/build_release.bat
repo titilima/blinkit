de97bc7d2e435d2706aacf6726012a83271d14cd
titilima <titilima@163.com>
2019-06-21 17:28:54 +0800

    Add BkRequest & BkCrawler into main solution.

--- projects/win/build_release.bat
+++ projects/win/build_release.bat
@@ -1,9 +1,5 @@
 @REM This file SHOULD BE executed in x64 mode!
 msbuild BlinKit.sln /t:Clean /p:Configuration=Release;Platform=x86
-msbuild BkRequest\BkRequest.vcxproj /t:Clean /p:Configuration=Release;Platform=x86
-msbuild blink_crawler\blink_crawler.vcxproj /t:Clean /p:Configuration=Release;Platform=x86
-msbuild js_crawler\js_crawler.vcxproj /t:Clean /p:Configuration=Release;Platform=x86
-msbuild BkCrawler\BkCrawler.vcxproj /t:Clean /p:Configuration=Release;Platform=x86
 
 msbuild BlinKit\BlinKit.vcxproj /t:Build /p:Configuration=Release;Platform=x86
 msbuild BkRequest\BkRequest.vcxproj /t:Build /p:Configuration=Release;Platform=x86
