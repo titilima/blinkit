d005de4304fc0137a1ca39076b994f8f6cbf6715
titilima <titilima@163.com>
2019-08-06 16:21:59 +0800

    Add comment for pyyaml module.

--- helpers/config_duk.bat
+++ helpers/config_duk.bat
@@ -1,2 +1,5 @@
+REM Please install yaml first:
+REM     pip install pyyaml
+
 cd %~dp0
 python ..\third_party\duktape\tools\configure.py --output-directory ..\src\js\duktape --line-directives
--- helpers/config_duk.sh
+++ helpers/config_duk.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Please install yaml first:
+#     pip install pyyaml
+
 cd $(dirname $0)
 python ../third_party/duktape/tools/configure.py --output-directory ../src/js/duktape
 
