b3e251c92de682af8dfe36a4c15a27d8622d2626
titilima <titilima@163.com>
2019-08-13 12:17:42 +0800

    Update BkCrawler project.

---
+++ src/blinkit/app/apple_app.h
@@ -0,0 +1,27 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: apple_app.h
+// Description: AppleApp Class
+//      Author: Ziming Li
+//     Created: 2019-08-13
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINKIT_APPLE_APP_H
+#define BLINKIT_BLINKIT_APPLE_APP_H
+
+#pragma once
+
+#include "app_impl.h"
+
+namespace BlinKit {
+
+class AppleApp final : public AppImpl
+{
+};
+
+} // namespace BlinKit
+
+#endif // BLINKIT_BLINKIT_APPLE_APP_H
---
+++ src/blinkit/app/apple_app.mm
@@ -0,0 +1,26 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: apple_app.mm
+// Description: AppleApp Class
+//      Author: Ziming Li
+//     Created: 2019-08-13
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#include "apple_app.h"
+
+namespace BlinKit {
+
+void AppImpl::CreateInstance(void)
+{
+    new AppleApp;
+}
+
+void AppImpl::Log(const char *s)
+{
+    NSLog(@"%s", s);
+}
+
+} // namespace BlinKit
