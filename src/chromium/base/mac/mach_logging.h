ce5a3f177fa7481b75d92dacbb141cbd758575e7
titilima <titilima@163.com>
2019-08-13 12:11:33 +0800

    MACH_DCHECK placeholder.

---
+++ src/chromium/base/mac/mach_logging.h
@@ -0,0 +1,21 @@
+// -------------------------------------------------
+// BlinKit - base Library
+// -------------------------------------------------
+//   File Name: mach_logging.h
+// Description: Placeholders for Logging
+//      Author: Ziming Li
+//     Created: 2019-08-13
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BASE_MACH_LOGGING_H
+#define BLINKIT_BASE_MACH_LOGGING_H
+
+#pragma once
+
+#include "base/logging.h"
+
+#define MACH_DCHECK(cond, x)    BKASSERT(cond)
+
+#endif // BLINKIT_BASE_MACH_LOGGING_H
