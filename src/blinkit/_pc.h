c02ea5722e0aedb76fa4e9db41a3ecfe69e50cda
titilima <titilima@163.com>
2019-08-13 11:53:03 +0800

    Build for macOS/iOS.

--- src/blinkit/_pc.h
+++ src/blinkit/_pc.h
@@ -17,13 +17,17 @@
 #include "build/build_config.h"
 
 #if OS_WIN
-
 #   pragma warning(disable: 4244 4291 5033)
-
 #   define _CRT_SECURE_NO_WARNINGS
 #   define NOMINMAX
 #   define WIN32_LEAN_AND_MEAN
+#endif
 
+#if (OS_MACOSX || OS_IOS)
+#   pragma clang diagnostic ignored "-Wcomma"
+#   pragma clang diagnostic ignored "-Wdocumentation"
+#   pragma clang diagnostic ignored "-Winconsistent-missing-override"
+#   pragma clang diagnostic ignored "-Wshorten-64-to-32"
 #endif
 
 #define BLINK_IMPLEMENTATION    1
