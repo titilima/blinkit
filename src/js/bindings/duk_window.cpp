5eb2441ac59e308f007e3e7e183d3979dbf8bc97
titilima <titilima@163.com>
2019-08-23 15:30:15 +0800

    Fix base64 codec.

--- src/js/bindings/duk_window.cpp
+++ src/js/bindings/duk_window.cpp
@@ -75,6 +75,7 @@ static duk_ret_t AToB(duk_context *ctx)
 {
     duk_dup(ctx, 0);
     duk_base64_decode(ctx, -1);
+    duk_buffer_to_string(ctx, -1);
     return 1;
 }
 
@@ -82,6 +83,7 @@ static duk_ret_t BToA(duk_context *ctx)
 {
     duk_dup(ctx, 0);
     duk_base64_encode(ctx, -1);
+    duk_buffer_to_string(ctx, -1);
     return 1;
 }
 
