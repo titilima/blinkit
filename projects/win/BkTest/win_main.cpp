f4cc91e8120ca63c32a597062412235186c8d907
titilima <titilima@163.com>
2019-09-03 22:50:36 +0800

    Reset crawler projects.

--- projects/win/BkTest/win_main.cpp
+++ projects/win/BkTest/win_main.cpp
@@ -4,42 +4,14 @@
 //   File Name: win_main.cpp
 // Description: Application Entry
 //      Author: Ziming Li
-//     Created: 2018-09-18
+//     Created: 2019-09-03
 // -------------------------------------------------
 // Copyright (C) 2018 MingYang Software Technology.
 // -------------------------------------------------
 
-#include "main_frame.h"
-
-CComModule _Module;
-
-static int Run(int nShowCmd)
-{
-    MainFrame *w = MainFrame::CreateInstance();
-    if (nullptr == w)
-        return EXIT_FAILURE;
-
-    w->ShowWindow(nShowCmd);
-    w->UpdateWindow();
-
-    MSG msg;
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-    return msg.wParam;
-}
+#include <Windows.h>
 
 int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE, PTSTR, int nShowCmd)
 {
-    _Module.Init(nullptr, hInstance);
-
-    BkApp *app = BkInitialize(nullptr);
-
-    int r = Run(nShowCmd);
-
-    app->Exit();
-    _Module.Term();
-    return r;
+    return EXIT_SUCCESS;
 }
