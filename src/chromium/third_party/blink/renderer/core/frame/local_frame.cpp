c3325d87002971854baad1f508108ec4af55e3b6
titilima <titilima@163.com>
2019-09-13 19:01:18 +0800

    Frame skeleton implementations.

---
+++ src/chromium/third_party/blink/renderer/core/frame/local_frame.cpp
@@ -0,0 +1,57 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: local_frame.cpp
+// Description: LocalFrame Class
+//      Author: Ziming Li
+//     Created: 2019-09-12
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+/*
+ * Copyright (C) 1998, 1999 Torben Weis <weis@kde.org>
+ *                     1999 Lars Knoll <knoll@kde.org>
+ *                     1999 Antti Koivisto <koivisto@kde.org>
+ *                     2000 Simon Hausmann <hausmann@kde.org>
+ *                     2000 Stefan Schimanski <1Stein@gmx.de>
+ *                     2001 George Staikos <staikos@kde.org>
+ * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011 Apple Inc. All
+ * rights reserved.
+ * Copyright (C) 2005 Alexey Proskuryakov <ap@nypop.com>
+ * Copyright (C) 2008 Nokia Corporation and/or its subsidiary(-ies)
+ * Copyright (C) 2008 Eric Seidel <eric@webkit.org>
+ * Copyright (C) 2008 Google Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "local_frame.h"
+
+#include "third_party/blink/renderer/core/frame/local_frame_client.h"
+
+namespace blink {
+
+LocalFrame::LocalFrame(LocalFrameClient &client, Page *page) : Frame(client, page)
+{
+}
+
+std::unique_ptr<LocalFrame> LocalFrame::Create(LocalFrameClient &client, Page *page)
+{
+    return std::unique_ptr<LocalFrame>(new LocalFrame(client, page));
+}
+
+} // namespace blink
---
+++ src/chromium/third_party/blink/renderer/core/frame/local_frame.h
@@ -0,0 +1,61 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: local_frame.h
+// Description: LocalFrame Class
+//      Author: Ziming Li
+//     Created: 2019-09-12
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+/*
+ * Copyright (C) 1998, 1999 Torben Weis <weis@kde.org>
+ *                     1999-2001 Lars Knoll <knoll@kde.org>
+ *                     1999-2001 Antti Koivisto <koivisto@kde.org>
+ *                     2000-2001 Simon Hausmann <hausmann@kde.org>
+ *                     2000-2001 Dirk Mueller <mueller@kde.org>
+ *                     2000 Stefan Schimanski <1Stein@gmx.de>
+ * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010 Apple Inc. All rights
+ * reserved.
+ * Copyright (C) 2008 Nokia Corporation and/or its subsidiary(-ies)
+ * Copyright (C) 2008 Eric Seidel <eric@webkit.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef BLINKIT_BLINK_LOCAL_FRAME_H
+#define BLINKIT_BLINK_LOCAL_FRAME_H
+
+#pragma once
+
+#include "third_party/blink/renderer/core/frame/frame.h"
+
+namespace blink {
+
+class LocalFrameClient;
+
+class LocalFrame final : public Frame
+{
+public:
+    static std::unique_ptr<LocalFrame> Create(LocalFrameClient &client, Page *page = nullptr);
+private:
+    LocalFrame(LocalFrameClient &client, Page *page);
+};
+
+} // namespace blink
+
+#endif // BLINKIT_BLINK_LOCAL_FRAME_H
