e72e1610606bbc172b40180a3b5ceeb41728a8c2
titilima <titilima@163.com>
2019-09-16 14:24:34 +0800

    Frame loader skeleton.

---
+++ src/blinkit/blink_impl/local_frame_client_impl.cpp
@@ -0,0 +1,16 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: local_frame_client_impl.cpp
+// Description: LocalFrameClientImpl Class
+//      Author: Ziming Li
+//     Created: 2019-09-13
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#include "local_frame_client_impl.h"
+
+namespace BlinKit {
+
+} // namespace BlinKit
---
+++ src/blinkit/blink_impl/local_frame_client_impl.h
@@ -0,0 +1,28 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: local_frame_client_impl.h
+// Description: LocalFrameClientImpl Class
+//      Author: Ziming Li
+//     Created: 2019-09-13
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINKIT_LOCAL_FRAME_CLIENT_IMPL_H
+#define BLINKIT_BLINKIT_LOCAL_FRAME_CLIENT_IMPL_H
+
+#pragma once
+
+#include "third_party/blink/renderer/core/frame/local_frame_client.h"
+
+namespace BlinKit {
+
+class LocalFrameClientImpl : public blink::LocalFrameClient
+{
+public:
+};
+
+} // namespace BlinKit
+
+#endif // BLINKIT_BLINKIT_LOCAL_FRAME_CLIENT_IMPL_H
