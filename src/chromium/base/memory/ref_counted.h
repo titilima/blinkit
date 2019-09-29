b035308520a50c39578b582d2a3e1f65e8c114c0
titilima <titilima@163.com>
2019-09-29 09:32:02 +0800

    Extract scoped_refptr class.

--- src/chromium/base/memory/ref_counted.h
+++ src/chromium/base/memory/ref_counted.h
@@ -26,13 +26,13 @@ public:
     bool HasOneRef(void) const { return 1 == m_refCount; }
     void AddRef(void) const
     {
-#ifdef _DEBUG
+#ifndef NDEBUG
         assert(!m_inDtor);
 #endif
         ++m_refCount;
     }
 protected:
-#ifdef _DEBUG
+#ifndef NDEBUG
     ~RefCountedBase(void)
     {
         assert(m_inDtor); // RefCounted object deleted without calling Release()!
@@ -40,12 +40,12 @@ protected:
 #endif
     bool Release(void) const
     {
-#ifdef _DEBUG
+#ifndef NDEBUG
         assert(!m_inDtor);
 #endif
         if (--m_refCount == 0)
         {
-#ifdef _DEBUG
+#ifndef NDEBUG
             m_inDtor = true;
 #endif
             return true;
@@ -54,8 +54,8 @@ protected:
     }
 private:
     mutable REF_COUNT m_refCount{ 0 };
-#ifdef _DEBUG
-    bool m_inDtor = false;
+#ifndef NDEBUG
+    mutable bool m_inDtor = false;
 #endif
 };
 
@@ -64,7 +64,7 @@ class RefCounted : public RefCountedBase<unsigned>
 {
     DISALLOW_COPY_AND_ASSIGN(RefCounted<T>);
 public:
-    void Release(void) const
+    void Release(void)
     {
         if (RefCountedBase::Release())
             delete static_cast<T *>(this);
@@ -88,130 +88,5 @@ protected:
 };
 
 } // namespace base
-
-template <class T>
-class scoped_refptr
-{
-public:
-    typedef T element_type;
-
-    scoped_refptr(void) : m_ptr(nullptr) {}
-    scoped_refptr(T *p) : m_ptr(p)
-    {
-        if (nullptr != m_ptr)
-            AddRef(m_ptr);
-    }
-
-    // Copy constructor.
-    scoped_refptr(const scoped_refptr<T> &r) : m_ptr(r.m_ptr)
-    {
-        if (nullptr != m_ptr)
-            AddRef(m_ptr);
-    }
-
-    // Copy conversion constructor.
-    template <typename U>
-    scoped_refptr(const scoped_refptr<U> &r) : m_ptr(r.get())
-    {
-        if (nullptr != m_ptr)
-            AddRef(m_ptr);
-    }
-
-    // Move constructor. This is required in addition to the conversion
-    // constructor below in order for clang to warn about pessimizing moves.
-    scoped_refptr(scoped_refptr &&r) : m_ptr(r.get()) { r.m_ptr = nullptr; }
-
-    // Move conversion constructor.
-    template <typename U>
-    scoped_refptr(scoped_refptr<U> &&r) : m_ptr(r.get()) { r.m_ptr = nullptr; }
-
-    ~scoped_refptr(void)
-    {
-        if (nullptr != m_ptr)
-            Release(m_ptr);
-    }
-
-    T* get(void) const { return m_ptr; }
-
-    T& operator*() const {
-        assert(m_ptr != nullptr);
-        return *m_ptr;
-    }
-
-    T* operator->() const {
-        assert(m_ptr != nullptr);
-        return m_ptr;
-    }
-
-    operator bool() const { return nullptr != m_ptr; }
-
-    scoped_refptr<T>& operator=(T *p)
-    {
-        // AddRef first so that self assignment should work
-        if (nullptr != p)
-            AddRef(p);
-        T *old_ptr = m_ptr;
-        m_ptr = p;
-        if (nullptr != old_ptr)
-            Release(old_ptr);
-        return *this;
-    }
-
-    scoped_refptr<T>& operator=(const scoped_refptr<T> &r) { return *this = r.m_ptr; }
-
-    template <typename U>
-    scoped_refptr<T>& operator=(const scoped_refptr<U> &r) { return *this = r.get(); }
-
-    scoped_refptr<T>& operator=(scoped_refptr<T> &&r)
-    {
-        scoped_refptr<T>(std::move(r)).swap(*this);
-        return *this;
-    }
-
-    template <typename U>
-    scoped_refptr<T>& operator=(scoped_refptr<U> &&r)
-    {
-        scoped_refptr<T>(std::move(r)).swap(*this);
-        return *this;
-    }
-
-    void swap(T **pp)
-    {
-        T *p = m_ptr;
-        m_ptr = *pp;
-        *pp = p;
-    }
-
-    void swap(scoped_refptr<T> &r) { swap(&r.m_ptr); }
-
-private:
-    template <typename U> friend class scoped_refptr;
-public:
-    template <typename U>
-    bool operator==(const scoped_refptr<U> &rhs) const { return m_ptr == rhs.get(); }
-
-    template <typename U>
-    bool operator!=(const scoped_refptr<U> &rhs) const { return !operator==(rhs); }
-
-    template <typename U>
-    bool operator<(const scoped_refptr<U> &rhs) const { return m_ptr < rhs.get(); }
-
-protected:
-    T *m_ptr;
-
-private:
-    // Non-inline helpers to allow:
-    //     class Opaque;
-    //     extern template class scoped_refptr<Opaque>;
-    // Otherwise the compiler will complain that Opaque is an incomplete type.
-    static void AddRef(T *ptr);
-    static void Release(T *ptr);
-};
-
-template <typename T>
-void scoped_refptr<T>::AddRef(T *ptr) { ptr->AddRef(); }
-
-template <typename T>
-void scoped_refptr<T>::Release(T *ptr) { ptr->Release(); }
 
 #endif // BLINKIT_BASE_REF_COUNTED_H
---
+++ src/chromium/base/memory/scoped_refptr.h
@@ -0,0 +1,142 @@
+// -------------------------------------------------
+// BlinKit - base Library
+// -------------------------------------------------
+//   File Name: scoped_refptr.h
+// Description: scoped_refptr Class
+//      Author: Ziming Li
+//     Created: 2019-09-29
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BASE_SCOPED_REFPTR_H
+#define BLINKIT_BASE_SCOPED_REFPTR_H
+
+#pragma once
+
+template <class T>
+class scoped_refptr
+{
+public:
+    typedef T element_type;
+
+    scoped_refptr(void) : m_ptr(nullptr) {}
+    scoped_refptr(T *p) : m_ptr(p)
+    {
+        if (nullptr != m_ptr)
+            AddRef(m_ptr);
+    }
+
+    // Copy constructor.
+    scoped_refptr(const scoped_refptr<T> &r) : m_ptr(r.m_ptr)
+    {
+        if (nullptr != m_ptr)
+            AddRef(m_ptr);
+    }
+
+    // Copy conversion constructor.
+    template <typename U>
+    scoped_refptr(const scoped_refptr<U> &r) : m_ptr(r.get())
+    {
+        if (nullptr != m_ptr)
+            AddRef(m_ptr);
+    }
+
+    // Move constructor. This is required in addition to the conversion
+    // constructor below in order for clang to warn about pessimizing moves.
+    scoped_refptr(scoped_refptr &&r) : m_ptr(r.get()) { r.m_ptr = nullptr; }
+
+    // Move conversion constructor.
+    template <typename U>
+    scoped_refptr(scoped_refptr<U> &&r) : m_ptr(r.get()) { r.m_ptr = nullptr; }
+
+    ~scoped_refptr(void)
+    {
+        if (nullptr != m_ptr)
+            Release(m_ptr);
+    }
+
+    T* get(void) const { return m_ptr; }
+
+    T& operator*() const {
+        assert(m_ptr != nullptr);
+        return *m_ptr;
+    }
+
+    T* operator->() const {
+        assert(m_ptr != nullptr);
+        return m_ptr;
+    }
+
+    operator bool() const { return nullptr != m_ptr; }
+
+    scoped_refptr<T>& operator=(T *p)
+    {
+        // AddRef first so that self assignment should work
+        if (nullptr != p)
+            AddRef(p);
+        T *old_ptr = m_ptr;
+        m_ptr = p;
+        if (nullptr != old_ptr)
+            Release(old_ptr);
+        return *this;
+    }
+
+    scoped_refptr<T>& operator=(const scoped_refptr<T> &r) { return *this = r.m_ptr; }
+
+    template <typename U>
+    scoped_refptr<T>& operator=(const scoped_refptr<U> &r) { return *this = r.get(); }
+
+    scoped_refptr<T>& operator=(scoped_refptr<T> &&r)
+    {
+        scoped_refptr<T>(std::move(r)).swap(*this);
+        return *this;
+    }
+
+    template <typename U>
+    scoped_refptr<T>& operator=(scoped_refptr<U> &&r)
+    {
+        scoped_refptr<T>(std::move(r)).swap(*this);
+        return *this;
+    }
+
+    void swap(T **pp)
+    {
+        T *p = m_ptr;
+        m_ptr = *pp;
+        *pp = p;
+    }
+
+    void swap(scoped_refptr<T> &r) { swap(&r.m_ptr); }
+
+private:
+    template <typename U> friend class scoped_refptr;
+public:
+    template <typename U>
+    bool operator==(const scoped_refptr<U> &rhs) const { return m_ptr == rhs.get(); }
+
+    template <typename U>
+    bool operator!=(const scoped_refptr<U> &rhs) const { return !operator==(rhs); }
+
+    template <typename U>
+    bool operator<(const scoped_refptr<U> &rhs) const { return m_ptr < rhs.get(); }
+
+protected:
+    T *m_ptr;
+
+private:
+    // Non-inline helpers to allow:
+    //     class Opaque;
+    //     extern template class scoped_refptr<Opaque>;
+    // Otherwise the compiler will complain that Opaque is an incomplete type.
+    static void AddRef(T *ptr);
+    static void Release(T *ptr);
+};
+
+template <typename T>
+void scoped_refptr<T>::AddRef(T *ptr) { ptr->AddRef(); }
+
+template <typename T>
+void scoped_refptr<T>::Release(T *ptr) { ptr->Release(); }
+
+#endif // BLINKIT_BASE_SCOPED_REFPTR_H
