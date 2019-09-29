b84aab97d6f74f9f3d0ed4afe0dbad5d53410242
titilima <titilima@163.com>
2019-09-29 09:31:34 +0800

    Placeholders for core part.

---
+++ src/chromium/third_party/blink/renderer/core/core_export.h
@@ -0,0 +1,19 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: core_export.h
+// Description: Placeholders for Core Part
+//      Author: Ziming Li
+//     Created: 2019-09-29
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINK_CORE_EXPORT_H
+#define BLINKIT_BLINK_WTF_EXPORT_H
+
+#pragma once
+
+#define CORE_EXPORT
+
+#endif  // BLINKIT_BLINK_CORE_EXPORT_H
