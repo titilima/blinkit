415ea36d793199eec14c676176a6ed085ebb73c1
titilima <titilima@163.com>
2019-09-29 11:39:43 +0800

    Placeholders for WTF part.

---
+++ src/chromium/third_party/blink/renderer/platform/wtf/allocator/partition_allocator.h
@@ -0,0 +1,86 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: partition_allocator.h
+// Description: Placeholders for WTF Part
+//      Author: Ziming Li
+//     Created: 2019-09-29
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINK_PARTITION_ALLOCATOR_H
+#define BLINKIT_BLINK_PARTITION_ALLOCATOR_H
+
+#pragma once
+
+namespace WTF {
+
+class PartitionAllocator
+{
+public:
+    static constexpr bool kIsGarbageCollected = false;
+
+    template <typename T>
+    static size_t QuantizedSize(size_t count)
+    {
+        CHECK_LE(count, MaxElementCountInBackingStore<T>());
+        return count * sizeof(T);
+    }
+    template <typename T>
+    static size_t MaxElementCountInBackingStore(void)
+    {
+        return kGenericMaxDirectMapped / sizeof(T);
+    }
+
+    template <typename T>
+    static T* AllocateInlineVectorBacking(size_t size)
+    {
+        return AllocateVectorBacking<T>(size);
+    }
+    template <typename T>
+    static T* AllocateVectorBacking(size_t size)
+    {
+        return reinterpret_cast<T*>(AllocateBacking(size));
+    }
+    template <typename T>
+    static T* AllocateExpandedVectorBacking(size_t size)
+    {
+        return reinterpret_cast<T*>(AllocateBacking(size));
+    }
+    static inline void FreeInlineVectorBacking(void *address)
+    {
+        FreeVectorBacking(address);
+    }
+    static void FreeVectorBacking(void *address)
+    {
+        free(address);
+    }
+
+    static bool IsAllocationAllowed(void) { return true; }
+    static bool IsObjectResurrectionForbidden(void) { return false; }
+    static bool IsSweepForbidden(void) { return false; }
+
+    static void BackingWriteBarrier(void *) {}
+    template <typename T, typename Traits> static void NotifyNewObject(T *object) {}
+    template <typename T, typename Traits> static void NotifyNewObjects(T *array, size_t len) {}
+    static inline bool ShrinkVectorBacking(void *address, size_t quantizedCurrentSize, size_t quantizedShrunkSize)
+    {
+        // Optimization: if we're downsizing inside the same allocator bucket,
+        // we can skip reallocation.
+        return quantizedCurrentSize == quantizedShrunkSize;
+    }
+private:
+    static void* AllocateBacking(size_t size)
+    {
+        return malloc(size);
+    }
+
+    static const size_t kGenericMaxDirectMapped = 0x7fffffff;
+};
+
+} // namespace WTF
+
+#define USE_ALLOCATOR(ClassName, Allocator)
+
+#endif  // BLINKIT_BLINK_PARTITION_ALLOCATOR_H
