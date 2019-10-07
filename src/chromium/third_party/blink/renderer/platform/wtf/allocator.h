696448781365045bbaef7f4ffc4b9b5d9921bf64
titilima <titilima@163.com>
2019-10-07 12:22:48 +0800

    Update.

--- src/chromium/third_party/blink/renderer/platform/wtf/allocator.h
+++ src/chromium/third_party/blink/renderer/platform/wtf/allocator.h
@@ -14,9 +14,13 @@
 
 #pragma once
 
+#include "third_party/blink/renderer/platform/wtf/allocator/partitions.h"
+
 #define DISALLOW_NEW()                          \
     void* operator new(size_t) = delete;        \
     void* operator new(size_t, void *) = delete
+
+#define STACK_ALLOCATED() DISALLOW_NEW()
 
 #define STATIC_ONLY(Type)                       \
     Type(void) = delete;                        \
@@ -24,5 +28,33 @@
     Type& operator=(const Type &) = delete;     \
     void* operator new(size_t) = delete;        \
     void* operator new(size_t, void *) = delete
+
+#define USING_FAST_MALLOC_INTERNAL(type, typeName)                      \
+public:                                                                 \
+    void* operator new(size_t, void* p) { return p; }                   \
+    void* operator new[](size_t, void* p) { return p; }                 \
+    void* operator new(size_t size) {                                   \
+        return ::WTF::Partitions::FastMalloc(size, typeName);           \
+    }                                                                   \
+    void operator delete(void* p) { ::WTF::Partitions::FastFree(p); }   \
+    void* operator new[](size_t size) {                                 \
+        return ::WTF::Partitions::FastMalloc(size, typeName);           \
+    }                                                                   \
+    void operator delete[](void* p) { ::WTF::Partitions::FastFree(p); } \
+    void* operator new(size_t, NotNullTag, void* location) {            \
+        DCHECK(location);                                               \
+        return location;                                                \
+    }
+
+#define WTF_HEAP_PROFILER_TYPE_NAME(T)  nullptr
+
+#define USING_FAST_MALLOC(type) USING_FAST_MALLOC_INTERNAL(type, WTF_HEAP_PROFILER_TYPE_NAME(type))
+
+enum NotNullTag { NotNull };
+inline void* operator new(size_t, NotNullTag, void *location)
+{
+    DCHECK(location);
+    return location;
+}
 
 #endif // BLINKIT_BLINK_ALLOCATOR_H
