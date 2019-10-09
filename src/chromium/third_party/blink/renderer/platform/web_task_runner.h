f023ab22cad518f8c2c8f1b9ef240d3eb6664a9b
titilima <titilima@163.com>
2019-10-09 11:51:04 +0800

    Task runner helpers.

---
+++ src/chromium/third_party/blink/renderer/platform/web_task_runner.h
@@ -0,0 +1,38 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: web_task_runner.h
+// Description: Task Runner Helpers
+//      Author: Ziming Li
+//     Created: 2019-10-08
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BLINKIT_BLINK_WEB_TASK_RUNNER_H
+#define BLINKIT_BLINK_WEB_TASK_RUNNER_H
+
+#pragma once
+
+namespace blink {
+
+class TaskHandle
+{
+public:
+    ~TaskHandle(void);
+
+    bool IsActive(void) const;
+    void Cancel(void);
+
+    class Runner;
+private:
+    std::shared_ptr<Runner> m_runner;
+};
+
+}  // namespace blink
+
+#endif  // BLINKIT_BLINK_WEB_TASK_RUNNER_H
