40375051ea147e6dd5701d5932325b66d3d0a673
titilima <titilima@163.com>
2019-10-14 11:25:38 +0800

    Placeholders for platform part.

---
+++ src/chromium/third_party/blink/renderer/platform/platform_export.h
@@ -0,0 +1,19 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: platform_export.h
+// Description: Placeholders for Platform Part
+//      Author: Ziming Li
+//     Created: 2019-10-12
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINK_PLATFORM_EXPORT_H
+#define BLINKIT_BLINK_PLATFORM_EXPORT_H
+
+#pragma once
+
+#define PLATFORM_EXPORT
+
+#endif  // BLINKIT_BLINK_PLATFORM_EXPORT_H
