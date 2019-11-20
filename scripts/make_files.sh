70bc652ac8e27fc7d71802c189de8da69c769a47
titilima <titilima@163.com>
2019-11-20 10:45:12 +0800

    Update `gen` part.

--- scripts/make_files.sh
+++ scripts/make_files.sh
@@ -2,5 +2,7 @@
 # Please install PHP and add it to PATH.
 
 cd $(dirname $0)
+php event_type_names.h.php > ../src/chromium/third_party/blink/renderer/core/event_type_names.h
+php event_type_names.cpp.php > ../src/chromium/third_party/blink/renderer/core/event_type_names.cpp
 php html_names.h.php > ../src/chromium/third_party/blink/renderer/core/html_names.h
 php html_names.cpp.php > ../src/chromium/third_party/blink/renderer/core/html_names.cpp
