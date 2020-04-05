64c6d33721c58da805a5e0071f40a5bfb8b73044
titilima <titilima@163.com>
2020-04-05 10:23:26 +0800

    Extract MemberBase class.

--- src/chromium/third_party/blink/renderer/platform/heap/member.h
+++ src/chromium/third_party/blink/renderer/platform/heap/member.h
@@ -17,13 +17,9 @@
 namespace blink {
 
 template<typename T>
-class Member
+class MemberBase
 {
 public:
-    Member(T *ptr = nullptr) : m_rawPtr(ptr) {}
-    Member(const Member &other) : m_rawPtr(other.Get()) {}
-    explicit Member(T &rawObj) : m_rawPtr(&rawObj) {}
-
     T* Get(void) const { return m_rawPtr; }
     T* Release(void)
     {
@@ -37,12 +33,32 @@ public:
     T* operator->() const { return m_rawPtr; }
     bool operator!() const { return !m_rawPtr; }
 
-    void Swap(Member<T> &other)
+    void Swap(MemberBase<T> &other)
     {
         std::swap(m_rawPtr, other.m_rawPtr);
     }
+protected:
+    MemberBase(T *ptr) : m_rawPtr(ptr) {}
+    MemberBase(const MemberBase &other) : m_rawPtr(other.Get()) {}
+    MemberBase(T &rawObj) : m_rawPtr(&rawObj) {}
 private:
     T *m_rawPtr;
+};
+
+template<typename T>
+class Member : public MemberBase<T>
+{
+public:
+    Member(T *ptr = nullptr) : MemberBase<T>(ptr) {}
+    Member(const Member &other) : MemberBase<T>(other) {}
+    explicit Member(T &rawObj) : MemberBase<T>(rawObj) {}
+};
+
+template<typename T>
+class WeakMember : public MemberBase<T>
+{
+public:
+    WeakMember(T *ptr = nullptr) : MemberBase<T>(ptr) {}
 };
 
 } // namespace blink
