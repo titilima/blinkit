b9e4696deac8a1a587d678f17e37f8f6d863a720
titilima <titilima@163.com>
2020-04-05 10:41:34 +0800

    <script> related implementations.

---
+++ src/chromium/third_party/blink/renderer/bindings/core/duk/duk_script_element.cpp
@@ -0,0 +1,66 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: duk_script_element.cpp
+// Description: DukScriptElement class
+//      Author: Ziming Li
+//     Created: 2020-04-01
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#include "duk_script_element.h"
+
+#include "third_party/blink/renderer/bindings/core/duk/duk.h"
+#include "third_party/blink/renderer/core/html_names.h"
+
+using namespace blink;
+
+namespace BlinKit {
+
+const char DukScriptElement::ProtoName[] = "HTMLScriptElement";
+
+namespace Impl {
+
+static duk_ret_t SrcGetter(duk_context *ctx)
+{
+    duk_push_this(ctx);
+    Element *element = DukScriptObject::To<Element>(ctx, -1);
+
+    BkURL ret = element->GetURLAttribute(html_names::kSrcAttr);
+    Duk::PushString(ctx, ret.AsString());
+    return 1;
+}
+
+static duk_ret_t SrcSetter(duk_context *ctx)
+{
+    const AtomicString src = Duk::To<AtomicString>(ctx, 0);
+
+    duk_push_this(ctx);
+    Element *element = DukScriptObject::To<Element>(ctx, -1);
+
+    DukExceptionState exceptionState(ctx);
+    element->setAttribute(html_names::kSrcAttr.LocalName(), src, exceptionState);
+    if (exceptionState.HadException())
+        exceptionState.ThrowIfNeeded();
+    return 0;
+}
+
+} // namespace Impl
+
+void DukScriptElement::FillPrototypeEntryForCrawler(PrototypeEntry &entry)
+{
+    static const PrototypeEntry::Property Properties[] = {
+        { "src", Impl::SrcGetter, Impl::SrcSetter },
+    };
+
+    DukElement::FillPrototypeEntryForCrawler(entry);
+    entry.Add(Properties, std::size(Properties));
+}
+
+void DukScriptElement::RegisterPrototypeForCrawler(PrototypeHelper &helper)
+{
+    helper.Register(ProtoName, FillPrototypeEntryForCrawler);
+}
+
+} // namespace BlinKit
---
+++ src/chromium/third_party/blink/renderer/bindings/core/duk/duk_script_element.h
@@ -0,0 +1,34 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: duk_script_element.h
+// Description: DukScriptElement class
+//      Author: Ziming Li
+//     Created: 2020-04-01
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINK_DUK_SCRIPT_ELEMENT_H
+#define BLINKIT_BLINK_DUK_SCRIPT_ELEMENT_H
+
+#pragma once
+
+#include "third_party/blink/renderer/bindings/core/duk/duk_element.h"
+
+namespace BlinKit {
+
+class DukScriptElement final : public DukElement
+{
+public:
+    static const char ProtoName[];
+    static void RegisterPrototypeForCrawler(PrototypeHelper &helper);
+private:
+    friend class DukElement;
+    static void Create(duk_context *ctx, blink::Element &element);
+    static void FillPrototypeEntryForCrawler(PrototypeEntry &entry);
+};
+
+} // namespace BlinKit
+
+#endif // BLINKIT_BLINK_DUK_SCRIPT_ELEMENT_H
