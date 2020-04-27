d9fa64044019db18ff303755a7b0af09c04a9335
Z <z@mysoft.tech>
2020-04-27 21:38:33 +0800

    Initialize skeleton project.

---
+++ tools/login_helper/win/win_main.cpp
@@ -0,0 +1,17 @@
+// -------------------------------------------------
+// BlinKit - Login Helper
+// -------------------------------------------------
+//   File Name: win_main.cpp
+// Description: Application Entry
+//      Author: Ziming Li
+//     Created: 2020-04-27
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#include <atlbase.h>
+
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE, PTSTR lpCmdLine, int nShowCmd)
+{
+    return EXIT_SUCCESS;
+}
