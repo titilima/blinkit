7e55b1bcb05b9f22196e91487a3cc48c1808f696
Z <z@mysoft.tech>
2020-05-01 11:45:29 +0800

    Cross platform stuff

---
+++ src/blinkit/common/bk_socket.h
@@ -0,0 +1,39 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: bk_socket.h
+// Description: Cross Platform Stuff for Socket
+//      Author: Ziming Li
+//     Created: 2020-04-29
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_COMMON_SOCKET_H
+#define BLINKIT_COMMON_SOCKET_H
+
+#pragma once
+
+#if defined(OS_WIN)
+#   include <WinSock2.h>
+#   include <WS2tcpip.h>
+#elif defined(OS_POSIX)
+#   include <sys/socket.h>
+#else
+#   error Not supported!
+#endif
+
+#ifdef OS_POSIX
+
+typedef int SOCKET;
+
+enum {
+    INVALID_SOCKET = -1,
+    SOCKET_ERROR   = -1
+};
+
+inline int closesocket(SOCKET s) { return close(s); }
+
+#endif // OS_POSIX
+
+#endif // BLINKIT_COMMON_SOCKET_H
