66334bccc1dba46a44f671ba0b9ede37535ef89c
Z <z@mysoft.tech>
2020-06-03 14:01:46 +0800

    EnsureSSLInitialized

---
+++ src/blinkit/login/login_globals.cpp
@@ -0,0 +1,35 @@
+// -------------------------------------------------
+// BlinKit - BkLogin Library
+// -------------------------------------------------
+//   File Name: login_globals.cpp
+// Description: LoginGlobals Class
+//      Author: Ziming Li
+//     Created: 2020-06-03
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#include "login_globals.h"
+
+#include <openssl/ssl.h>
+
+namespace BlinKit {
+
+LoginGlobals LoginGlobals::s_singleton;
+
+LoginGlobals::~LoginGlobals(void)
+{
+}
+
+void LoginGlobals::EnsureSSLInitialized(void)
+{
+    if (!s_singleton.m_sslInitialized)
+    {
+        SSL_library_init();
+        OpenSSL_add_all_algorithms();
+        SSL_load_error_strings();
+        s_singleton.m_sslInitialized = true;
+    }
+}
+
+} // namespace BlinKit
---
+++ src/blinkit/login/login_globals.h
@@ -0,0 +1,35 @@
+// -------------------------------------------------
+// BlinKit - BkLogin Library
+// -------------------------------------------------
+//   File Name: login_globals.h
+// Description: LoginGlobals Class
+//      Author: Ziming Li
+//     Created: 2020-06-03
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BKLOGIN_LOGIN_GLOBALS_H
+#define BLINKIT_BKLOGIN_LOGIN_GLOBALS_H
+
+#pragma once
+
+namespace BlinKit {
+
+class LoginGlobals
+{
+public:
+    ~LoginGlobals(void);
+
+    static void EnsureSSLInitialized(void);
+private:
+    LoginGlobals(void) = default;
+
+    static LoginGlobals s_singleton;
+
+    bool m_sslInitialized = false;
+};
+
+} // namespace BlinKit
+
+#endif // BLINKIT_BKLOGIN_LOGIN_GLOBALS_H
