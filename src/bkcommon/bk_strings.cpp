09c6012f2c8cdb6c713ded7badd754d6e777ecac
Z <z@mysoft.tech>
2020-06-21 16:04:28 +0800

    Extract common strings.

---
+++ src/bkcommon/bk_strings.cpp
@@ -0,0 +1,39 @@
+// -------------------------------------------------
+// BlinKit - BkCommon Library
+// -------------------------------------------------
+//   File Name: bk_strings.h
+// Description: Common Strings
+//      Author: Ziming Li
+//     Created: 2020-06-21
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#include "bk_strings.h"
+
+namespace BlinKit {
+namespace Strings {
+
+const char DefaultUserAgent[] =
+    "Mozilla/5.0 ("
+#if OS_WIN
+    "Windows NT 10.0; Win64; x64"
+#elif OS_MACOSX
+    "Macintosh; Intel Mac OS X 10_14_3"
+#elif OS_LINUX
+    "X11; Linux x86_64"
+#else
+#   error To be implemented!
+#endif
+    ") AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36";
+
+namespace HttpHeader {
+
+const char ContentEncoding[] = "Content-Encoding";
+const char ContentType[] = "Content-Type";
+
+} // namespace HttpHeader
+
+} // namespace Strings
+
+} // namespace BlinKit
---
+++ src/bkcommon/bk_strings.h
@@ -0,0 +1,32 @@
+// -------------------------------------------------
+// BlinKit - BkCommon Library
+// -------------------------------------------------
+//   File Name: bk_strings.h
+// Description: Common Strings
+//      Author: Ziming Li
+//     Created: 2020-06-21
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BKCOMMON_BK_STRINGS_H
+#define BLINKIT_BKCOMMON_BK_STRINGS_H
+
+#pragma once
+
+namespace BlinKit {
+
+namespace Strings {
+
+extern const char DefaultUserAgent[];
+
+namespace HttpHeader {
+extern const char ContentEncoding[];
+extern const char ContentType[];
+} // namespace HttpHeader
+
+} // namespace Strings
+
+} // namespace BlinKit
+
+#endif // BLINKIT_BKCOMMON_BK_STRINGS_H
