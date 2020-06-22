0aeeb685d7eec628827af304f07f7fdebcc94052
Z <z@mysoft.tech>
2020-06-22 11:34:36 +0800

    Update.

---
+++ src/bklogin/_pc.h
@@ -0,0 +1,28 @@
+// -------------------------------------------------
+// BlinKit - BkLogin Library
+// -------------------------------------------------
+//   File Name: _pc.h
+// Description: Pre-compiled Header File
+//      Author: Ziming Li
+//     Created: 2020-06-21
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BKLOGIN__PC_H
+#define BLINKIT_BKLOGIN__PC_H
+
+#pragma once
+
+#include <memory>
+#include <string>
+#include "build/build_config.h"
+
+#if OS_WIN
+#   define WIN32_LEAN_AND_MEAN
+#endif
+
+#include "BlinKit.hpp"
+#include "base/logging.h"
+
+#endif // BLINKIT_BKLOGIN__PC_H
