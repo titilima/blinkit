8f780de757959c51654900adc13c7cbc005fa654
Z <z@mysoft.tech>
2020-06-23 17:15:52 +0800

    Cleanup.

--- projects/posix/BlinKit.mk
+++ projects/posix/BlinKit.mk
@@ -1,17 +1,12 @@
 CrawlerSrc = $(BkRoot)src/blinkit
 CrawlerFlags = -I$(CrawlerSrc) -I$(BkRoot)src/stub/icu -DBLINKIT_CRAWLER_ONLY $(CrFlags) -include _pc.h
-CrawlerObjects = app_constants.o app_impl.o posix_app.o \
-	local_frame_client_impl.o posix_task_runner.o posix_thread.o thread_impl.o url_loader_impl.o \
-	bk_http_header_map.o bk_url.o \
-	crawler_document.o crawler_element.o crawler_script_element.o client_request_task.o client_response_task.o crawler_impl.o login_proxy.o posix_login_proxy.o \
-	curl_request.o request_impl.o response_impl.o \
+CrawlerObjects = app_impl.o posix_app.o \
+	local_frame_client_impl.o posix_task_runner.o posix_thread.o thread_impl.o url_loader_impl.o	\
+	crawler_document.o crawler_element.o crawler_script_element.o crawler_impl.o	\
 	context_impl.o js_value_impl.o \
 	http_loader_task.o loader_task.o \
-	buffer.o controller.o \
 	task_loop.o
 
-app_constants.o: $(CrawlerSrc)/app/app_constants.cpp
-	$(CXX) -c $(CXXFLAGS) $(CrawlerFlags) $< -o $@
 app_impl.o: $(CrawlerSrc)/app/app_impl.cpp
 	$(CXX) -c $(CXXFLAGS) $(CrawlerFlags) $< -o $@
 posix_app.o: $(CrawlerSrc)/app/posix_app.cpp
@@ -28,33 +23,13 @@ thread_impl.o: $(CrawlerSrc)/blink_impl/thread_impl.cpp
 url_loader_impl.o: $(CrawlerSrc)/blink_impl/url_loader_impl.cpp
 	$(CXX) -c $(CXXFLAGS) $(CrawlerFlags) $< -o $@
 
-bk_http_header_map.o: $(CrawlerSrc)/common/bk_http_header_map.cpp
-	$(CXX) -c $(CXXFLAGS) $(CrawlerFlags) $< -o $@
-bk_url.o: $(CrawlerSrc)/common/bk_url.cpp
-	$(CXX) -c $(CXXFLAGS) $(CrawlerFlags) $< -o $@
-
 crawler_document.o: $(CrawlerSrc)/crawler/dom/crawler_document.cpp
 	$(CXX) -c $(CXXFLAGS) $(CrawlerFlags) $< -o $@
 crawler_element.o: $(CrawlerSrc)/crawler/dom/crawler_element.cpp
 	$(CXX) -c $(CXXFLAGS) $(CrawlerFlags) $< -o $@
 crawler_script_element.o: $(CrawlerSrc)/crawler/dom/crawler_script_element.cpp
 	$(CXX) -c $(CXXFLAGS) $(CrawlerFlags) $< -o $@
-client_request_task.o: $(CrawlerSrc)/crawler/login/client_request_task.cpp
-	$(CXX) -c $(CXXFLAGS) $(CrawlerFlags) $< -o $@
-client_response_task.o: $(CrawlerSrc)/crawler/login/client_response_task.cpp
-	$(CXX) -c $(CXXFLAGS) $(CrawlerFlags) $< -o $@
 crawler_impl.o: $(CrawlerSrc)/crawler/crawler_impl.cpp
-	$(CXX) -c $(CXXFLAGS) $(CrawlerFlags) $< -o $@
-login_proxy.o: $(CrawlerSrc)/crawler/login_proxy.cpp
-	$(CXX) -c $(CXXFLAGS) $(CrawlerFlags) $< -o $@
-posix_login_proxy.o: $(CrawlerSrc)/crawler/posix_login_proxy.cpp
-	$(CXX) -c $(CXXFLAGS) $(CrawlerFlags) $< -o $@
-
-curl_request.o: $(CrawlerSrc)/http/curl_request.cpp
-	$(CXX) -c $(CXXFLAGS) $(CrawlerFlags) $< -o $@
-request_impl.o: $(CrawlerSrc)/http/request_impl.cpp
-	$(CXX) -c $(CXXFLAGS) $(CrawlerFlags) $< -o $@
-response_impl.o: $(CrawlerSrc)/http/response_impl.cpp
 	$(CXX) -c $(CXXFLAGS) $(CrawlerFlags) $< -o $@
 
 context_impl.o: $(CrawlerSrc)/js/context_impl.cpp
@@ -65,11 +40,6 @@ js_value_impl.o: $(CrawlerSrc)/js/js_value_impl.cpp
 http_loader_task.o: $(CrawlerSrc)/loader_tasks/http_loader_task.cpp
 	$(CXX) -c $(CXXFLAGS) $(CrawlerFlags) $< -o $@
 loader_task.o: $(CrawlerSrc)/loader_tasks/loader_task.cpp
-	$(CXX) -c $(CXXFLAGS) $(CrawlerFlags) $< -o $@
-
-buffer.o: $(CrawlerSrc)/misc/buffer.cpp
-	$(CXX) -c $(CXXFLAGS) $(CrawlerFlags) $< -o $@
-controller.o: $(CrawlerSrc)/misc/controller.cpp
 	$(CXX) -c $(CXXFLAGS) $(CrawlerFlags) $< -o $@
 
 task_loop.o: $(CrawlerSrc)/posix/task_loop.cpp
