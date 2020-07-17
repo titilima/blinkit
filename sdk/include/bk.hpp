e44697a285d2ea315a4bee54fa208f42897520e8
Z <z@mysoft.tech>
2020-07-17 14:04:07 +0800

    New C++ helpers.

---
+++ sdk/include/bk.hpp
@@ -0,0 +1,251 @@
+// -------------------------------------------------
+// BlinKit SDK
+// -------------------------------------------------
+//   File Name: bk.hpp
+//      Author: Ziming Li
+//     Created: 2020-07-16
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_SDK_BK_HPP
+#define BLINKIT_SDK_BK_HPP
+
+#pragma once
+
+#include <cassert>
+#include <functional>
+#include <string>
+#include <string_view>
+#include <vector>
+#include "bk_crawler.h"
+#include "bk_http.h"
+#include "bk_js.h"
+
+namespace bk {
+
+template <typename C>
+class buffer_adaptor
+{
+public:
+    buffer_adaptor(C &data)
+    {
+        m_buffer.Allocator = Alloc;
+        m_buffer.UserData = &data;
+    }
+    operator BkBuffer* (void) const
+    {
+        BkBuffer *buf = const_cast<BkBuffer *>(&m_buffer);
+        return buf;
+    }
+private:
+    static void* BKAPI Alloc(size_t size, void *user_data)
+    {
+        C *c = reinterpret_cast<C *>(user_data);
+        c->resize(size / sizeof(typename C::value_type));
+        return const_cast<typename C::value_type *>(c->data());
+    }
+    BkBuffer m_buffer;
+};
+
+template <typename CharType>
+inline buffer_adaptor<std::basic_string<CharType>> make_buffer(std::basic_string<CharType> &s)
+{
+    return buffer_adaptor<std::basic_string<CharType>>(s);
+}
+
+inline buffer_adaptor<std::vector<unsigned char>> make_buffer(std::vector<unsigned char> &v)
+{
+    return buffer_adaptor<std::vector<unsigned char>>(v);
+}
+
+template <class T, typename Client>
+class client_root_impl
+{
+protected:
+    Client get_client(void)
+    {
+        Client client = { 0 };
+        client.SizeOfStruct = sizeof(Client);
+        client.UserData = static_cast<T *>(this);
+        adjust_raw_client(client);
+        return client;
+    }
+    static T* get_client_root(void *p)
+    {
+        return reinterpret_cast<T *>(p);
+    }
+private:
+    virtual void adjust_raw_client(Client &) const {}
+};
+
+template <class T, class ClientRoot>
+class client_impl : public ClientRoot
+{
+protected:
+    static T* get_this(void *p)
+    {
+        return static_cast<T *>(ClientRoot::get_client_root(p));
+    }
+};
+
+class crawler_client_root : public client_root_impl<crawler_client_root, BkCrawlerClient>
+{
+public:
+    BkCrawler create_crawler(void)
+    {
+        BkCrawlerClient client = get_client();
+        return BkCreateCrawler(&client);
+    }
+protected:
+    void adjust_raw_client(BkCrawlerClient &client) const override
+    {
+        client.DocumentReady = document_ready_callback;
+        client.Error = error_callback;
+    }
+    virtual bool get_crawler_config(int cfg, std::string &dst) const
+    {
+        switch (cfg)
+        {
+            case BK_CFG_REQUEST_PROXY:
+                // Leave it empty to apply system proxy.
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+private:
+    virtual void document_ready(void) = 0;
+    virtual void on_crawler_error(int code, const char *url)
+    {
+        assert(false); // TODO: Process error!
+    }
+
+    static void BKAPI document_ready_callback(void *p)
+    {
+        get_client_root(p)->document_ready();
+    }
+    static bool_t BKAPI get_config_callback(int cfg, BkBuffer *dst, void *p)
+    {
+        std::string s;
+        if (!get_client_root(p)->get_crawler_config(cfg, s))
+            return false;
+        BkSetBufferData(dst, s.data(), s.length());
+        return true;
+    }
+    static void BKAPI error_callback(int code, const char *url, void *p)
+    {
+        get_client_root(p)->on_crawler_error(code, url);
+    }
+};
+
+class request_client_root : public client_root_impl<request_client_root, BkRequestClient>
+{
+public:
+    BkRequest create_request(const char *url)
+    {
+        BkRequestClient client = get_client();
+        return BkCreateRequest(url, &client);
+    }
+protected:
+    void adjust_raw_client(BkRequestClient &client) const override
+    {
+        client.RequestComplete = request_complete_callback;
+        client.RequestFailed = request_failed_callback;
+    }
+private:
+    virtual void request_complete(BkResponse response) = 0;
+    virtual void on_request_failed(int code)
+    {
+        assert(false); // TODO: Process error!
+    }
+
+    static void BKAPI request_complete_callback(BkResponse response, void *p)
+    {
+        get_client_root(p)->request_complete(response);
+    }
+    static void BKAPI request_failed_callback(int code, void *p)
+    {
+        get_client_root(p)->on_request_failed(code);
+    }
+};
+
+template <class T>
+class crawler_client_impl : public client_impl<T, crawler_client_root>
+{
+};
+
+template <class T>
+class request_client_impl : public client_impl<T, request_client_root>
+{
+};
+
+class js_value
+{
+public:
+    js_value(BkJSValue val = nullptr) : m_val(val) {}
+    ~js_value(void)
+    {
+        if (nullptr != m_val)
+            BkReleaseValue(m_val);
+    }
+
+    BkJSValue* operator&() { return &m_val; }
+
+    std::string to_string(void) const
+    {
+        std::string ret;
+        if (nullptr != m_val)
+            BkGetValueAsString(m_val, make_buffer(ret));
+        return ret;
+    }
+private:
+    BkJSValue m_val;
+};
+
+class js_function
+{
+public:
+    static js_function* prepare(BkCrawler crawler, int ctx, const char *name)
+    {
+        js_function *ret = nullptr;
+        BkJSContext js_ctx = BkGetScriptContextFromCrawler(crawler);
+        BkJSCallerContext fn_ctx = BkPrepareFunctionCall(js_ctx, ctx, name);
+        if (nullptr != fn_ctx)
+            ret = new js_function(fn_ctx);
+        return ret;
+    }
+
+    class arg_list
+    {
+    public:
+        void push(const std::string_view &s)
+        {
+            BkPushStringPiece(m_ctx, s.data(), s.length());
+        }
+    private:
+        friend class js_function;
+        arg_list(BkJSCallerContext ctx) : m_ctx(ctx) {}
+
+        BkJSCallerContext m_ctx;
+    };
+    typedef std::function<void(arg_list &)> args_callback;
+
+    int call(const args_callback &cb, BkJSValue *ret = nullptr)
+    {
+        arg_list args(m_ctx);
+        cb(args);
+        int r = BkCallFunction(m_ctx, ret);
+        delete this;
+        return r;
+    }
+private:
+    js_function(BkJSCallerContext ctx) : m_ctx(ctx) {}
+
+    BkJSCallerContext m_ctx;
+};
+
+} // namespace bk
+
+#endif // BLINKIT_SDK_BK_HPP
