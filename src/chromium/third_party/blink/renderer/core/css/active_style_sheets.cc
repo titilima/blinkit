4155417bb41d7dd0deeb09a53d50f4b69b0d5dfe
Z <z@mysoft.tech>
2020-08-05 11:24:50 +0800

    Update.

--- src/chromium/third_party/blink/renderer/core/css/active_style_sheets.cc
+++ src/chromium/third_party/blink/renderer/core/css/active_style_sheets.cc
@@ -1,3 +1,14 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: active_style_sheets.cc
+// Description: Active Style Sheets Helpers
+//      Author: Ziming Li
+//     Created: 2020-08-05
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2016 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
