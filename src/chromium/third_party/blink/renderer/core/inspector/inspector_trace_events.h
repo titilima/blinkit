784dca9bb7abbb4a1404b478a95e3d7c67ba1ae5
Z <z@mysoft.tech>
2020-08-28 13:03:34 +0800

    Update placeholders.

---
+++ src/chromium/third_party/blink/renderer/core/inspector/inspector_trace_events.h
@@ -0,0 +1,61 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: inspector_trace_events.h
+// Description: Placeholders for Inspector
+//      Author: Ziming Li
+//     Created: 2020-08-28
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINK_INSPECTOR_TRACE_EVENTS_H
+#define BLINKIT_BLINK_INSPECTOR_TRACE_EVENTS_H
+
+#pragma once
+
+namespace blink {
+
+typedef int LayoutInvalidationReasonForTracing;
+namespace LayoutInvalidationReason {
+
+enum {
+    kUnknown,
+    kSizeChanged,
+    kAncestorMoved,
+    kStyleChange,
+    kDomChanged,
+    kTextChanged,
+    kPrintingChanged,
+    kAttributeChanged,
+    kColumnsChanged,
+    kChildAnonymousBlockChanged,
+    kAnonymousBlockChange,
+    kFullscreen,
+    kChildChanged,
+    kListValueChange,
+    kImageChanged,
+    kLineBoxesChanged,
+    kSliderValueChanged,
+    kAncestorMarginCollapsing,
+    kFieldsetChanged,
+    kTextAutosizing,
+    kSvgResourceInvalidated,
+    kFloatDescendantChanged,
+    kCountersChanged,
+    kGridChanged,
+    kMenuOptionsChanged,
+    kRemovedFromLayout,
+    kAddedToLayout,
+    kTableChanged,
+    kPaddingChanged,
+    kTextControlChanged,
+    kSvgChanged,
+    kScrollbarChanged
+};
+
+} // namespace LayoutInvalidationReason
+
+} // namespace blink
+
+#endif // BLINKIT_BLINK_INSPECTOR_TRACE_EVENTS_H
