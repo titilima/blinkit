6b55c921f4014fe363988293d6b557983afbf65b
Z <z@mysoft.tech>
2020-08-30 13:25:19 +0800

    Update placeholders.

---
+++ src/chromium/third_party/blink/renderer/core/animation/effect_timing.h
@@ -0,0 +1,25 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: effect_timing.h
+// Description: EffectTiming Class
+//      Author: Ziming Li
+//     Created: 2020-08-30
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINK_EFFECT_TIMING_H
+#define BLINKIT_BLINK_EFFECT_TIMING_H
+
+#pragma once
+
+namespace blink {
+
+class EffectTiming
+{
+};
+
+} // namespace blink
+
+#endif // BLINKIT_BLINK_EFFECT_TIMING_H
