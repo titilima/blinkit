dd03ca26db7d437f3f78af2fb07a780032df03a7
Z <z@mysoft.tech>
2020-09-03 09:55:09 +0800

    Rollback.

--- src/chromium/third_party/blink/renderer/core/css/cssom/css_style_value.h
+++ src/chromium/third_party/blink/renderer/core/css/cssom/css_style_value.h
@@ -1,14 +1,3 @@
-// -------------------------------------------------
-// BlinKit - blink Library
-// -------------------------------------------------
-//   File Name: css_style_value.h
-// Description: CSSStyleValue Class
-//      Author: Ziming Li
-//     Created: 2020-08-29
-// -------------------------------------------------
-// Copyright (C) 2020 MingYang Software Technology.
-// -------------------------------------------------
-
 // Copyright 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -16,8 +5,8 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_CSS_CSSOM_CSS_STYLE_VALUE_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_CSSOM_CSS_STYLE_VALUE_H_
 
-#include <optional>
 #include "base/macros.h"
+#include "base/optional.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/css/css_value.h"
 #include "third_party/blink/renderer/core/css_property_names.h"
