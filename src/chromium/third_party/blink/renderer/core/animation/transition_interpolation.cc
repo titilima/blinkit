1179f7490a35728478f422947515f10772a72854
Z <z@mysoft.tech>
2020-09-08 17:09:34 +0800

    Build.

--- src/chromium/third_party/blink/renderer/core/animation/transition_interpolation.cc
+++ src/chromium/third_party/blink/renderer/core/animation/transition_interpolation.cc
@@ -1,3 +1,14 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: transition_interpolation.cc
+// Description: TransitionInterpolation Class
+//      Author: Ziming Li
+//     Created: 2020-09-08
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -46,11 +57,14 @@ NonInterpolableValue* TransitionInterpolation::CurrentNonInterpolableValue()
 }
 
 void TransitionInterpolation::Apply(StyleResolverState& state) const {
+  ASSERT(false); // BKTODO:
+#if 0
   CSSInterpolationTypesMap map(state.GetDocument().GetPropertyRegistry(),
                                state.GetDocument());
   CSSInterpolationEnvironment environment(map, state, nullptr);
   type_.Apply(CurrentInterpolableValue(), CurrentNonInterpolableValue(),
               environment);
+#endif
 }
 
 std::unique_ptr<TypedInterpolationValue>
