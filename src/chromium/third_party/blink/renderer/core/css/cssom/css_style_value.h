0975fe169e3f29dffcef3bd97e437951011d43d6
Z <z@mysoft.tech>
2020-09-11 13:00:48 +0800

    Build.

--- src/chromium/third_party/blink/renderer/core/css/cssom/css_style_value.h
+++ src/chromium/third_party/blink/renderer/core/css/cssom/css_style_value.h
@@ -1,3 +1,14 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: css_style_value.h
+// Description: CSSStyleValue Class
+//      Author: Ziming Li
+//     Created: 2020-09-11
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -81,6 +92,12 @@ class CORE_EXPORT CSSStyleValue : public ScriptWrappable {
   CSSStyleValue() = default;
 
  private:
+  GCType GetGCType(void) const final
+  {
+    ASSERT(false); // BKTODO:
+    return GC_MANUAL;
+  }
+
   String css_text_;
   DISALLOW_COPY_AND_ASSIGN(CSSStyleValue);
 };
