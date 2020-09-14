63d2eb4d28f1e0290170602773f88a789a6496e4
Z <z@mysoft.tech>
2020-09-14 10:30:59 +0800

    Import from Chromium.

---
+++ src/chromium/third_party/blink/renderer/core/css/cssom/css_numeric_type.h
@@ -0,0 +1,25 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: css_numeric_type.h
+// Description: CSSNumericType Class
+//      Author: Ziming Li
+//     Created: 2020-09-14
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINK_CSS_NUMERIC_TYPE_H
+#define BLINKIT_BLINK_CSS_NUMERIC_TYPE_H
+
+#pragma once
+
+namespace blink {
+
+class CSSNumericType
+{
+};
+
+} // namespace blink
+
+#endif // BLINKIT_BLINK_CSS_NUMERIC_TYPE_H
