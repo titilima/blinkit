e370f67631823a907fa6e0de123e004e98136f5c
Z <z@mysoft.tech>
2020-09-15 17:36:59 +0800

    Build.

--- src/chromium/third_party/blink/renderer/core/css/cssom/css_skew_y.cc
+++ src/chromium/third_party/blink/renderer/core/css/cssom/css_skew_y.cc
@@ -1,3 +1,14 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: css_skew_y.cc
+// Description: CSSSkewY Class
+//      Author: Ziming Li
+//     Created: 2020-09-15
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -9,7 +20,7 @@
 #include "third_party/blink/renderer/core/css/cssom/css_numeric_value.h"
 #include "third_party/blink/renderer/core/css/cssom/css_style_value.h"
 #include "third_party/blink/renderer/core/css/cssom/css_unit_value.h"
-#include "third_party/blink/renderer/core/geometry/dom_matrix.h"
+// BKTODO: #include "third_party/blink/renderer/core/geometry/dom_matrix.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 
 namespace blink {
@@ -54,9 +65,13 @@ CSSSkewY* CSSSkewY::FromCSSValue(const CSSFunctionValue& value) {
 DOMMatrix* CSSSkewY::toMatrix(ExceptionState&) const {
   CSSUnitValue* ay = ay_->to(CSSPrimitiveValue::UnitType::kRadians);
   DCHECK(ay);
+  ASSERT(false); // BKTODO:
+  return nullptr;
+#if 0
   DOMMatrix* result = DOMMatrix::Create();
   result->skewYSelf(ay->value());
   return result;
+#endif
 }
 
 const CSSFunctionValue* CSSSkewY::ToCSSValue() const {
