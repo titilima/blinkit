8bca51630ba5bffd60160794b97de8502bac7a05
Z <z@mysoft.tech>
2020-09-15 18:15:45 +0800

    Build.

--- src/chromium/third_party/blink/renderer/core/css/cssom/css_style_image_value.cc
+++ src/chromium/third_party/blink/renderer/core/css/cssom/css_style_image_value.cc
@@ -1,3 +1,14 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: css_style_image_value.cc
+// Description: CSSStyleImageValue Class
+//      Author: Ziming Li
+//     Created: 2020-09-15
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2016 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -33,10 +44,12 @@ double CSSStyleImageValue::intrinsicRatio(bool& is_null) const {
   return static_cast<double>(size.value().Width()) / size.value().Height();
 }
 
+#if 0 // BKTODO:
 FloatSize CSSStyleImageValue::ElementSize(
     const FloatSize& default_object_size) const {
   bool not_used;
   return FloatSize(intrinsicWidth(not_used), intrinsicHeight(not_used));
 }
+#endif
 
 }  // namespace blink
