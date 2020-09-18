be812fe80b14fc336cbdadfb3904cb2aed0dab8b
Z <z@mysoft.tech>
2020-09-18 12:14:41 +0800

    Update placeholders.

---
+++ src/chromium/third_party/blink/renderer/core/css/font_face_set_load_event_init.h
@@ -0,0 +1,25 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: font_face_set_load_event_init.h
+// Description: FontFaceSetLoadEventInit Class
+//      Author: Ziming Li
+//     Created: 2020-09-18
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINK_FONT_FACE_SET_LOAD_EVENT_INIT_H
+#define BLINKIT_BLINK_FONT_FACE_SET_LOAD_EVENT_INIT_H
+
+#pragma once
+
+namespace blink {
+
+class FontFaceSetLoadEventInit
+{
+};
+
+} // namespace blink
+
+#endif // BLINKIT_BLINK_FONT_FACE_SET_LOAD_EVENT_INIT_H
