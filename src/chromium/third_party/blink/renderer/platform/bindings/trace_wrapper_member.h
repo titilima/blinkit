6d78e3eef8732c4c5646b2d467af184aa3019e5b
Z <z@mysoft.tech>
2020-09-18 09:18:45 +0800

    Update placeholders.

---
+++ src/chromium/third_party/blink/renderer/platform/bindings/trace_wrapper_member.h
@@ -0,0 +1,19 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: trace_wrapper_member.h
+// Description: Placeholder for `platform` Part
+//      Author: Ziming Li
+//     Created: 2020-09-18
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINK_TRACE_WRAPPER_MEMBER_H
+#define BLINKIT_BLINK_TRACE_WRAPPER_MEMBER_H
+
+#pragma once
+
+#include "third_party/blink/renderer/platform/heap/heap_allocator.h"
+
+#endif // BLINKIT_BLINK_TRACE_WRAPPER_MEMBER_H
