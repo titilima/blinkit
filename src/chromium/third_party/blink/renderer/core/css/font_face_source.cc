c08df5454221dd532aad0220dedd067de1f891d0
Z <z@mysoft.tech>
2020-09-18 22:15:19 +0800

    Build.Build.

--- src/chromium/third_party/blink/renderer/core/css/font_face_source.cc
+++ src/chromium/third_party/blink/renderer/core/css/font_face_source.cc
@@ -1,3 +1,14 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: font_face_source.cc
+// Description: FontFaceSource Class
+//      Author: Ziming Li
+//     Created: 2020-09-18
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -14,7 +25,11 @@ FontFaceSet* FontFaceSource::fonts(Document& document) {
 }
 
 FontFaceSet* FontFaceSource::fonts(WorkerGlobalScope& worker) {
+  ASSERT(false); // BKTODO:
+  return nullptr;
+#if 0
   return FontFaceSetWorker::From(worker);
+#endif
 }
 
 }  // namespace blink
