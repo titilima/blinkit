9837c4ed42d8249abf533a8d45af261bb58bf9dc
Z <z@mysoft.tech>
2020-09-20 14:15:49 +0800

    Build.

--- src/chromium/third_party/blink/renderer/core/css/media_values_initial_viewport.cc
+++ src/chromium/third_party/blink/renderer/core/css/media_values_initial_viewport.cc
@@ -1,3 +1,14 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: media_values_initial_viewport.cc
+// Description: MediaValuesInitialViewport Class
+//      Author: Ziming Li
+//     Created: 2020-09-20
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2016 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -19,13 +30,21 @@ MediaValuesInitialViewport::MediaValuesInitialViewport(LocalFrame& frame)
     : MediaValuesDynamic(&frame) {}
 
 double MediaValuesInitialViewport::ViewportWidth() const {
+  ASSERT(false); // BKTODO:
+  return 0;
+#if 0
   DCHECK(frame_->View());
   return frame_->View()->InitialViewportWidth();
+#endif
 }
 
 double MediaValuesInitialViewport::ViewportHeight() const {
+  ASSERT(false); // BKTODO:
+  return 0;
+#if 0
   DCHECK(frame_->View());
   return frame_->View()->InitialViewportHeight();
+#endif
 }
 
 }  // namespace blink
