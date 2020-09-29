b8a88d256ef66cfc1de44cf93f9a4446184b80a0
Z <z@mysoft.tech>
2020-09-29 11:24:23 +0800

    Build.

--- src/chromium/third_party/blink/renderer/core/editing/state_machines/text_segmentation_machine_state.cc
+++ src/chromium/third_party/blink/renderer/core/editing/state_machines/text_segmentation_machine_state.cc
@@ -1,3 +1,14 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: text_segmentation_machine_state.cc
+// Description: TextSegmentationMachineState
+//      Author: Ziming Li
+//     Created: 2020-09-29
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2016 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -15,8 +26,8 @@ std::ostream& operator<<(std::ostream& os, TextSegmentationMachineState state) {
   };
 
   auto* const* const it = std::begin(kTexts) + static_cast<size_t>(state);
-  DCHECK_GE(it, std::begin(kTexts)) << "Unknown state value";
-  DCHECK_LT(it, std::end(kTexts)) << "Unknown state value";
+  DCHECK_GE(it, std::begin(kTexts)); // Unknown state value
+  DCHECK_LT(it, std::end(kTexts)); // Unknown state value
   return os << *it;
 }
 
