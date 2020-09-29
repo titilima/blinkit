12760de4d4a203780179ec3fc92f960310a5c886
Z <z@mysoft.tech>
2020-09-29 11:27:51 +0800

    Build.

--- src/chromium/third_party/blink/renderer/core/editing/text_affinity.cc
+++ src/chromium/third_party/blink/renderer/core/editing/text_affinity.cc
@@ -1,3 +1,14 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: text_affinity.cc
+// Description: TextAffinity
+//      Author: Ziming Li
+//     Created: 2020-09-29
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2016 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -5,7 +16,7 @@
 #include "third_party/blink/renderer/core/editing/text_affinity.h"
 
 #include <ostream>  // NOLINT
-#include "third_party/blink/public/web/web_ax_enums.h"
+// BKTODO: #include "third_party/blink/public/web/web_ax_enums.h"
 #include "third_party/blink/renderer/platform/wtf/assertions.h"
 
 namespace blink {
