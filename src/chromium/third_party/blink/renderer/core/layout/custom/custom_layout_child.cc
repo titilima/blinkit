b32a8fce575e6ba7a48a1b171f06b1a557efea91
Z <z@mysoft.tech>
2020-09-29 21:47:13 +0800

    Build.

--- src/chromium/third_party/blink/renderer/core/layout/custom/custom_layout_child.cc
+++ src/chromium/third_party/blink/renderer/core/layout/custom/custom_layout_child.cc
@@ -1,19 +1,33 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: custom_layout_child.cc
+// Description: CustomLayoutChild Class
+//      Author: Ziming Li
+//     Created: 2020-09-29
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "third_party/blink/renderer/core/layout/custom/custom_layout_child.h"
 
-#include "third_party/blink/renderer/bindings/core/v8/serialization/serialized_script_value.h"
+// BKTODO: #include "third_party/blink/renderer/bindings/core/v8/serialization/serialized_script_value.h"
 #include "third_party/blink/renderer/core/css/cssom/prepopulated_computed_style_property_map.h"
+#if 0 // BKTODO:
 #include "third_party/blink/renderer/core/layout/custom/css_layout_definition.h"
 #include "third_party/blink/renderer/core/layout/custom/custom_layout_fragment_request.h"
+#endif
 #include "third_party/blink/renderer/core/layout/layout_box.h"
 
 namespace blink {
 
 CustomLayoutChild::CustomLayoutChild(const CSSLayoutDefinition& definition,
                                      LayoutBox* box)
+#if 0 // BKTODO:
     : box_(box),
       style_map_(new PrepopulatedComputedStylePropertyMap(
           box->GetDocument(),
@@ -21,7 +35,13 @@ CustomLayoutChild::CustomLayoutChild(const CSSLayoutDefinition& definition,
           box->GetNode(),
           definition.ChildNativeInvalidationProperties(),
           definition.ChildCustomInvalidationProperties())) {}
+#else
+    : box_(box) {
+  ASSERT(false); // BKTODO:
+}
+#endif
 
+#if 0 // BKTODO:
 CustomLayoutFragmentRequest* CustomLayoutChild::layoutNextFragment(
     ScriptState* script_state,
     const CustomLayoutConstraintsOptions& options,
@@ -44,10 +64,6 @@ CustomLayoutFragmentRequest* CustomLayoutChild::layoutNextFragment(
   return new CustomLayoutFragmentRequest(this, options,
                                          std::move(constraint_data));
 }
-
-void CustomLayoutChild::Trace(blink::Visitor* visitor) {
-  visitor->Trace(style_map_);
-  ScriptWrappable::Trace(visitor);
-}
+#endif
 
 }  // namespace blink
