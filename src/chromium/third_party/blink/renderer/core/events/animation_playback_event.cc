61024b2b25e26e7de7c8d2fb8dff44c04f567319
Z <z@mysoft.tech>
2020-09-29 15:29:58 +0800

    Build.

--- src/chromium/third_party/blink/renderer/core/events/animation_playback_event.cc
+++ src/chromium/third_party/blink/renderer/core/events/animation_playback_event.cc
@@ -1,10 +1,21 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: animation_playback_event.cc
+// Description: AnimationPlaybackEvent Class
+//      Author: Ziming Li
+//     Created: 2020-09-29
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "third_party/blink/renderer/core/events/animation_playback_event.h"
 
-#include "third_party/blink/renderer/core/event_names.h"
+// BKTODO: #include "third_party/blink/renderer/core/event_names.h"
 
 namespace blink {
 
@@ -18,12 +29,18 @@ AnimationPlaybackEvent::AnimationPlaybackEvent(const AtomicString& type,
 AnimationPlaybackEvent::AnimationPlaybackEvent(
     const AtomicString& type,
     const AnimationPlaybackEventInit& initializer)
+#if 0 // BKTODO:
     : Event(type, initializer) {
   if (initializer.hasCurrentTime())
     current_time_ = initializer.currentTime();
   if (initializer.hasTimelineTime())
     timeline_time_ = initializer.timelineTime();
 }
+#else
+{
+  ASSERT(false); // BKTODO:
+}
+#endif
 
 AnimationPlaybackEvent::~AnimationPlaybackEvent() = default;
 
@@ -38,11 +55,11 @@ double AnimationPlaybackEvent::timelineTime(bool& is_null) const {
 }
 
 const AtomicString& AnimationPlaybackEvent::InterfaceName() const {
+  ASSERT(false); // BKTODO:
+  return g_null_atom;
+#if 0
   return EventNames::AnimationPlaybackEvent;
-}
-
-void AnimationPlaybackEvent::Trace(blink::Visitor* visitor) {
-  Event::Trace(visitor);
+#endif
 }
 
 }  // namespace blink
