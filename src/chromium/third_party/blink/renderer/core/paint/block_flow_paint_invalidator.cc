d700536efd6500cbb4c5c90f25f279ce4d5dc984
Z <z@mysoft.tech>
2020-10-05 13:49:21 +0800

    Build.

--- src/chromium/third_party/blink/renderer/core/paint/block_flow_paint_invalidator.cc
+++ src/chromium/third_party/blink/renderer/core/paint/block_flow_paint_invalidator.cc
@@ -1,3 +1,14 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: block_flow_paint_invalidator.cc
+// Description: BlockFlowPaintInvalidator Class
+//      Author: Ziming Li
+//     Created: 2020-10-05
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2016 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -18,6 +29,8 @@ void BlockFlowPaintInvalidator::InvalidatePaintForOverhangingFloatsInternal(
   if (!block_flow_.HasOverhangingFloats())
     return;
 
+  ASSERT(false); // BKTODO:
+#if 0
   for (const auto& floating_object : block_flow_.GetFloatingObjects()->Set()) {
     // Only issue paint invalidations for the object if it is overhanging, is
     // not in its own layer, and is our responsibility to paint (m_shouldPaint
@@ -36,6 +49,7 @@ void BlockFlowPaintInvalidator::InvalidatePaintForOverhangingFloatsInternal(
                 kDontInvalidateDescendants);
     }
   }
+#endif
 }
 
 void BlockFlowPaintInvalidator::InvalidateDisplayItemClients(
