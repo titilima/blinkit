d7c77c72870eea5ead33591b2d03d9015d020d50
Z <z@mysoft.tech>
2020-10-05 13:52:57 +0800

    Build.

--- src/chromium/third_party/blink/renderer/core/paint/block_paint_invalidator.cc
+++ src/chromium/third_party/blink/renderer/core/paint/block_paint_invalidator.cc
@@ -1,3 +1,14 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: block_paint_invalidator.cc
+// Description: BlockPaintInvalidator Class
+//      Author: Ziming Li
+//     Created: 2020-10-05
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2016 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -16,16 +27,22 @@
 namespace blink {
 
 void BlockPaintInvalidator::ClearPreviousVisualRects() {
+  ASSERT(false); // BKTODO:
+#if 0
   block_.GetFrame()->Selection().ClearPreviousCaretVisualRect(block_);
   block_.GetFrame()->GetPage()->GetDragCaret().ClearPreviousVisualRect(block_);
+#endif
 }
 
 void BlockPaintInvalidator::InvalidatePaint(
     const PaintInvalidatorContext& context) {
   BoxPaintInvalidator(block_, context).InvalidatePaint();
 
+  ASSERT(false); // BKTODO:
+#if 0
   block_.GetFrame()->Selection().InvalidatePaint(block_, context);
   block_.GetFrame()->GetPage()->GetDragCaret().InvalidatePaint(block_, context);
+#endif
 }
 
 }  // namespace blink
