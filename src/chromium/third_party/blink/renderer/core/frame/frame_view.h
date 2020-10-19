6b111c5686c713b4daed444e110ec05960dc6275
Z <z@mysoft.tech>
2020-10-19 18:04:26 +0800

    FrameView skeleton class.

---
+++ src/chromium/third_party/blink/renderer/core/frame/frame_view.h
@@ -0,0 +1,27 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: frame_view.h
+// Description: FrameView Class
+//      Author: Ziming Li
+//     Created: 2020-10-19
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINK_FRAME_VIEW_H
+#define BLINKIT_BLINK_FRAME_VIEW_H
+
+#pragma once
+
+namespace blink {
+
+class FrameView
+{
+public:
+    virtual ~FrameView(void) = default;
+};
+
+} // namespace blink
+
+#endif // BLINKIT_BLINK_FRAME_VIEW_H
