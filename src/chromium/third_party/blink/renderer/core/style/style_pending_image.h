8360b79208109501ed9e96e1c78b6c519f32c69b
Z <z@mysoft.tech>
2020-11-25 11:13:12 +0800

    Rollback to original Chromium source file.

--- src/chromium/third_party/blink/renderer/core/style/style_pending_image.h
+++ src/chromium/third_party/blink/renderer/core/style/style_pending_image.h
@@ -1,14 +1,3 @@
-// -------------------------------------------------
-// BlinKit - blink Library
-// -------------------------------------------------
-//   File Name: style_pending_image.h
-// Description: StylePendingImage
-//      Author: Ziming Li
-//     Created: 2020-10-08
-// -------------------------------------------------
-// Copyright (C) 2020 MingYang Software Technology.
-// -------------------------------------------------
-
 /*
  * Copyright (C) 2010 Apple Inc. All rights reserved.
  *
@@ -101,6 +90,11 @@ class StylePendingImage final : public StyleImage {
   }
   bool KnownToBeOpaque(const Document&, const ComputedStyle&) const override {
     return false;
+  }
+
+  void Trace(blink::Visitor* visitor) override {
+    visitor->Trace(value_);
+    StyleImage::Trace(visitor);
   }
 
  private:
