76b6a1dea3154a1d2d7c4523edad2ef2231fe6e0
Z <z@mysoft.tech>
2020-11-27 23:28:48 +0800

    New GC implementations.

--- src/chromium/third_party/blink/renderer/core/css/css_value_pool.h
+++ src/chromium/third_party/blink/renderer/core/css/css_value_pool.h
@@ -64,17 +64,17 @@ class CORE_EXPORT CSSValuePool
   static const int kMaximumCacheableIntegerValue = 255;
   using CSSColorValue = cssvalue::CSSColorValue;
   using CSSUnsetValue = cssvalue::CSSUnsetValue;
-  using ColorValueCache = HeapHashMap<unsigned, std::shared_ptr<CSSColorValue>>;
+  using ColorValueCache = HeapHashMap<unsigned, Member<CSSColorValue>>;
   static const unsigned kMaximumColorCacheSize = 512;
   using FontFaceValueCache =
       HeapHashMap<AtomicString, Member<const CSSValueList>>;
   static const unsigned kMaximumFontFaceCacheSize = 128;
-  using FontFamilyValueCache = HeapHashMap<String, std::shared_ptr<CSSFontFamilyValue>>;
+  using FontFamilyValueCache = HeapHashMap<String, Member<CSSFontFamilyValue>>;
 
   // Cached individual values.
-  std::shared_ptr<CSSColorValue> TransparentColor() { return color_transparent_; }
-  std::shared_ptr<CSSColorValue> WhiteColor() { return color_white_; }
-  std::shared_ptr<CSSColorValue> BlackColor() { return color_black_; }
+  CSSColorValue* TransparentColor() { return color_transparent_; }
+  CSSColorValue* WhiteColor() { return color_white_; }
+  CSSColorValue* BlackColor() { return color_black_; }
   CSSInheritedValue* InheritedValue() { return inherited_value_; }
   CSSInitialValue* InitialValue() { return initial_value_; }
   CSSUnsetValue* UnsetValue() { return unset_value_; }
@@ -110,13 +110,13 @@ class CORE_EXPORT CSSValuePool
   }
 
   // Hash map caches.
-  std::shared_ptr<CSSColorValue>& GetColorCacheEntry(RGBA32 rgb_value) {
+  Member<CSSColorValue>& GetColorCacheEntry(RGBA32 rgb_value) {
     // Just wipe out the cache and start rebuilding if it gets too big.
     if (color_value_cache_.size() > kMaximumColorCacheSize)
       color_value_cache_.clear();
     return color_value_cache_[rgb_value];
   }
-  std::shared_ptr<CSSFontFamilyValue>& GetFontFamilyCacheEntry(
+  Member<CSSFontFamilyValue>& GetFontFamilyCacheEntry(
       const String& family_name) {
     return font_family_value_cache_[family_name];
   }
@@ -130,6 +130,8 @@ class CORE_EXPORT CSSValuePool
   }
 #endif
 
+  void Trace(blink::Visitor*);
+
  private:
   CSSValuePool();
 
@@ -137,9 +139,9 @@ class CORE_EXPORT CSSValuePool
   Member<CSSInheritedValue> inherited_value_;
   Member<CSSInitialValue> initial_value_;
   Member<CSSUnsetValue> unset_value_;
-  std::shared_ptr<CSSColorValue> color_transparent_;
-  std::shared_ptr<CSSColorValue> color_white_;
-  std::shared_ptr<CSSColorValue> color_black_;
+  Member<CSSColorValue> color_transparent_;
+  Member<CSSColorValue> color_white_;
+  Member<CSSColorValue> color_black_;
 
   // Vector caches.
   HeapVector<Member<CSSIdentifierValue>, numCSSValueKeywords>
