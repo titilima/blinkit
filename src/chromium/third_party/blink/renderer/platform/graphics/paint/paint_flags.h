8e0fb4416face086a1f8d0683ae1a09fdf0a55a7
Z <titilima@163.com>
2020-12-21 18:31:36 +0800

    Rollback to original Chromium source file.

--- src/chromium/third_party/blink/renderer/platform/graphics/paint/paint_flags.h
+++ src/chromium/third_party/blink/renderer/platform/graphics/paint/paint_flags.h
@@ -1,14 +1,3 @@
-// -------------------------------------------------
-// BlinKit - blink Library
-// -------------------------------------------------
-//   File Name: paint_flags.h
-// Description: PaintFlags Class
-//      Author: Ziming Li
-//     Created: 2020-09-01
-// -------------------------------------------------
-// Copyright (C) 2020 MingYang Software Technology.
-// -------------------------------------------------
-
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -16,10 +5,10 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_PAINT_PAINT_FLAGS_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_PAINT_PAINT_FLAGS_H_
 
-// BKTODO: #include "cc/paint/paint_flags.h"
+#include "cc/paint/paint_flags.h"
 
 namespace blink {
-// BKTODO: using cc::PaintFlags;
+using cc::PaintFlags;
 }
 
 #endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_PAINT_PAINT_FLAGS_H_
