edc97ef4fe2abff6f8b3341230ac31a73297f08b
Z <titilima@163.com>
2020-12-21 17:36:04 +0800

    Rollback to original Chromium source file.

--- src/chromium/third_party/blink/renderer/platform/graphics/paint/paint_recorder.h
+++ src/chromium/third_party/blink/renderer/platform/graphics/paint/paint_recorder.h
@@ -1,14 +1,3 @@
-// -------------------------------------------------
-// BlinKit - blink Library
-// -------------------------------------------------
-//   File Name: paint_recorder.h
-// Description: PaintRecorder Class
-//      Author: Ziming Li
-//     Created: 2020-09-01
-// -------------------------------------------------
-// Copyright (C) 2020 MingYang Software Technology.
-// -------------------------------------------------
-
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -16,12 +5,10 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_PAINT_PAINT_RECORDER_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_PAINT_PAINT_RECORDER_H_
 
-#if 0 // BKTODO:
 #include "cc/paint/paint_recorder.h"
 
 namespace blink {
 using cc::PaintRecorder;
 }
-#endif
 
 #endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_PAINT_PAINT_RECORDER_H_
