e61d3896bc659359db380836c01654887386e2c5
Z <titilima@163.com>
2020-12-22 18:22:19 +0800

    Update placeholders.

---
+++ src/chromium/base/trace_event/common/trace_event_common.h
@@ -0,0 +1,38 @@
+// -------------------------------------------------
+// BlinKit - base Library
+// -------------------------------------------------
+//   File Name: trace_event_common.h
+// Description: Placeholders for trace_event
+//      Author: Ziming Li
+//     Created: 2020-12-22
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BASE_TRACE_EVENT_COMMON_H
+#define BLINKIT_BASE_TRACE_EVENT_COMMON_H
+
+#pragma once
+
+#define TRACE_EVENT_BEGIN0(...)                     ((void)0)
+#define TRACE_EVENT_END0(...)                       ((void)0)
+
+#define TRACE_EVENT_BEGIN1(...)                     ((void)0)
+#define TRACE_EVENT_END1(...)                       ((void)0)
+
+#define TRACE_EVENT_BEGIN2(...)                     ((void)0)
+#define TRACE_EVENT_END2(...)                       ((void)0)
+
+#define TRACE_EVENT_CATEGORY_GROUP_ENABLED(...)     ((void)0)
+#define TRACE_EVENT_OBJECT_SNAPSHOT_WITH_ID(...)    ((void)0)
+#define TRACE_EVENT_INSTANT1(...)                   ((void)0)
+
+#define TRACE_EVENT_NESTABLE_ASYNC_BEGIN1(...)      ((void)0)
+#define TRACE_EVENT_NESTABLE_ASYNC_END1(...)        ((void)0)
+#define TRACE_EVENT_NESTABLE_ASYNC_INSTANT1(...)    ((void)0)
+
+#define TRACE_EVENT0(...)                           ((void)0)
+#define TRACE_EVENT1(...)                           ((void)0)
+#define TRACE_EVENT2(...)                           ((void)0)
+
+#endif // BLINKIT_BASE_TRACE_EVENT_COMMON_H
--- src/chromium/base/trace_event/trace_event.h
+++ src/chromium/base/trace_event/trace_event.h
@@ -1,19 +1,19 @@
 // -------------------------------------------------
-// BlinKit - blink Library
+// BlinKit - base Library
 // -------------------------------------------------
 //   File Name: trace_event.h
-// Description: Placeholders for Instrumentation
+// Description: Placeholders for trace_event
 //      Author: Ziming Li
-//     Created: 2020-08-28
+//     Created: 2020-12-22
 // -------------------------------------------------
 // Copyright (C) 2020 MingYang Software Technology.
 // -------------------------------------------------
 
-#ifndef BLINKIT_BLINK_TRACE_EVENT_H
-#define BLINKIT_BLINK_TRACE_EVENT_H
+#ifndef BLINKIT_BASE_TRACE_EVENT_H
+#define BLINKIT_BASE_TRACE_EVENT_H
 
 #pragma once
 
-#include "base/trace_event/trace_event.h"
+#include "base/trace_event/common/trace_event_common.h"
 
-#endif // BLINKIT_BLINK_TRACE_EVENT_H
+#endif // BLINKIT_BASE_TRACE_EVENT_H
