0d6d09d5c9b12868141d314b1580a713d8e07a78
Z <titilima@163.com>
2020-12-29 09:18:55 +0800

    Add ICU project.

---
+++ src/icu/_pc.h
@@ -0,0 +1,19 @@
+// -------------------------------------------------
+// BlinKit - icu Library
+// -------------------------------------------------
+//   File Name: _pc.h
+// Description: Pre-compiled Header File
+//      Author: Ziming Li
+//     Created: 2020-12-29
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_ICU__PC_H
+#define BLINKIT_ICU__PC_H
+
+#pragma once
+
+#include "chromium/build/build_config.h"
+
+#endif // BLINKIT_ICU__PC_H
