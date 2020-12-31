78eb41569aa516f1fc4fe94ae41d4082a193ccbe
Z <titilima@163.com>
2020-12-31 21:36:58 +0800

    Build.

--- src/chromium/third_party/blink/renderer/core/html/rel_list.cc
+++ src/chromium/third_party/blink/renderer/core/html/rel_list.cc
@@ -1,3 +1,14 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: rel_list.cc
+// Description: RelList Class
+//      Author: Ziming Li
+//     Created: 2020-12-31
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -8,7 +19,7 @@
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/html/html_element.h"
 #include "third_party/blink/renderer/core/html_names.h"
-#include "third_party/blink/renderer/core/origin_trials/origin_trials.h"
+// BKTODO: #include "third_party/blink/renderer/core/origin_trials/origin_trials.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/wtf/hash_map.h"
 
@@ -42,11 +53,14 @@ bool RelList::ValidateTokenValue(const AtomicString& token_value,
                                  ExceptionState&) const {
   //  https://html.spec.whatwg.org/multipage/links.html#linkTypes
   if (GetElement().HasTagName(HTMLNames::linkTag)) {
+    ASSERT(false); // BKTODO:
+#if 0
     if (SupportedTokensLink().Contains(token_value) ||
         (RuntimeEnabledFeatures::ModulePreloadEnabled() &&
          token_value == "modulepreload")) {
       return true;
     }
+#endif
   } else if ((GetElement().HasTagName(HTMLNames::aTag) ||
               GetElement().HasTagName(HTMLNames::areaTag)) &&
              SupportedTokensAnchorAndArea().Contains(token_value)) {
