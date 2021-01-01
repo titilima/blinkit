41127125deebe0c32c9ce9ee555b68d19513e8bb
Z <titilima@163.com>
2021-01-01 12:08:37 +0800

    Import from Chromium.

--- src/chromium/url/third_party/mozilla/url_parse.h
+++ src/chromium/url/third_party/mozilla/url_parse.h
@@ -177,6 +177,13 @@ struct URL_EXPORT Parsed {
   // the string with the scheme stripped off.
   Component GetContent() const;
 
+  // True if the URL's source contained a raw `<` character, and whitespace was
+  // removed from the URL during parsing
+  //
+  // TODO(mkwst): Link this to something in a spec if
+  // https://github.com/whatwg/url/pull/284 lands.
+  bool potentially_dangling_markup;
+
   // This is used for nested URL types, currently only filesystem.  If you
   // parse a filesystem URL, the resulting Parsed will have a nested
   // inner_parsed_ to hold the parsed inner URL's component information.
