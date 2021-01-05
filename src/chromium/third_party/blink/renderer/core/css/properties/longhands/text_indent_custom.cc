c0566f375683e9ac48c0da4f0f58006f32def825
Z <titilima@163.com>
2021-01-05 21:12:37 +0800

    Rollback to original Chromium source file.

--- src/chromium/third_party/blink/renderer/core/css/properties/longhands/text_indent_custom.cc
+++ src/chromium/third_party/blink/renderer/core/css/properties/longhands/text_indent_custom.cc
@@ -1,14 +1,3 @@
-// -------------------------------------------------
-// BlinKit - blink Library
-// -------------------------------------------------
-//   File Name: text_indent_custom.cc
-// Description: TextIndent Class
-//      Author: Ziming Li
-//     Created: 2020-10-13
-// -------------------------------------------------
-// Copyright (C) 2020 MingYang Software Technology.
-// -------------------------------------------------
-
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -46,13 +35,10 @@ const CSSValue* TextIndent::ParseSingleValue(
 
     if (RuntimeEnabledFeatures::CSS3TextEnabled()) {
       CSSValueID id = range.Peek().Id();
-      ASSERT(false); // BKTODO:
-#if 0
       if (!hanging && id == CSSValueHanging) {
         hanging = CSSPropertyParserHelpers::ConsumeIdent(range);
         continue;
       }
-#endif
       if (!each_line && id == CSSValueEachLine) {
         each_line = CSSPropertyParserHelpers::ConsumeIdent(range);
         continue;
@@ -85,11 +71,8 @@ const CSSValue* TextIndent::CSSValueFromComputedStyleInternal(
   list->Append(*ComputedStyleUtils::ZoomAdjustedPixelValueForLength(
       style.TextIndent(), style));
   if (RuntimeEnabledFeatures::CSS3TextEnabled()) {
-    ASSERT(false); // BKTODO:
-#if 0
     if (style.GetTextIndentType() == TextIndentType::kHanging)
       list->Append(*CSSIdentifierValue::Create(CSSValueHanging));
-#endif
     if (style.GetTextIndentLine() == TextIndentLine::kEachLine)
       list->Append(*CSSIdentifierValue::Create(CSSValueEachLine));
   }
@@ -119,8 +102,6 @@ void TextIndent::ApplyValue(StyleResolverState& state,
       ComputedStyleInitialValues::InitialTextIndentType();
 
   for (auto& list_value : ToCSSValueList(value)) {
-    ASSERT(false); // BKTODO:
-#if 0
     if (list_value->IsPrimitiveValue()) {
       length_or_percentage_value =
           ToCSSPrimitiveValue(*list_value)
@@ -134,7 +115,6 @@ void TextIndent::ApplyValue(StyleResolverState& state,
     } else {
       NOTREACHED();
     }
-#endif
   }
 
   state.Style()->SetTextIndent(length_or_percentage_value);
