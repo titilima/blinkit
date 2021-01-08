cd32b77ed5b30f8b713fbfaa776b80af939b9369
Z <titilima@163.com>
2021-01-08 11:57:10 +0800

    Rollback to original Chromium source file.

--- src/chromium/third_party/blink/renderer/platform/graphics/paint/paint_image.h
+++ src/chromium/third_party/blink/renderer/platform/graphics/paint/paint_image.h
@@ -1,14 +1,3 @@
-// -------------------------------------------------
-// BlinKit - blink Library
-// -------------------------------------------------
-//   File Name: paint_image.h
-// Description: PaintImage Classes
-//      Author: Ziming Li
-//     Created: 2020-09-01
-// -------------------------------------------------
-// Copyright (C) 2020 MingYang Software Technology.
-// -------------------------------------------------
-
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -16,7 +5,6 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_PAINT_PAINT_IMAGE_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_PAINT_PAINT_IMAGE_H_
 
-#if 0 // BKTODO:
 #include "cc/paint/paint_image.h"
 #include "cc/paint/paint_image_builder.h"
 
@@ -27,10 +15,5 @@ using cc::PaintImageBuilder;
 using cc::PaintImageGenerator;
 using cc::SkiaPaintImageGenerator;
 }
-#else
-
-#include "third_party/skia/include/core/SkImage.h"
-
-#endif // 0
 
 #endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_PAINT_PAINT_IMAGE_H_
