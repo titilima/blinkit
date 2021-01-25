966e94e78397c1cb65801ad116e788295653dc40
Z <titilima@163.com>
2021-01-25 16:55:59 +0800

    Cleanup.

--- src/blinkit/blink_impl/win_thread.cpp
+++ src/blinkit/blink_impl/win_thread.cpp
@@ -9,24 +9,11 @@
 // Copyright (C) 2019 MingYang Software Technology.
 // -------------------------------------------------
 
-#include "win_thread.h"
-
-#include "blinkit/blink_impl/win_single_thread_task_runner.h"
+#include "blinkit/blink_impl/thread_impl.h"
 
 using namespace blink;
 
 namespace BlinKit {
-
-WinThread::~WinThread(void) = default;
-
-std::shared_ptr<base::SingleThreadTaskRunner> WinThread::GetTaskRunner(void) const
-{
-    if (!m_taskRunner)
-        m_taskRunner = std::make_shared<WinSingleThreadTaskRunner>();
-    return m_taskRunner;
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 PlatformThreadId ThreadImpl::CurrentThreadId(void)
 {
