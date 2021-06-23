0d429932b5cbe1d802db13b1ef37defd52333229
Z <z@mysoft.tech>
2021-06-23 17:50:59 +0800

    Build.

--- src/blinkit/app/app_caller_impl.cpp
+++ src/blinkit/app/app_caller_impl.cpp
@@ -11,9 +11,9 @@
 
 #include "app_caller_impl.h"
 
+#include "third_party/zed/include/zed/threading/signal.hpp"
 #include "base/single_thread_task_runner.h"
-#include "bkcommon/bk_signal.hpp"
-#include "third_party/blink/renderer/platform/wtf/wtf.h"
+#include "blinkit/blink/renderer/wtf/MainThread.h"
 
 namespace BlinKit {
 
@@ -31,19 +31,20 @@ void AppCallerImpl::Call(const base::Location &loc, std::function<void()> &&task
 
 void AppCallerImpl::SyncCall(const base::Location &loc, std::function<void()> &&task)
 {
-    if (IsMainThread())
+    if (isMainThread())
     {
         task();
     }
     else
     {
-        ScopedSignalWaiter waiter;
+        zed::signal waiter;
         auto waitableTask = [&waiter, task = std::move(task)]
         {
             task();
-            waiter.Signal();
+            waiter.notify();
         };
         m_taskRunner->PostTask(loc, std::move(waitableTask));
+        waiter.wait();
     }
 }
 
