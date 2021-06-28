1bb9058c6ddc96f1a08c21e7d8db00871152de43
Z <z@mysoft.tech>
2021-06-28 13:55:19 +0800

    ResourceRequest class.

---
+++ src/blinkit/blink/renderer/platform/network/resource_request.cpp
@@ -0,0 +1,12 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: resource_request.cpp
+// Description: ResourceRequest Class
+//      Author: Ziming Li
+//     Created: 2021-06-25
+// -------------------------------------------------
+// Copyright (C) 2021 MingYang Software Technology.
+// -------------------------------------------------
+
+#include "./ResourceRequest.h"
