f405f47a485340ca008af6f4e7cb2118a5839d6f
Z <z@mysoft.tech>
2021-06-28 13:54:39 +0800

    Update placeholders.

---
+++ src/blinkit/blink/renderer/wtf/testing/WTFUnitTestHelpersExport.h
@@ -0,0 +1,15 @@
+#pragma once
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: WTFUnitTestHelpersExport.h
+// Description: Placeholder
+//      Author: Ziming Li
+//     Created: 2021-06-24
+// -------------------------------------------------
+// Copyright (C) 2021 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef WTF_UNITTEST_HELPERS_EXPORT
+#define WTF_UNITTEST_HELPERS_EXPORT
+#endif
