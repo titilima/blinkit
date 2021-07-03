234742d3069cf942f65a04e56c0be2df0055b0b7
Z <z@mysoft.tech>
2021-07-03 20:25:28 +0800

    Build old code.

---
+++ src/blinkit/blink/renderer/bindings/core/duk/script_wrappers.cpp
@@ -0,0 +1,38 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: script_wrappers.cpp
+// Description: Script Wrapper Classes
+//      Author: Ziming Li
+//     Created: 2020-03-15
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#include "./script_wrappers.h"
+
+#include "blinkit/blink/renderer/bindings/core/duk/script_wrappable.h"
+
+using namespace blink;
+
+namespace BlinKit {
+
+PushWrapper::PushWrapper(duk_context *ctx, ScriptWrappable *nativeObject) : m_ctx(ctx), m_nativeObject(nativeObject)
+{
+}
+
+PushWrapper::~PushWrapper(void)
+{
+    if (nullptr == m_nativeObject)
+        return;
+
+    if (nullptr == m_nativeObject->m_contextObject)
+    {
+        m_nativeObject->m_contextObject = duk_get_heapptr(m_ctx, -1);
+        GCSetFlag(m_nativeObject, GCObjectFlag::JSRetained);
+    }
+
+    ASSERT(duk_get_heapptr(m_ctx, -1) == m_nativeObject->m_contextObject);
+}
+
+} // namespace BlinKit
---
+++ src/blinkit/blink/renderer/bindings/core/duk/script_wrappers.h
@@ -0,0 +1,41 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: script_wrappers.h
+// Description: Script Wrapper Classes
+//      Author: Ziming Li
+//     Created: 2020-03-15
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINK_SCRIPT_WRAPPERS_H
+#define BLINKIT_BLINK_SCRIPT_WRAPPERS_H
+
+#pragma once
+
+#include "base/macros.h"
+#include "duktape/duktape.h"
+#include "blinkit/blink/renderer/wtf/Allocator.h"
+
+namespace blink {
+class ScriptWrappable;
+}
+
+namespace BlinKit {
+
+class PushWrapper
+{
+    STACK_ALLOCATED();
+    DISALLOW_COPY_AND_ASSIGN(PushWrapper);
+public:
+    PushWrapper(duk_context *ctx, blink::ScriptWrappable *nativeObject);
+    ~PushWrapper(void);
+private:
+    duk_context *m_ctx;
+    blink::ScriptWrappable *m_nativeObject;
+};
+
+} // namespace BlinKit
+
+#endif // BLINKIT_BLINK_SCRIPT_WRAPPERS_H
