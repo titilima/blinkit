50b8f4d260805ce19a5545beafdcd12636f3c223
Z <z@mysoft.tech>
2021-07-04 22:01:44 +0800

    Update placeholders.

---
+++ src/blinkit/blink/renderer/bindings/core/v8/ExceptionState.h
@@ -0,0 +1,13 @@
+#pragma once
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: ExceptionState.h
+// Description: Placeholder for V8 Part
+//      Author: Ziming Li
+//     Created: 2021-07-04
+// -------------------------------------------------
+// Copyright (C) 2021 MingYang Software Technology.
+// -------------------------------------------------
+
+#include "blinkit/blink/renderer/bindings/core/duk/exception_state.h"
