822ac278df5e42f4232b2ff9b8d8e7d9cf1257fc
Z <z@mysoft.tech>
2021-07-05 11:32:38 +0800

    Update placeholders.

---
+++ src/blinkit/blink/renderer/platform/weborigin/KURL.h
@@ -0,0 +1,22 @@
+#pragma once
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: KURL.h
+// Description: Placeholder for KURL
+//      Author: Ziming Li
+//     Created: 2021-07-05
+// -------------------------------------------------
+// Copyright (C) 2021 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINKIT_KURL_H
+#define BLINKIT_BLINKIT_KURL_H
+
+#include "third_party/zed/include/zed/net/url.hpp"
+
+namespace blink {
+using KURL = zed::url;
+}
+
+#endif // BLINKIT_BLINKIT_KURL_H
