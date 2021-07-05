922be7251f4558f0d5967d37bbdd45975982cf38
Z <z@mysoft.tech>
2021-07-05 10:20:55 +0800

    Build old code.

---
+++ src/blinkit/blink/renderer/core/events/EventListenerOptions.h
@@ -0,0 +1,49 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: EventListenerOptions.h
+// Description: EventListenerOptions Classes
+//      Author: Ziming Li
+//     Created: 2019-02-04
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINKIT_EVENT_LISTENER_OPTIONS_H
+#define BLINKIT_BLINKIT_EVENT_LISTENER_OPTIONS_H
+
+#pragma once
+
+#include <optional>
+
+namespace blink {
+
+class EventListenerOptions
+{
+public:
+    bool hasCapture(void) const { return m_capture.has_value(); }
+    bool capture(void) const
+    {
+        ASSERT(m_capture.has_value());
+        return m_capture.value();
+    }
+    void setCapture(bool capture) { m_capture = capture; }
+
+    bool hasPassive(void) const { return m_passive.has_value(); }
+    bool passive(void) const
+    {
+        ASSERT(m_passive.has_value());
+        return m_passive.value();
+    }
+    void setPassive(bool passive) { m_passive = passive; }
+private:
+    std::optional<bool> m_capture, m_passive;
+};
+
+class EventListenerOptionsOrBoolean
+{
+};
+
+} // namespace blink
+
+#endif // BLINKIT_BLINKIT_EVENT_LISTENER_OPTIONS_H
