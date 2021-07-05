740b9f0e87fe562bcef109793813a0907818b577
Z <z@mysoft.tech>
2021-07-05 21:17:55 +0800

    Build old code.

---
+++ src/blinkit/blink/renderer/core/events/EventInit.h
@@ -0,0 +1,35 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: EventInit.h
+// Description: EventInit Class
+//      Author: Ziming Li
+//     Created: 2018-07-12
+// -------------------------------------------------
+// Copyright (C) 2018 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINKIT_EVENT_INIT_H
+#define BLINKIT_BLINKIT_EVENT_INIT_H
+
+#pragma once
+
+namespace blink {
+
+class EventInit
+{
+public:
+    bool hasBubbles(void) const { return true; }
+    bool bubbles(void) const { return m_bubbles; }
+    void setBubbles(bool value) { m_bubbles = value; }
+
+    bool hasCancelable(void) const { return true; }
+    bool cancelable(void) const { return m_cancelable; }
+    void setCancelable(bool value) { m_cancelable = value; }
+private:
+    bool m_bubbles = false, m_cancelable = false;
+};
+
+} // namespace blink
+
+#endif // BLINKIT_BLINKIT_EVENT_INIT_H
