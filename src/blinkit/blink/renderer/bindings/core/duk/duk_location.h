7b83a1d1eb7ff8afcad4f22eace2041641a54cfb
Z <z@mysoft.tech>
2021-07-08 13:40:56 +0800

    Build old code.

---
+++ src/blinkit/blink/renderer/bindings/core/duk/duk_location.h
@@ -0,0 +1,33 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: duk_location.h
+// Description: DukLocation Class
+//      Author: Ziming Li
+//     Created: 2020-03-26
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINK_DUK_LOCATION_H
+#define BLINKIT_BLINK_DUK_LOCATION_H
+
+#pragma once
+
+#include "blinkit/blink/renderer/bindings/core/duk/duk_script_object.h"
+#include "blinkit/blink/renderer/core/frame/Location.h"
+
+namespace BlinKit {
+
+class DukLocation final : public DukScriptObject
+{
+public:
+    static const char ProtoName[];
+    static void RegisterPrototype(PrototypeHelper &helper);
+private:
+    static void FillPrototypeEntry(PrototypeEntry &entry);
+};
+
+} // namespace BlinKit
+
+#endif // BLINKIT_BLINK_DUK_LOCATION_H
