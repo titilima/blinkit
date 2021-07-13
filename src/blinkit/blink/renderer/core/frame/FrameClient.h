78e8027fb938cbc0b5f5602bb18ada3acf84bd95
Z <z@mysoft.tech>
2021-07-13 11:14:24 +0800

    Import from Chromium.

--- src/blinkit/blink/renderer/core/frame/FrameClient.h
+++ src/blinkit/blink/renderer/core/frame/FrameClient.h
@@ -1,28 +1,53 @@
-#pragma once
-// -------------------------------------------------
-// BlinKit - BlinKit Library
-// -------------------------------------------------
-//   File Name: FrameClient.h
-// Description: FrameClient Class
-//      Author: Ziming Li
-//     Created: 2021-06-28
-// -------------------------------------------------
-// Copyright (C) 2021 MingYang Software Technology.
-// -------------------------------------------------
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-#ifndef BLINKIT_BLINKIT_FRAME_CLIENT_H
-#define BLINKIT_BLINKIT_FRAME_CLIENT_H
+#ifndef FrameClient_h
+#define FrameClient_h
 
+#include "core/CoreExport.h"
 #include "platform/heap/Handle.h"
 
 namespace blink {
 
-class FrameClient
-{
+class Frame;
+class LocalFrame;
+class MessageEvent;
+class SecurityOrigin;
+enum class FrameDetachType;
+
+class CORE_EXPORT FrameClient : public NoBaseWillBeGarbageCollectedFinalized<FrameClient> {
+    USING_FAST_MALLOC_WILL_BE_REMOVED(FrameClient);
 public:
-    virtual ~FrameClient(void) = default;
+    virtual bool inShadowTree() const = 0;
+
+    // TODO(dcheng): Move this into FrameLoaderClient, since remote frames don't
+    // need this.
+    virtual void willBeDetached() = 0;
+    virtual void detached(FrameDetachType) = 0;
+
+    virtual Frame* opener() const = 0;
+    virtual void setOpener(Frame*) = 0;
+
+    virtual Frame* parent() const = 0;
+    virtual Frame* top() const = 0;
+    virtual Frame* previousSibling() const = 0;
+    virtual Frame* nextSibling() const = 0;
+    virtual Frame* firstChild() const = 0;
+    virtual Frame* lastChild() const = 0;
+
+    virtual unsigned backForwardLength() = 0;
+
+    // Returns true if the embedder intercepted the postMessage call
+    virtual bool willCheckAndDispatchMessageEvent(SecurityOrigin* /*target*/, MessageEvent*, LocalFrame* /*sourceFrame*/) const { return false; }
+
+    virtual void frameFocused() const = 0;
+
+    virtual ~FrameClient() { }
+
+    DEFINE_INLINE_VIRTUAL_TRACE() { }
 };
 
 } // namespace blink
 
-#endif // BLINKIT_BLINKIT_FRAME_CLIENT_H
+#endif // FrameClient_h
