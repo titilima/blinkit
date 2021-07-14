7c3827db53384ea162d3e05365a52326a61178ab
Z <z@mysoft.tech>
2021-07-14 17:18:58 +0800

    Update placeholders.

---
+++ src/blinkit/blink/renderer/core/css/resolver/StyleResolverStats.h
@@ -0,0 +1,17 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: StyleResolverStats.h
+// Description: Placeholder for Stats
+//      Author: Ziming Li
+//     Created: 2021-07-14
+// -------------------------------------------------
+// Copyright (C) 2021 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINKIT_STYLE_RESOLVER_STATS_H
+#define BLINKIT_BLINKIT_STYLE_RESOLVER_STATS_H
+
+#define INCREMENT_STYLE_STATS_COUNTER(...)  ((void)0)
+
+#endif // BLINKIT_BLINKIT_STYLE_RESOLVER_STATS_H
