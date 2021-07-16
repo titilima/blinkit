3401228aae342aa9b9a926a4e2cee2ae8bd680d8
Z <z@mysoft.tech>
2021-07-16 22:00:21 +0800

    Build old code.

---
+++ src/blinkit/blink/renderer/bindings/core/duk/duk_element.h
@@ -0,0 +1,47 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: duk_element.h
+// Description: DukElement Class
+//      Author: Ziming Li
+//     Created: 2020-02-11
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINK_DUK_ELEMENT_H
+#define BLINKIT_BLINK_DUK_ELEMENT_H
+
+#pragma once
+
+#include "blinkit/blink/renderer/bindings/core/duk/duk_container_node.h"
+#include "blinkit/blink/renderer/core/dom/Element.h"
+
+namespace BlinKit {
+
+class DukElement : DukContainerNode
+{
+public:
+    static const char ProtoName[];
+    static void RegisterPrototypeForCrawler(PrototypeHelper &helper);
+
+    static duk_idx_t Push(duk_context *ctx, blink::Element *element);
+
+    static duk_ret_t GetURLAttributeImpl(duk_context *ctx, const blink::QualifiedName &name);
+    static duk_ret_t SetAttributeImpl(duk_context *ctx, const blink::QualifiedName &name);
+    static duk_ret_t SetAttributeEventListenerImpl(duk_context *ctx, const WTF::AtomicString &attrName);
+
+    static const std::unordered_map<std::string, const char *>& PrototypeMapForCrawler(void);
+#ifndef BLINKIT_CRAWLER_ONLY
+    static const std::unordered_map<std::string, const char *>& PrototypeMapForUI(void);
+#endif
+protected:
+    static void FillPrototypeEntryForCrawler(PrototypeEntry &entry);
+private:
+    friend class DukNode;
+    static void Create(duk_context *ctx, blink::Element &element);
+};
+
+} // namespace BlinKit
+
+#endif // BLINKIT_BLINK_DUK_ELEMENT_H
