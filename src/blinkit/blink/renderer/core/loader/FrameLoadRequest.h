ed4e769fc27aa75e8048d9c7064d260f82f79c93
Z <z@mysoft.tech>
2021-07-16 22:31:08 +0800

    Build.

--- src/blinkit/blink/renderer/core/loader/FrameLoadRequest.h
+++ src/blinkit/blink/renderer/core/loader/FrameLoadRequest.h
@@ -1,3 +1,14 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: FrameLoadRequest.h
+// Description: FrameLoadRequest Struct
+//      Author: Ziming Li
+//     Created: 2021-07-16
+// -------------------------------------------------
+// Copyright (C) 2021 MingYang Software Technology.
+// -------------------------------------------------
+
 /*
  * Copyright (C) 2003, 2006, 2010 Apple Inc. All rights reserved.
  *
@@ -31,7 +42,7 @@
 #include "core/fetch/ResourceLoaderOptions.h"
 #include "core/fetch/SubstituteData.h"
 #include "core/html/HTMLFormElement.h"
-#include "core/loader/FrameLoaderTypes.h"
+// BKTODO: #include "core/loader/FrameLoaderTypes.h"
 #include "platform/network/ResourceRequest.h"
 
 namespace blink {
@@ -77,8 +88,10 @@ public:
     bool replacesCurrentItem() const { return m_replacesCurrentItem; }
     void setReplacesCurrentItem(bool replacesCurrentItem) { m_replacesCurrentItem = replacesCurrentItem; }
 
+#if 0 // BKTODO:
     ClientRedirectPolicy clientRedirect() const { return m_clientRedirect; }
     void setClientRedirect(ClientRedirectPolicy clientRedirect) { m_clientRedirect = clientRedirect; }
+#endif
 
     Event* triggeringEvent() const { return m_triggeringEvent.get(); }
     void setTriggeringEvent(PassRefPtrWillBeRawPtr<Event> triggeringEvent) { m_triggeringEvent = triggeringEvent; }
@@ -86,11 +99,13 @@ public:
     HTMLFormElement* form() const { return m_form.get(); }
     void setForm(PassRefPtrWillBeRawPtr<HTMLFormElement> form) { m_form = form; }
 
+#if 0 // BKTODO:
     ShouldSendReferrer shouldSendReferrer() const { return m_shouldSendReferrer; }
     void setShouldSendReferrer(ShouldSendReferrer shouldSendReferrer) { m_shouldSendReferrer = shouldSendReferrer; }
 
     ShouldSetOpener shouldSetOpener() const { return m_shouldSetOpener; }
     void setShouldSetOpener(ShouldSetOpener shouldSetOpener) { m_shouldSetOpener = shouldSetOpener; }
+#endif
 
     ContentSecurityPolicyDisposition shouldCheckMainWorldContentSecurityPolicy() const { return m_shouldCheckMainWorldContentSecurityPolicy; }
 
@@ -101,32 +116,41 @@ private:
         , m_frameName(frameName)
         , m_substituteData(substituteData)
         , m_replacesCurrentItem(false)
-        , m_clientRedirect(NotClientRedirect)
+#if 0 // BKTODO:
+        m_clientRedirect(NotClientRedirect)
         , m_shouldSendReferrer(MaybeSendReferrer)
         , m_shouldSetOpener(MaybeSetOpener)
+#endif
         , m_shouldCheckMainWorldContentSecurityPolicy(shouldCheckMainWorldContentSecurityPolicy)
     {
         initializeFetchFlags();
+#if 0 // BKTODO:
         if (originDocument)
             m_resourceRequest.setRequestorOrigin(SecurityOrigin::create(originDocument->url()));
+#endif
     }
     void initializeFetchFlags()
     {
+        ASSERT(false); // BKTODO:
+#if 0
         // These flags are passed to a service worker which controls the page.
         m_resourceRequest.setFetchRequestMode(WebURLRequest::FetchRequestModeNavigate);
         m_resourceRequest.setFetchCredentialsMode(WebURLRequest::FetchCredentialsModeInclude);
         m_resourceRequest.setFetchRedirectMode(WebURLRequest::FetchRedirectModeManual);
+#endif
     }
     RefPtrWillBeMember<Document> m_originDocument;
     ResourceRequest m_resourceRequest;
     AtomicString m_frameName;
     SubstituteData m_substituteData;
     bool m_replacesCurrentItem;
-    ClientRedirectPolicy m_clientRedirect;
+    // BKTODO: ClientRedirectPolicy m_clientRedirect;
     RefPtrWillBeMember<Event> m_triggeringEvent;
     RefPtrWillBeMember<HTMLFormElement> m_form;
+#if 0 // BKTODO:
     ShouldSendReferrer m_shouldSendReferrer;
     ShouldSetOpener m_shouldSetOpener;
+#endif
     ContentSecurityPolicyDisposition m_shouldCheckMainWorldContentSecurityPolicy;
 };
 
