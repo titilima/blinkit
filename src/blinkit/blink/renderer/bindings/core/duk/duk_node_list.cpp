40509e137973f038d2555ec63696b27993c10abc
Z <z@mysoft.tech>
2021-07-17 11:56:38 +0800

    Build old code.

---
+++ src/blinkit/blink/renderer/bindings/core/duk/duk_node_list.cpp
@@ -0,0 +1,25 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: duk_node_list.cpp
+// Description: DukNodeList Class
+//      Author: Ziming Li
+//     Created: 2020-03-31
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#include "./duk_node_list.h"
+
+#include "blinkit/blink/renderer/bindings/core/duk/duk_node.h"
+
+using namespace blink;
+
+namespace BlinKit {
+
+void DukNodeList::PushItem(duk_context *ctx, NodeList *nodeList, duk_uarridx_t index)
+{
+    DukNode::Push(ctx, nodeList->item(index));
+}
+
+} // namespace BlinKit
---
+++ src/blinkit/blink/renderer/bindings/core/duk/duk_node_list.h
@@ -0,0 +1,31 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: duk_node_list.h
+// Description: DukNodeList Class
+//      Author: Ziming Li
+//     Created: 2020-03-31
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINK_DUK_NODE_LIST_H
+#define BLINKIT_BLINK_DUK_NODE_LIST_H
+
+#pragma once
+
+#include "blinkit/blink/renderer/bindings/core/duk/duk_array_like_object_impl.hpp"
+#include "blinkit/blink/renderer/core/dom/NodeList.h"
+
+namespace BlinKit {
+
+class DukNodeList final : public DukArrayLikeObjectImpl<DukNodeList, blink::NodeList>
+{
+private:
+    template <class T, class N> friend class DukArrayLikeObjectImpl;
+    static void PushItem(duk_context *ctx, blink::NodeList *nodeList, duk_uarridx_t index);
+};
+
+} // namespace BlinKit
+
+#endif // BLINKIT_BLINK_DUK_NODE_LIST_H
