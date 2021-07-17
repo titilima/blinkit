320993efbcc40425cd928c500f6e1ed860099af2
Z <z@mysoft.tech>
2021-07-17 11:33:23 +0800

    Build old code.

---
+++ src/blinkit/blink/renderer/bindings/core/duk/duk_named_node_map.h
@@ -0,0 +1,32 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: duk_named_node_map.h
+// Description: DukNamedNodeMap Class
+//      Author: Ziming Li
+//     Created: 2020-03-18
+// -------------------------------------------------
+// Copyright (C) 2020 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINK_DUK_HTML_COLLECTION_H
+#define BLINKIT_BLINK_DUK_HTML_COLLECTION_H
+
+#pragma once
+
+#include "blinkit/blink/renderer/bindings/core/duk/duk_script_object.h"
+#include "blinkit/blink/renderer/core/dom/NamedNodeMap.h"
+
+namespace BlinKit {
+
+class DukNamedNodeMap : public DukScriptObject
+{
+public:
+    static duk_idx_t Push(duk_context *ctx, blink::NamedNodeMap *namedNodeMap);
+private:
+    static void InitializeProxy(duk_context *ctx);
+};
+
+} // namespace BlinKit
+
+#endif // BLINKIT_BLINK_DUK_HTML_COLLECTION_H
