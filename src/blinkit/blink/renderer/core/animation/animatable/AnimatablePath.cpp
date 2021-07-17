c9c37480a7429782ee4e4dda649dfd22914fcf86
Z <z@mysoft.tech>
2021-07-17 20:38:38 +0800

    Build.

--- src/blinkit/blink/renderer/core/animation/animatable/AnimatablePath.cpp
+++ src/blinkit/blink/renderer/core/animation/animatable/AnimatablePath.cpp
@@ -1,17 +1,33 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: AnimatablePath.cpp
+// Description: AnimatablePath Class
+//      Author: Ziming Li
+//     Created: 2021-07-17
+// -------------------------------------------------
+// Copyright (C) 2021 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "core/animation/animatable/AnimatablePath.h"
 
+#if 0 // BKTODO:
 #include "core/svg/SVGPathBlender.h"
 #include "core/svg/SVGPathByteStreamBuilder.h"
 #include "core/svg/SVGPathByteStreamSource.h"
+#endif
 
 namespace blink {
 
 bool AnimatablePath::usesDefaultInterpolationWith(const AnimatableValue* value) const
 {
+    ASSERT(false); // BKTODO:
+    return true;
+#if 0
     // Default interpolation is used if the paths have different lengths,
     // or the paths have a segment with different types (ignoring "relativeness").
 
@@ -32,6 +48,7 @@ bool AnimatablePath::usesDefaultInterpolationWith(const AnimatableValue* value) 
     }
 
     return toSource.hasMoreData();
+#endif
 }
 
 PassRefPtr<AnimatableValue> AnimatablePath::interpolateTo(const AnimatableValue* value, double fraction) const
@@ -39,6 +56,9 @@ PassRefPtr<AnimatableValue> AnimatablePath::interpolateTo(const AnimatableValue*
     if (usesDefaultInterpolationWith(value))
         return defaultInterpolateTo(this, value, fraction);
 
+    ASSERT(false); // BKTODO:
+    return nullptr;
+#if 0
     RefPtr<SVGPathByteStream> byteStream = SVGPathByteStream::create();
     SVGPathByteStreamBuilder builder(*byteStream);
 
@@ -49,6 +69,7 @@ PassRefPtr<AnimatableValue> AnimatablePath::interpolateTo(const AnimatableValue*
     bool ok = blender.blendAnimatedPath(fraction);
     ASSERT_UNUSED(ok, ok);
     return AnimatablePath::create(StylePath::create(byteStream.release()));
+#endif
 }
 
 StylePath* AnimatablePath::path() const
