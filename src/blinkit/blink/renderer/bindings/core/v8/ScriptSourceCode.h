0d36f69ed5880138e6460f0c280d29cdd69777fd
Z <z@mysoft.tech>
2021-07-21 19:58:58 +0800

    Update placeholders.

---
+++ src/blinkit/blink/renderer/bindings/core/v8/ScriptSourceCode.h
@@ -0,0 +1,13 @@
+#pragma once
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: ScriptSourceCode.h
+// Description: Placeholder for V8 Part
+//      Author: Ziming Li
+//     Created: 2021-07-21
+// -------------------------------------------------
+// Copyright (C) 2021 MingYang Software Technology.
+// -------------------------------------------------
+
+#include "blinkit/blink/renderer/bindings/core/duk/script_source_code.h"
