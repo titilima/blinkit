6a50263d28602abbf966c523f0993107e9e3c9df
Z <z@mysoft.tech>
2021-07-24 10:17:44 +0800

    Build.

--- src/blinkit/blink/renderer/core/html/DocumentNameCollection.cpp
+++ src/blinkit/blink/renderer/core/html/DocumentNameCollection.cpp
@@ -1,11 +1,24 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: DocumentNameCollection.cpp
+// Description: DocumentNameCollection Class
+//      Author: Ziming Li
+//     Created: 2021-07-24
+// -------------------------------------------------
+// Copyright (C) 2021 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "core/html/DocumentNameCollection.h"
 
+#if 0 // BKTODO:
 #include "core/html/HTMLEmbedElement.h"
 #include "core/html/HTMLObjectElement.h"
+#endif
 
 namespace blink {
 
@@ -16,6 +29,8 @@ DocumentNameCollection::DocumentNameCollection(ContainerNode& document, const At
 
 bool DocumentNameCollection::elementMatches(const HTMLElement& element) const
 {
+    ASSERT(false); // BKTODO:
+#if 0
     // Match images, forms, embeds, objects and iframes by name,
     // object by id, and images by id but only if they have
     // a name attribute (this very strange rule matches IE)
@@ -25,6 +40,7 @@ bool DocumentNameCollection::elementMatches(const HTMLElement& element) const
         return element.getNameAttribute() == m_name || element.getIdAttribute() == m_name;
     if (isHTMLImageElement(element))
         return element.getNameAttribute() == m_name || (element.getIdAttribute() == m_name && element.hasName());
+#endif
     return false;
 }
 
