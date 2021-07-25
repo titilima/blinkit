6c1320c7752baa081c4a5ed5f0d3b155ff084ec0
Z <z@mysoft.tech>
2021-07-25 16:47:06 +0800

    Build.

--- src/blinkit/blink/renderer/core/html/parser/ResourcePreloader.cpp
+++ src/blinkit/blink/renderer/core/html/parser/ResourcePreloader.cpp
@@ -1,20 +1,34 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: ResourcePreloader.cpp
+// Description: ResourcePreloader Class
+//      Author: Ziming Li
+//     Created: 2021-07-25
+// -------------------------------------------------
+// Copyright (C) 2021 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "core/html/parser/ResourcePreloader.h"
-#include "core/loader/NetworkHintsInterface.h"
+// BKTODO: #include "core/loader/NetworkHintsInterface.h"
 
 namespace blink {
 
 void ResourcePreloader::takeAndPreload(PreloadRequestStream& r)
 {
+    ASSERT(false); // BKTODO:
+#if 0
     PreloadRequestStream requests;
     NetworkHintsInterfaceImpl networkHintsInterface;
     requests.swap(r);
 
     for (PreloadRequestStream::iterator it = requests.begin(); it != requests.end(); ++it)
         preload(it->release(), networkHintsInterface);
+#endif
 }
 
 }
