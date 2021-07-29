9fa10462b0609c8c39a50d0b79b9521470df57ee
Z <z@mysoft.tech>
2021-07-29 20:17:25 +0800

    Build.

--- src/blinkit/blink/renderer/core/dom/DOMArrayBuffer.h
+++ src/blinkit/blink/renderer/core/dom/DOMArrayBuffer.h
@@ -1,3 +1,14 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: DOMArrayBuffer.h
+// Description: DOMArrayBuffer Class
+//      Author: Ziming Li
+//     Created: 2021-07-19
+// -------------------------------------------------
+// Copyright (C) 2021 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -46,8 +57,6 @@ public:
     {
         return create(buffer()->slice(begin));
     }
-
-    v8::Local<v8::Object> wrap(v8::Isolate*, v8::Local<v8::Object> creationContext) override;
 
 private:
     explicit DOMArrayBuffer(PassRefPtr<WTF::ArrayBuffer> buffer)
