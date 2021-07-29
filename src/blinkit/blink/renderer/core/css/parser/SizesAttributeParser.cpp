4cdfaac6b9acbd78734d6a4ec183d84fe6dec14b
Z <z@mysoft.tech>
2021-07-29 11:55:42 +0800

    Build.

--- src/blinkit/blink/renderer/core/css/parser/SizesAttributeParser.cpp
+++ src/blinkit/blink/renderer/core/css/parser/SizesAttributeParser.cpp
@@ -1,10 +1,21 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: SizesAttributeParser.cpp
+// Description: SizesAttributeParser Class
+//      Author: Ziming Li
+//     Created: 2021-07-29
+// -------------------------------------------------
+// Copyright (C) 2021 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "core/css/parser/SizesAttributeParser.h"
 
-#include "core/MediaTypeNames.h"
+// BKTODO: #include "core/MediaTypeNames.h"
 #include "core/css/MediaQueryEvaluator.h"
 #include "core/css/parser/CSSTokenizer.h"
 #include "core/css/parser/SizesCalcParser.h"
