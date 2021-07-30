cc02e3f7f9594790a1373ef95a8f9d372add2543
Z <z@mysoft.tech>
2021-07-30 10:23:21 +0800

    Build.

--- src/blinkit/blink/renderer/core/fetch/LinkFetchResource.cpp
+++ src/blinkit/blink/renderer/core/fetch/LinkFetchResource.cpp
@@ -1,3 +1,14 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: LinkFetchResource.cpp
+// Description: LinkFetchResource Class
+//      Author: Ziming Li
+//     Created: 2021-07-30
+// -------------------------------------------------
+// Copyright (C) 2021 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -13,9 +24,13 @@ namespace blink {
 ResourcePtr<Resource> LinkFetchResource::fetch(Resource::Type type, FetchRequest& request, ResourceFetcher* fetcher)
 {
     ASSERT(type == LinkPrefetch || type == LinkSubresource);
+    ASSERT(false); // BKTODO:
+    return nullptr;
+#if 0
     ASSERT(request.resourceRequest().frameType() == WebURLRequest::FrameTypeNone);
     fetcher->determineRequestContext(request.mutableResourceRequest(), type);
     return fetcher->requestResource(request, LinkResourceFactory(type));
+#endif
 }
 
 LinkFetchResource::LinkFetchResource(const ResourceRequest& request, Type type)
