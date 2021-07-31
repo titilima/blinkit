ce5366a69099bfe6a0bb67ef561b5d59286b2688
Z <z@mysoft.tech>
2021-07-31 16:46:57 +0800

    Build.

--- src/blinkit/blink/renderer/platform/fonts/FontFallbackIterator.cpp
+++ src/blinkit/blink/renderer/platform/fonts/FontFallbackIterator.cpp
@@ -1,10 +1,21 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: FontFallbackIterator.cpp
+// Description: FontFallbackIterator Class
+//      Author: Ziming Li
+//     Created: 2021-07-31
+// -------------------------------------------------
+// Copyright (C) 2021 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "platform/fonts/FontFallbackIterator.h"
 
-#include "platform/Logging.h"
+// BKTODO: #include "platform/Logging.h"
 #include "platform/fonts/FontCache.h"
 #include "platform/fonts/FontDescription.h"
 #include "platform/fonts/FontFallbackList.h"
