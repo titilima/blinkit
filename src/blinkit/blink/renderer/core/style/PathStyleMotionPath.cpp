2b158f1496b9df54e51f5872766903c4a679499c
Z <z@mysoft.tech>
2021-07-31 16:12:54 +0800

    Build.

--- src/blinkit/blink/renderer/core/style/PathStyleMotionPath.cpp
+++ src/blinkit/blink/renderer/core/style/PathStyleMotionPath.cpp
@@ -1,17 +1,28 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: PathStyleMotionPath.cpp
+// Description: PathStyleMotionPath Class
+//      Author: Ziming Li
+//     Created: 2021-07-31
+// -------------------------------------------------
+// Copyright (C) 2021 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "core/style/PathStyleMotionPath.h"
 
-#include "core/svg/SVGPathUtilities.h"
+// BKTODO: #include "core/svg/SVGPathUtilities.h"
 
 namespace blink {
 
 PathStyleMotionPath::PathStyleMotionPath(const String& pathString)
     : m_pathString(pathString.stripWhiteSpace())
 {
-    buildPathFromString(pathString, m_path);
+    ASSERT(false); // BKTODO: buildPathFromString(pathString, m_path);
     m_length = m_path.length();
 }
 
