772f4c9f0fb704ea3fe3e4e4cffaea3eec8cb163
Z <z@mysoft.tech>
2021-08-01 10:06:57 +0800

    Cleanup.

--- scripts/make_files.bat
+++
@@ -1,7 +0,0 @@
-@REM Please install PHP and add it to PATH.
-
-cd %~dp0
-php event_type_names.h.php > ..\src\chromium\third_party\blink\renderer\core\event_type_names.h
-php event_type_names.cpp.php > ..\src\chromium\third_party\blink\renderer\core\event_type_names.cpp
-php html_names.h.php > ..\src\chromium\third_party\blink\renderer\core\html_names.h
-php html_names.cpp.php > ..\src\chromium\third_party\blink\renderer\core\html_names.cpp
--- scripts/make_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Please install PHP and add it to PATH.
-
-cd $(dirname $0)
-php event_type_names.h.php > ../src/chromium/third_party/blink/renderer/core/event_type_names.h
-php event_type_names.cpp.php > ../src/chromium/third_party/blink/renderer/core/event_type_names.cpp
-php html_names.h.php > ../src/chromium/third_party/blink/renderer/core/html_names.h
-php html_names.cpp.php > ../src/chromium/third_party/blink/renderer/core/html_names.cpp
