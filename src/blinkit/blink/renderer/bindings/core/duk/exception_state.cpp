ac1e6e7dd1e80829929d5a5f9c0af8634d3f77e8
Z <z@mysoft.tech>
2021-08-01 17:39:00 +0800

    Build old code.

---
+++ src/blinkit/blink/renderer/bindings/core/duk/exception_state.cpp
@@ -0,0 +1,65 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: exception_state.cpp
+// Description: ExceptionState & Related Classes
+//      Author: Ziming Li
+//     Created: 2019-03-07
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#include "./exception_state.h"
+
+// BKTODO: #include "base/logging.h"
+
+namespace blink {
+
+#if 0 // BKTODO:
+void ExceptionState::ClearException(void)
+{
+    ASSERT(false); // BKTODO:
+}
+#endif
+
+void ExceptionState::throwDOMException(ExceptionCode code, const String &message)
+{
+    ASSERT(false); // BKTODO:
+#if 0
+    m_exceptionCode = exceptionCode;
+    m_message = message;
+    BKLOG("DOM Exception (%d): %s", m_exceptionCode, m_message.to_string().c_str());
+#endif
+}
+
+void ExceptionState::ThrowException(void)
+{
+    NOTREACHED();
+}
+
+#if 0 // BKTODO:
+void ExceptionState::ThrowRangeError(const String &message)
+{
+    ASSERT(false); // BKTODO:
+}
+
+void ExceptionState::ThrowSecurityError(const String &sanitizedMessage, const String &unsanitizedMessage)
+{
+    ASSERT(false); // BKTODO:
+}
+
+void ExceptionState::ThrowTypeError(const String &message)
+{
+    ASSERT(false); // BKTODO:
+}
+#endif
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+NonThrowableExceptionState::NonThrowableExceptionState(const char *fileName, int line)
+    : m_fileName(fileName), m_line(line)
+{
+    // Nothing
+}
+
+} // namespace blink
---
+++ src/blinkit/blink/renderer/bindings/core/duk/exception_state.h
@@ -0,0 +1,75 @@
+// -------------------------------------------------
+// BlinKit - blink Library
+// -------------------------------------------------
+//   File Name: exception_state.h
+// Description: ExceptionState & Related Classes
+//      Author: Ziming Li
+//     Created: 2019-01-21
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_BLINK_EXCEPTION_STATE_H
+#define BLINKIT_BLINK_EXCEPTION_STATE_H
+
+#pragma once
+
+#include "blinkit/blink/renderer/core/dom/ExceptionCode.h"
+#include "blinkit/blink/renderer/wtf/Allocator.h"
+#include "blinkit/blink/renderer/wtf/Noncopyable.h"
+#include "blinkit/blink/renderer/wtf/text/WTFString.h"
+
+namespace blink {
+
+class ExceptionState
+{
+    WTF_MAKE_NONCOPYABLE(ExceptionState);
+public:
+    ExceptionCode Code(void) const { return m_exceptionCode; }
+    bool hadException(void) const { return 0 != m_exceptionCode; }
+
+    virtual void throwDOMException(ExceptionCode code, const String &message);
+    // BKTODO: virtual void ThrowRangeError(const String &message);
+    virtual void throwTypeError(const String &message);
+#if 0 // BKTODO:
+    virtual void ThrowSecurityError(const String &sanitizedMessage, const String &unsanitizedMessage = String());
+
+    void ClearException(void);
+#endif
+
+    bool ThrowIfNeeded(void)
+    {
+        if (!hadException())
+            return false;
+        ThrowException();
+        return true;
+    }
+protected:
+    ExceptionState(void) = default;
+
+    ExceptionCode m_exceptionCode = 0;
+    std::string m_message;
+private:
+    virtual void ThrowException(void);
+};
+
+class NonThrowableExceptionState final : public ExceptionState
+{
+public:
+    NonThrowableExceptionState(const char* fileName, int line);
+
+    ExceptionState& ReturnThis(void) { return *this; }
+private:
+    const char* m_fileName;
+    const int m_line;
+};
+
+class TrackExceptionState final : public ExceptionState
+{
+public:
+    TrackExceptionState(void) = default;
+};
+
+} // namespace blink
+
+#endif // BLINKIT_BLINK_EXCEPTION_STATE_H
