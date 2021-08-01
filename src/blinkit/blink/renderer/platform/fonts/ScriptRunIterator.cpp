67447c88da16d9c8e29cff4574280ce8d2ac4e27
Z <z@mysoft.tech>
2021-08-01 11:50:18 +0800

    Build.

--- src/blinkit/blink/renderer/platform/fonts/ScriptRunIterator.cpp
+++ src/blinkit/blink/renderer/platform/fonts/ScriptRunIterator.cpp
@@ -1,10 +1,21 @@
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: ScriptRunIterator.cpp
+// Description: ScriptRunIterator Class
+//      Author: Ziming Li
+//     Created: 2021-08-01
+// -------------------------------------------------
+// Copyright (C) 2021 MingYang Software Technology.
+// -------------------------------------------------
+
 // Copyright 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "ScriptRunIterator.h"
 
-#include "platform/Logging.h"
+// BKTODO: #include "platform/Logging.h"
 #include "wtf/Threading.h"
 
 #include <algorithm>
