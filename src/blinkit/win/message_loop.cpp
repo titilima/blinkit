bbfa021f57a3da8074f08106659a9908b87d30f1
Z <titilima@163.com>
2021-11-27 13:57:21 +0800

    Optimize timers.

--- src/blinkit/win/message_loop.cpp
+++ src/blinkit/win/message_loop.cpp
@@ -14,6 +14,8 @@
 #include <optional>
 #include "blinkit/win/message_task.h"
 #include "chromium/base/time/time.h"
+#include "third_party/zed/include/zed/container_utilites.hpp"
+#include "third_party/zed/include/zed/float.hpp"
 
 using namespace blink;
 
@@ -21,13 +23,45 @@ namespace BlinKit {
 
 struct MessageLoop::TimerData {
     TimerData(std::function<void()> &&t, const base::TimeTicks &desiredFireTick)
-        : task(std::move(t)), fireTick(desiredFireTick)
+        : task(std::move(t)), m_fireTick(desiredFireTick)
     {
     }
 
+    LARGE_INTEGER CalculateDueTime(const base::TimeTicks &tick = base::TimeTicks::Now()) const
+    {
+        base::TimeDelta delta = m_fireTick - tick;
+
+        LARGE_INTEGER ret = { 0 };
+        ret.QuadPart -= delta.InMicroseconds() * 10;
+        return ret;
+    }
+
     std::function<void()> task;
-    base::TimeTicks fireTick;
+private:
+    base::TimeTicks m_fireTick;
 };
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct MessageLoop::TimerTaskData {
+    TimerTaskData(MessageLoop &theLoop, HANDLE hTimer, std::function<void()> &&task)
+        : m_theLoop(theLoop), m_hTimer(hTimer), m_task(std::move(task))
+    {
+    }
+
+    void Run(const LARGE_INTEGER &tick)
+    {
+        m_task();
+        m_theLoop.OnTimerFired(m_hTimer, tick);
+        delete this;
+    }
+private:
+    MessageLoop &m_theLoop;
+    HANDLE m_hTimer;
+    std::function<void()> m_task;
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class MessageLoop::TaskRunnerImpl final : public WebTaskRunner
 {
@@ -46,6 +80,12 @@ private:
     }
     void postDelayedTask(const WebTraceLocation &loc, Task *task, double delayMs) override
     {
+        if (zed::almost_equals(delayMs, 0.0))
+        {
+            m_loop.PostTask(loc, std::bind(&TaskRunnerImpl::RunTask, task));
+            return;
+        }
+
         base::TimeTicks desiredTick = base::TimeTicks::Now();
         desiredTick += base::TimeDelta::FromMillisecondsD(delayMs);
 
@@ -68,20 +108,61 @@ private:
     MessageLoop &m_loop;
 };
 
-MessageLoop::MessageLoop(void) : m_threadId(::GetCurrentThreadId())
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+MessageLoop::MessageLoop(void)
+    : m_threadId(::GetCurrentThreadId())
+    , m_hSchedulerTimer(CreateWaitableTimer(nullptr, FALSE, nullptr))
 {
-    NewTimer();
+    m_hEvent[0] = CreateEvent(nullptr, TRUE, FALSE, nullptr);
 }
 
 MessageLoop::~MessageLoop(void)
 {
-    for (size_t i = 0; i < m_tasks.size(); ++i)
+    for (HANDLE hTimer : m_activeTimers)
     {
-        HANDLE hTimer = m_timers.at(i);
-        if (m_tasks.at(i))
-            CancelWaitableTimer(hTimer);
+        CancelWaitableTimer(hTimer);
         CloseHandle(hTimer);
     }
+
+    for (const auto &[hTimer, _] : m_alternateTimers)
+        CloseHandle(hTimer);
+
+    if (m_schedulerRunning)
+        CancelWaitableTimer(m_hSchedulerTimer);
+    CloseHandle(m_hSchedulerTimer);
+
+    CloseHandle(m_hEvent[0]);
+}
+
+void MessageLoop::Cleanup(const LARGE_INTEGER &tick)
+{
+    auto it = m_alternateTimers.begin();
+    while (m_alternateTimers.end() != it)
+    {
+        LONGLONG idle = tick.QuadPart - it->second.QuadPart;
+        idle /= 1000 * 10; // => ms
+        if (idle < 2000)
+        {
+            ++it;
+        }
+        else
+        {
+            CloseHandle(it->first);
+            it = m_alternateTimers.erase(it);
+        }
+    }
+
+    CloseHandle(m_hCleanupTimer);
+    m_hCleanupTimer = nullptr;
+}
+
+void APIENTRY MessageLoop::CleanupCallback(PVOID arg, DWORD low, DWORD high)
+{
+    LARGE_INTEGER tick;
+    tick.LowPart = low;
+    tick.HighPart = high;
+    reinterpret_cast<MessageLoop *>(arg)->Cleanup(tick);
 }
 
 std::shared_ptr<WebTaskRunner> MessageLoop::GetTaskRunner(void) const
@@ -93,33 +174,31 @@ std::shared_ptr<WebTaskRunner> MessageLoop::GetTaskRunner(void) const
 
 void MessageLoop::InstallTimer(TimerData *timerData)
 {
-    ASSERT(m_timers.size() == m_tasks.size());
-
-    size_t slot;
-    for (slot = 0; slot < m_tasks.size(); ++slot)
+    LARGE_INTEGER dueTime = timerData->CalculateDueTime();
+    if (IsLowPriority(dueTime))
     {
-        if (!m_tasks.at(slot))
-            break;
+        m_lowPriorityTimers.emplace(timerData);
+        if (!m_schedulerRunning)
+            ResumeTimerScheduler();
     }
-
-    if (m_timers.size() == slot)
-        NewTimer();
-    m_tasks[slot] = std::move(timerData->task);
-
-    base::TimeDelta delta = timerData->fireTick - base::TimeTicks::Now();
-
-    LARGE_INTEGER delay = { 0 };
-    delay.QuadPart -= delta.InMicroseconds() * 10;
-    SetWaitableTimer(m_timers.at(slot), &delay, 0, nullptr, nullptr, FALSE);
-
-    delete timerData;
+    else
+    {
+        std::unique_ptr<TimerData> _(timerData);
+        SetTimer(dueTime, std::move(timerData->task));
+    }
 }
 
-void MessageLoop::NewTimer(void)
+bool MessageLoop::IsLowPriority(const LARGE_INTEGER &dueTime)
 {
-    HANDLE hTimer = CreateWaitableTimer(nullptr, FALSE, nullptr);
-    m_timers.push_back(hTimer);
-    m_tasks.push_back(std::function<void()>());
+    static constexpr LONGLONG limit = 110 * 1000 * 10; // 110ms
+    return limit + dueTime.QuadPart < 0;
+}
+
+void MessageLoop::OnTimerFired(HANDLE hTimer, const LARGE_INTEGER &tick)
+{
+    ASSERT(zed::key_exists(m_activeTimers, hTimer));
+    m_activeTimers.erase(hTimer);
+    m_alternateTimers.emplace_front(hTimer, tick);
 }
 
 bool MessageLoop::PostTask(const WebTraceLocation &, std::function<void()> &&task)
@@ -127,13 +206,33 @@ bool MessageLoop::PostTask(const WebTraceLocation &, std::function<void()> &&tas
     return MessageTask::Post(m_threadId, std::move(task));
 }
 
-void MessageLoop::ProcessTimer(size_t slot)
+HANDLE MessageLoop::RequireTimer(void)
 {
-    ASSERT(slot < m_tasks.size());
+    HANDLE hTimer;
 
-    std::function<void()> task;
-    std::swap(task, m_tasks.at(slot));
-    task();
+    if (!m_alternateTimers.empty())
+    {
+        auto it = m_alternateTimers.begin();
+        hTimer = it->first;
+        m_alternateTimers.erase(it);
+    }
+    else
+    {
+        hTimer = CreateWaitableTimer(nullptr, FALSE, nullptr);
+    }
+
+    ASSERT(nullptr != hTimer);
+    m_activeTimers.emplace(hTimer);
+    return hTimer;
+}
+
+void MessageLoop::ResumeTimerScheduler(void)
+{
+    LARGE_INTEGER dueTime = { 0 };
+    dueTime.QuadPart = -(100 * 1000 * 10); // 100ms
+    SetWaitableTimer(m_hSchedulerTimer, &dueTime, 0, TimerSchedulerCallback, this, FALSE);
+
+    m_schedulerRunning = true;
 }
 
 int MessageLoop::Run(BkMessageFilter filter, void *userData)
@@ -141,15 +240,15 @@ int MessageLoop::Run(BkMessageFilter filter, void *userData)
     std::optional<int> ret;
     while (!ret.has_value())
     {
-        DWORD timerCount = m_timers.size();
-        DWORD dwWait = MsgWaitForMultipleObjects(timerCount, m_timers.data(), FALSE, INFINITE, QS_ALLINPUT);
-        if (WAIT_OBJECT_0 <= dwWait && dwWait < WAIT_OBJECT_0 + timerCount)
+        DWORD dwWait = MsgWaitForMultipleObjectsEx(1, m_hEvent, INFINITE, QS_ALLINPUT,
+            MWMO_ALERTABLE | MWMO_INPUTAVAILABLE);
+#ifndef NDEBUG
+        if (WAIT_FAILED == dwWait)
         {
-            ProcessTimer(dwWait - WAIT_OBJECT_0);
-            continue;
+            DWORD dwError = GetLastError();
+            ASSERT(WAIT_FAILED != dwWait);
         }
-
-        ASSERT(dwWait == timerCount);
+#endif
 
         MSG msg;
         while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
@@ -170,6 +269,67 @@ int MessageLoop::Run(BkMessageFilter filter, void *userData)
         }
     }
     return ret.value();
+}
+
+void MessageLoop::ScheduleTimers(void)
+{
+    const base::TimeTicks now = base::TimeTicks::Now();
+
+    ASSERT(!m_lowPriorityTimers.empty());
+    auto it = m_lowPriorityTimers.begin();
+    while (m_lowPriorityTimers.end() != it)
+    {
+        TimerData *data = *it;
+
+        LARGE_INTEGER dueTime = data->CalculateDueTime(now);
+        if (!IsLowPriority(dueTime))
+        {
+            std::unique_ptr<TimerData> _(data);
+            SetTimer(dueTime, std::move(data->task));
+            it = m_lowPriorityTimers.erase(it);
+        }
+        else
+        {
+            ++it;
+        }
+    }
+
+    if (!m_lowPriorityTimers.empty())
+    {
+        ResumeTimerScheduler();
+        return;
+    }
+
+    m_schedulerRunning = false;
+    if (nullptr == m_hCleanupTimer)
+    {
+        m_hCleanupTimer = CreateWaitableTimer(nullptr, FALSE, nullptr);
+
+        LARGE_INTEGER dueTime = { 0 };
+        dueTime.QuadPart = -(5 * 1000 * 1000 * 10); // 5s
+        SetWaitableTimer(m_hCleanupTimer, &dueTime, 0, CleanupCallback, this, FALSE);
+    }
+}
+
+void MessageLoop::SetTimer(const LARGE_INTEGER &dueTime, std::function<void()> &&task)
+{
+    HANDLE hTimer = RequireTimer();
+
+    TimerTaskData *taskData = new TimerTaskData(*this, hTimer, std::move(task));
+    SetWaitableTimer(hTimer, &dueTime, 0, TimerCallback, taskData, FALSE);
+}
+
+void APIENTRY MessageLoop::TimerCallback(PVOID arg, DWORD low, DWORD high)
+{
+    LARGE_INTEGER tick;
+    tick.HighPart = high;
+    tick.LowPart = low;
+    reinterpret_cast<TimerTaskData *>(arg)->Run(tick);
+}
+
+void APIENTRY MessageLoop::TimerSchedulerCallback(PVOID arg, DWORD, DWORD)
+{
+    reinterpret_cast<MessageLoop *>(arg)->ScheduleTimers();
 }
 
 } // namespace BlinKit
--- src/blinkit/win/message_loop.h
+++ src/blinkit/win/message_loop.h
@@ -14,6 +14,8 @@
 
 #pragma once
 
+#include <list>
+#include <unordered_set>
 #include "bk_app.h"
 #include "blinkit/blink/public/platform/WebTaskRunner.h"
 
@@ -30,15 +32,34 @@ public:
     std::shared_ptr<blink::WebTaskRunner> GetTaskRunner(void) const;
     bool PostTask(const blink::WebTraceLocation &loc, std::function<void()> &&task);
 private:
-    void NewTimer(void);
-    void ProcessTimer(size_t slot);
-
     struct TimerData;
+    struct TimerTaskData;
+
     void InstallTimer(TimerData *timerData);
+    void SetTimer(const LARGE_INTEGER &dueTime, std::function<void()> &&task);
+    HANDLE RequireTimer(void);
+    void OnTimerFired(HANDLE hTimer, const LARGE_INTEGER &tick);
+    static void APIENTRY TimerCallback(PVOID arg, DWORD low, DWORD high);
+
+    static bool IsLowPriority(const LARGE_INTEGER &dueTime);
+    void ResumeTimerScheduler(void);
+    void ScheduleTimers(void);
+    static void APIENTRY TimerSchedulerCallback(PVOID arg, DWORD, DWORD);
+
+    void Cleanup(const LARGE_INTEGER &tick);
+    static void APIENTRY CleanupCallback(PVOID arg, DWORD low, DWORD high);
 
     const DWORD m_threadId;
-    std::vector<HANDLE> m_timers;
-    std::vector<std::function<void()>> m_tasks;
+    HANDLE m_hEvent[1]; // Just a placeholder for waiting.
+
+    std::unordered_set<HANDLE> m_activeTimers;
+    std::list<std::pair<HANDLE, LARGE_INTEGER>> m_alternateTimers; // FILO for reusage.
+
+    HANDLE m_hSchedulerTimer;
+    bool m_schedulerRunning = false;
+    std::unordered_set<TimerData *> m_lowPriorityTimers;
+
+    HANDLE m_hCleanupTimer = nullptr;
 
     class TaskRunnerImpl;
     mutable std::shared_ptr<TaskRunnerImpl> m_taskRunner;
