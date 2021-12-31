3dda4451c549b2ff95676e069af364584d66244a
Z <titilima@163.com>
2021-12-31 14:34:58 +0800

    Range helpers for TileGrid.

--- src/blinkit/ui/compositor/tile.cpp
+++ src/blinkit/ui/compositor/tile.cpp
@@ -23,9 +23,40 @@ size_t Tile::CalculateCount(size_t length)
 
 IntSize Tile::GetAlignedSize(const IntSize &size)
 {
-    size_t columnCount = Tile::CalculateCount(size.width());
-    size_t rowCount = Tile::CalculateCount(size.height());
+    size_t columnCount = CalculateCount(size.width());
+    size_t rowCount = CalculateCount(size.height());
     return IntSize(columnCount * Size, rowCount * Size);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void TileGrid::Range::Unite(const Range &other)
+{
+    minX = std::min(minX, other.minX);
+    minY = std::min(minY, other.minY);
+    maxX = std::max(maxX, other.maxX);
+    maxY = std::max(maxY, other.maxY);
+}
+
+TileGrid::Range TileGrid::GetRangeForRect(const IntRect &rect)
+{
+    Range ret;
+    ret.minX = rect.x() / Tile::Size;
+    ret.minY = rect.y() / Tile::Size;
+    ret.maxX = Tile::CalculateCount(rect.maxX());
+    ret.maxY = Tile::CalculateCount(rect.maxY());
+    return ret;
+}
+
+IntRect TileGrid::GetRectForRange(const Range &range)
+{
+    IntRect ret;
+    if (range.minX > 0)
+        ret.setX(range.minX * Tile::Size);
+    if (range.minY > 0)
+        ret.setY(range.minY * Tile::Size);
+    ret.setSize(IntSize((range.maxX + 1) * Tile::Size, (range.maxY + 1) * Tile::Size));
+    return ret;
 }
 
 } // namespace BlinKit
--- src/blinkit/ui/compositor/tile.h
+++ src/blinkit/ui/compositor/tile.h
@@ -28,42 +28,19 @@ public:
     static IntSize GetAlignedSize(const IntSize &size);
 };
 
-template <typename T>
 class TileGrid
 {
+    STATIC_ONLY(TileGrid);
 public:
-    TileGrid(const IntSize &viewportSize)
-        : m_columnCount(Tile::CalculateCount(viewportSize.width()))
-        , m_rowCount(Tile::CalculateCount(viewportSize.height()))
-    {
-        ASSERT(!viewportSize.isEmpty());
-        m_cells.resize(m_columnCount * m_rowCount);
-    }
+    struct Range {
+        size_t minX, maxX;
+        size_t minY, maxY;
 
-    T& CellAt(size_t x, size_t y)
-    {
-        ASSERT(x < m_columnCount);
-        ASSERT(y < m_rowCount);
-        return m_cells.at(y * m_columnCount + x);
-    }
+        void Unite(const Range &other);
+    };
 
-    using Callback = std::function<void(size_t, size_t, T &)>;
-    void ProcessTilesForRect(const IntRect &rect, const Callback &callback)
-    {
-        const size_t minX = rect.x() / Tile::Size;
-        const size_t minY = rect.y() / Tile::Size;
-        const size_t maxX = Tile::CalculateCount(rect.maxX());
-        const size_t maxY = Tile::CalculateCount(rect.maxY());
-        for (size_t y = minY; y < maxY; ++y)
-        {
-            for (size_t x = minX; x < maxX; ++x)
-                callback(x, y, CellAt(x, y));
-        }
-    }
-private:
-    const size_t m_columnCount;
-    const size_t m_rowCount;
-    std::vector<T> m_cells;
+    static Range GetRangeForRect(const IntRect &rect);
+    static IntRect GetRectForRange(const Range &range);
 };
 
 } // namespace BlinKit
