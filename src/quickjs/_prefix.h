c592a9a1a8358e17468fc5c743ad240a6db8cbec
Z <titilima@163.com>
2022-03-01 17:50:15 +0800

    Prefix for zed.

---
+++ src/quickjs/_prefix.h
@@ -0,0 +1,18 @@
+#pragma once
+// -------------------------------------------------
+// BlinKit - quickjs Library
+// -------------------------------------------------
+//   File Name: _prefix.h
+// Description: Prefix Header
+//      Author: Ziming Li
+//     Created: 2022-03-01
+// -------------------------------------------------
+// Copyright (C) 2022 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_QUICKJS__PREFIX_H
+#define BLINKIT_QUICKJS__PREFIX_H
+
+#include "third_party/zed/include/zed/build_macros.h"
+
+#endif // BLINKIT_QUICKJS__PREFIX_H
