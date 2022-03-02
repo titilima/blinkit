85f40f752f5e864765de7274faca51df6b137a96
Z <titilima@163.com>
2022-03-02 12:14:22 +0800

    Dispatcher for ScriptWrappable class.

---
+++ src/blinkit/blink/renderer/bindings/core/script_wrappable.h
@@ -0,0 +1,13 @@
+#pragma once
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: script_wrappable.h
+// Description: Dispatcher for ScriptWrappable Class
+//      Author: Ziming Li
+//     Created: 2022-03-02
+// -------------------------------------------------
+// Copyright (C) 2022 MingYang Software Technology.
+// -------------------------------------------------
+
+#include "./qjs/script_wrappable.h"
