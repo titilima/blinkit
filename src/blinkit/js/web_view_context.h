43d12b559fb1355cf5153ad77024fcb6f45b600a
Z <titilima@163.com>
2022-03-03 20:56:14 +0800

    Integrate QuickJS.

---
+++ src/blinkit/js/web_view_context.h
@@ -0,0 +1,28 @@
+#pragma once
+// -------------------------------------------------
+// BlinKit - BlinKit Library
+// -------------------------------------------------
+//   File Name: web_view_context.h
+// Description: WebViewContext Class
+//      Author: Ziming Li
+//     Created: 2022-03-03
+// -------------------------------------------------
+// Copyright (C) 2022 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef BLINKIT_WEB_VIEW_CONTEXT_H
+#define BLINKIT_WEB_VIEW_CONTEXT_H
+
+#include "blinkit/blink/renderer/bindings/core/script_controller.h"
+
+namespace BlinKit {
+
+class WebViewContext final : public ScriptController
+{
+public:
+    WebViewContext(LocalFrame &frame);
+};
+
+} // namespace BlinKit
+
+#endif // BLINKIT_WEB_VIEW_CONTEXT_H
