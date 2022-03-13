456b438c7b8c4581ca21d8e64458882e7601bedd
Z <titilima@163.com>
2022-03-13 13:57:31 +0800

    Optimize for crawlers.

--- src/blinkit/blink/renderer/core/dom/script_loader_client.h
+++ src/blinkit/blink/renderer/core/dom/script_loader_client.h
@@ -40,21 +40,29 @@ namespace blink {
 class ScriptLoaderClient
 {
 public:
-    virtual ~ScriptLoaderClient() { }
+    virtual ~ScriptLoaderClient(void) = default;
 
-    virtual void dispatchLoadEvent() = 0;
+    virtual void dispatchLoadEvent(void) = 0;
 
-    virtual String sourceAttributeValue() const = 0;
-    virtual String charsetAttributeValue() const = 0;
-    virtual String typeAttributeValue() const = 0;
-    virtual String languageAttributeValue() const = 0;
+    virtual String sourceAttributeValue(void) const = 0;
+    virtual String charsetAttributeValue(void) const = 0;
+    virtual String typeAttributeValue(void) const = 0;
+    virtual String languageAttributeValue(void) const = 0;
 #ifdef BLINKIT_CRAWLER_ENABLED
-    virtual String forAttributeValue() const = 0;
-    virtual String eventAttributeValue() const = 0;
+    String forAttributeValue(void) const {
+        return String();
+    }
+    String eventAttributeValue(void) const {
+        return String();
+    }
 #endif
-    virtual bool asyncAttributeValue() const = 0;
-    virtual bool deferAttributeValue() const = 0;
-    virtual bool hasSourceAttribute() const = 0;
+    constexpr bool asyncAttributeValue(void) const {
+        return false;
+    }
+    const bool deferAttributeValue(void) const {
+        return false;
+    }
+    virtual bool hasSourceAttribute(void) const = 0;
 };
 
 }
